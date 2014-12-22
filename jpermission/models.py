bc5422821d759b7f99faedce7b1baebe94fdb531
ibuler <ibuler@qq.com>
2014-12-22 23:36:32 +0800

    添加jpermssion models

--- jpermission/models.py
+++ jpermission/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from juser.models import User
+from jasset.models import Asset
 
-# Create your models here.
+
+class Permission(models.Model):
+    PERM_USER_TYPE_CHOICE = (
+        ('S', 'Super'),
+        ('C', 'Common'),
+    )
+    user = models.ForeignKey(User)
+    asset = models.ForeignKey(Asset)
+    is_ldap = models.BooleanField(default=True)
+    perm_user_type = models.CharField(choices=PERM_USER_TYPE_CHOICE,
+                                      blank=True,
+                                      null=True)
+
+    def __unicode__(self):
+        return '%s_%s' % (self.user.username, self.asset.ip)
