6d32e6a1a76f0e494523cfc10c48078d84428749
ibuler <ibuler@qq.com>
2015-01-02 17:43:28 +0800

    添加Demo皮肤设置

--- jumpserver/views.py
+++ jumpserver/views.py
@@ -6,7 +6,3 @@ from django.shortcuts import render_to_response
 
 def base(request):
     return render_to_response('base.html')
-
-
-def skin_config(request):
-    return render_to_response('skin_config.html')
