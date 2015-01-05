a184ae98886e0646da0c44b8499fabd799e41662
ibuler <ibuler@qq.com>
2015-01-05 21:20:09 +0800

    添加测试 useradd

--- juser/urls.py
+++ juser/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from jumpserver import views
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns('juser.views',
     # Examples:
     # url(r'^$', 'jumpserver.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    (r'^user/', include('juser.urls')),
+    (r'^useradd/$', 'user_add'),
 )
--- juser/views.py
+++ juser/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 
 
-
+def user_add(request):
+    return render_to_response('user_add.html')
 
 
