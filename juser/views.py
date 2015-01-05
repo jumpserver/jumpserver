64c155bede493a6dad1431b8550c9c8a560ee0fa
ibuler <ibuler@qq.com>
2015-01-05 23:55:05 +0800

    修改添加用户

--- juser/views.py
+++ juser/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+# coding: utf-8
+
 from django.shortcuts import render_to_response
 
 
 def user_add(request):
-    return render_to_response('user_add.html', {'path1': 'juser', 'path2': 'user_add'})
+    return render_to_response('user_add.html', {'header_title': u'添加用户 | Add User', 'path1': 'juser', 'path2': 'user_add'})
 
 
