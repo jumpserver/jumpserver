c8828f40bab012353ba6274f1813633e2726f19a
ibuler <ibuler@qq.com>
2015-01-05 23:52:25 +0800

    测试修改Path

--- juser/views.py
+++ juser/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response
 
 
 def user_add(request):
-    return render_to_response('user_add.html')
+    return render_to_response('user_add.html', {'path1': 'juser', 'path2': 'user_add'})
 
 
