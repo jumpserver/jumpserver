fde3d19a9d328f39d27b5d0f110950b47f07ea6d
ibuler <ibuler@qq.com>
2015-01-07 23:20:48 +0800

    添加分组template

--- juser/views.py
+++ juser/views.py
@@ -3,7 +3,16 @@
 from django.shortcuts import render_to_response
 
 
+def group_add(request):
+    return render_to_response('juser/group_add.html',
+                              {'header_title': u'添加属组 | Add Group', 'path1': 'juser', 'path2': 'group_add'})
+
+
 def user_add(request):
-    return render_to_response('user_add.html', {'header_title': u'添加用户 | Add User', 'path1': 'juser', 'path2': 'user_add'})
+    return render_to_response('juser/user_add.html',
+                              {'header_title': u'添加用户 | Add User', 'path1': 'juser', 'path2': 'user_add'})
+
+
+
 
 
