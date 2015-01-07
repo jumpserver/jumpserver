0ed085348972f867c9ce486cb2e0ecbe7d7cfa8b
halcyon <864072399@qq.com>
2015-01-07 18:35:09 +0800

    完善资产添加和查看资产页面

--- jasset/urls.py
+++ jasset/urls.py
@@ -5,4 +5,7 @@ from jasset.views import *
 urlpatterns = patterns('',
     url(r'^$', index),
     url(r'jadd', jadd),
+    url(r'jlist', jlist),
+    url(r'jadd_idc', jadd_idc),
+    url(r'jlist_idc', jlist_idc),
 )
