30cfa8a5f4d65aa12176a5e224b86744581f2455
guanghongwei <hongwei.guang@yolu-inc.com>
2015-01-10 13:30:01 +0800

    修改asset models

--- jasset/models.py
+++ jasset/models.py
@@ -1,6 +1,6 @@
 import datetime
 from django.db import models
-from juser.models import Group, User
+from juser.models import Group as UserGroup
 
 
 class IDC(models.Model):
@@ -9,6 +9,7 @@ class IDC(models.Model):
 
     def __unicode__(self):
         return self.name
+
 
 class Group(models.Model):
     name = models.CharField(max_length=80, unique=True)
@@ -28,13 +29,14 @@ class Asset(models.Model):
     ip = models.IPAddressField(unique=True)
     port = models.SmallIntegerField(max_length=5)
     idc = models.ForeignKey(IDC)
-    group = models.ManyToManyField(Group)
+    user_group = models.ManyToManyField(UserGroup)
+    bis_group = models.ManyToManyField(Group)
     login_type = models.CharField(max_length=1, choices=LOGIN_TYPE_CHOICES, default='L')
     username_common = models.CharField(max_length=20, blank=True, null=True)
     password_common = models.CharField(max_length=80, blank=True, null=True)
     username_super = models.CharField(max_length=20, blank=True, null=True)
     password_super = models.CharField(max_length=80, blank=True, null=True)
-    date_added = models.DateTimeField(auto_now=True,default=datetime.datetime.now(), null=True)
+    date_added = models.DateTimeField(auto_now=True, default=datetime.datetime.now(), null=True)
     is_active = models.BooleanField(default=True)
     comment = models.CharField(max_length=100, blank=True, null=True)
 
