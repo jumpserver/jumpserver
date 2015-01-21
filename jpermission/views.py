5957286d573cc4209058f66ef21defe97337b630
ibuler <ibuler@qq.com>
2015-01-21 23:27:56 +0800

    查看授权用户

--- jpermission/views.py
+++ jpermission/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
+from juser.models import User
 
 
 def perm_user_list(request):
-    return render_to_response('jperm/perm_user_list.html')
+    header_title, path1, path2 = u'查看授权用户 | Perm User Detail.', u'授权管理', u'用户详情'
+    users = User.objects.all()
+    return render_to_response('jperm/perm_user_list.html', locals(),)
