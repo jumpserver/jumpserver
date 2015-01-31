795183c7abad72fa6e406d08ccefccb73a4268a3
root <root@saltstack.com>
2015-01-31 13:09:29 +0800

    remove node module from repo

--- websocket/node_modules/express/node_modules/merge-descriptors/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (dest, src) {
-  Object.getOwnPropertyNames(src).forEach(function (name) {
-    var descriptor = Object.getOwnPropertyDescriptor(src, name)
-    Object.defineProperty(dest, name, descriptor)
-  })
-
-  return dest
-}
--- websocket/node_modules/node-tail/node_modules/socket.io/node_modules/socket.io-client/node_modules/has-binary/History.md
+++
@@ -1,5 +0,0 @@
-
-0.1.5 / 2014-09-04
-==================
-
- * prevent browserify from bundling `Buffer`
