76b1bf2c39d650697774b11c810f8df315170175
ibuler <ibuler@qq.com>
2015-04-19 18:39:15 +0800

    测试脚本去掉注释

--- docs/AddUserAsset.py
+++ docs/AddUserAsset.py
@@ -128,12 +128,12 @@ def test_add_log():
 
 if __name__ == '__main__':
     #install()
-    #test_add_dept()
-    #test_add_group()
-    #test_add_user()
-    #test_add_idc()
-    #test_add_asset_group()
-    #test_add_asset()
+    test_add_dept()
+    test_add_group()
+    test_add_user()
+    test_add_idc()
+    test_add_asset_group()
+    test_add_asset()
     test_add_log()
 
 
