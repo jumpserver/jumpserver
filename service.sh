8f7e1bfc38badc6f680daab8189d8568a1245f32
ibuler <ibuler@qq.com>
2015-04-20 22:55:26 +0800

    modify readme

--- service.sh
+++ service.sh
@@ -12,11 +12,12 @@ case $1 in
 start)
     python $base_dir/manage.py runserver 0.0.0.0:80 &
     python $base_dir/log_handler.py &
-    cd $base_dir/websocket/; daemon node index.js &
+    cd $base_dir/websocket/; node index.js &
     ;;
 
 stop)
-    pkill -15 python
+    pkill -15 runserver
+    pkill -15 log_handler.py
     pkill -15 node
     ;;
 
