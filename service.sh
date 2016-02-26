8aec0c1ac72b480d26690a48a065c759a31d0345
yumaojun <719118794@qq.com>
2016-02-26 14:13:21 +0800

    fix (install jumpserver):   install  jumpserver compatible with ubuntu
    1.  install.py  add  platform judge, fix get_ip_addr function
    2.  next.py  little adjust
    3. service.sh little adjust

--- service.sh
+++ service.sh
@@ -24,54 +24,62 @@ lockfile=/var/lock/subsys/${PROC_NAME}
 
 
 start() {
-	jump_start=$"Starting ${PROC_NAME} service:"
-	
-	if [ -f $lockfile ];then	
-		 echo "jumpserver  is running..."
-		 success "$jump_start"
-	else
-		 daemon python $base_dir/manage.py runserver 0.0.0.0:80 &>> /tmp/jumpserver.log 2>&1 &
-		 daemon python $base_dir/manage.py crontab add &>> /tmp/jumpserver.log 2>&1
-		 daemon python $base_dir/run_websocket.py &> /dev/null 2>&1 &
+        jump_start=$"Starting ${PROC_NAME} service:"
+
+        if [ -f $lockfile ];then
+                 echo "jumpserver  is running..."
+                 success "$jump_start"
+        else
+                 daemon python $base_dir/manage.py runserver 0.0.0.0:80 &>> /tmp/jumpserver.log 2>&1 &
+                 daemon python $base_dir/manage.py crontab add &>> /tmp/jumpserver.log 2>&1
+                 daemon python $base_dir/run_websocket.py &> /dev/null 2>&1 &
          sleep 4
 
-		 echo -n "$jump_start"
-		 nums=0
+                 echo -n "$jump_start"
+                 nums=0
          for i in manage.py run_websocket.py;do
-             ps aux | grep "$i" | grep -v 'grep' &> /dev/null && let nums+=1  || echo "$i not running"
+             if ps aux | grep "$i" | grep -v 'grep' &> /dev/null; then
+                 nums=$[nums+1]
+             else
+                 echo "$i not running"
+             fi
          done
 
          if [ "x$nums" == "x2" ];then
             success "$jump_start"
+            if [ ! -e $lockfile ]; then
+                lockfile_dir=`dirname $lockfile`
+                mkdir -pv $lockfile_dir
+            fi
             touch "$lockfile"
             echo
          else
             failure "$jump_start"
             echo
          fi
-	 fi
-		
-	
+         fi
+
+
 }
 
 
 stop() {
 
-	echo -n $"Stopping ${PROC_NAME} service:"
-	
-    daemon python $base_dir/manage.py crontab remove &>> /tmp/jumpserver.log 2>&1
-	ps aux | grep -E 'manage.py|run_websocket.py' | grep -v grep | awk '{print $2}' | xargs kill -9 &> /dev/null
-	ret=$?
+        echo -n $"Stopping ${PROC_NAME} service:"
 
-	if [ $ret -eq 0 ]; then
-		echo_success
-		echo
+    daemon python $base_dir/manage.py crontab remove &>> /tmp/jumpserver.log 2>&1
+        ps aux | grep -E 'manage.py|run_websocket.py' | grep -v grep | awk '{print $2}' | xargs kill -9 &> /dev/null
+        ret=$?
+
+        if [ $ret -eq 0 ]; then
+                echo_success
+                echo
         rm -f "$lockfile"
-	else
-		echo_failure
-		echo
+        else
+                echo_failure
+                echo
         rm -f "$lockfile"
-	fi
+        fi
 
 }
 
@@ -83,22 +91,19 @@ restart(){
 }
 
 # See how we were called.
-case "$1" in    
-  start)                
-        start           
-        ;;              
-  stop)                         
-        stop                    
-        ;;                      
+case "$1" in
+  start)
+        start
+        ;;
+  stop)
+        stop
+        ;;
 
-  restart)      
-        restart         
-        ;;              
-            
-  *)                    
+  restart)
+        restart
+        ;;
+
+  *)
         echo $"Usage: $0 {start|stop|restart}"
-        exit 2          
-esac                    
-                        
-
-
+        exit 2
+esac
