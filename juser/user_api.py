e0ae7eeee7c3e25592d2e721036556c1903d6f4f
老广 <ibuler@qq.com>
2016-07-12 17:08:26 +0800

    Update user_api.py

--- juser/user_api.py
+++ juser/user_api.py
@@ -183,7 +183,7 @@ def server_del_user(username):
     bash('userdel -r -f %s' % username)
     logger.debug('rm -f %s/%s_*.pem' % (os.path.join(KEY_DIR, 'user'), username))
     bash('rm -f %s/%s_*.pem' % (os.path.join(KEY_DIR, 'user'), username))
-    bash('rm -f %s/%s.pem' % (os.path.join(KEY_DIR, 'user'), username))
+    bash('rm -f %s/%s.pem*' % (os.path.join(KEY_DIR, 'user'), username))
 
 
 def get_display_msg(user, password='', ssh_key_pwd='', send_mail_need=False):
