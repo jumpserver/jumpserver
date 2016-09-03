d371c0c5ae363685735ab61056c808726802cf48
liuzheng712 <liuzheng712@gmail.com>
2016-09-04 06:55:12 +0800

    mkdirp

--- apps/manage.py
+++ apps/manage.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
+import errno
 
 if __name__ == "__main__":
+    try:
+        os.makedirs('./logs')
+    except OSError as exc: # Python > 2.5 (except OSError, exec: for Python < 2.5)
+        if exc.errno == errno.EEXIST and os.path.isdir('./logs'):
+            pass
+        else: raise
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jumpserver.settings")
     try:
         from django.core.management import execute_from_command_line
