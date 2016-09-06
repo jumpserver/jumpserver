dc01833a5b8056baa1764bf2f1aa37e113a758e6
ibuler <ibuler@qq.com>
2016-09-06 16:55:57 +0800

    Forget to forgot

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,9 +8,9 @@ app_name = 'users'
 urlpatterns = [
     url(r'^login$', views.UserLoginView.as_view(), name='login'),
     url(r'^logout$', views.UserLogoutView.as_view(), name='logout'),
-    url(r'^password/forget$', views.UserForgotPasswordView.as_view(), name='forget-password'),
-    url(r'^password/forget/sendmail-success$',
-        views.UserForgotPasswordSendmailSuccessView.as_view(), name='forget-password-sendmail-success'),
+    url(r'^password/forgot$', views.UserForgotPasswordView.as_view(), name='forgot-password'),
+    url(r'^password/forgot/sendmail-success$',
+        views.UserForgotPasswordSendmailSuccessView.as_view(), name='forgot-password-sendmail-success'),
     url(r'^password/reset$', views.UserResetPasswordView.as_view(), name='reset-password'),
     url(r'^password/reset/success$', views.UserResetPasswordSuccessView.as_view(),
         name='reset-password-success'),
