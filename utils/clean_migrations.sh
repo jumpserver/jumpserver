b14b97e03058c5ad1dea67183de5e3428db624b7
oldseven <oldseven@nn.com>
2017-09-19 13:36:21 +0800

    apply-perms-list

--- utils/clean_migrations.sh
+++ utils/clean_migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 
-for app in users assets perms audits ops applications;do
+for app in users assets perms apply_perms audits ops applications;do
     rm -f ../apps/$app/migrations/00*
 done
