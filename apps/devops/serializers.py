0c0760e42236893a65dee173d225f47f91bed9ba
ZQian <qian.zhang01@hand-china.com>
2017-09-28 11:41:50 +0800

    devops  task列表页serializers完成

--- apps/devops/serializers.py
+++ apps/devops/serializers.py
@@ -1,13 +1,21 @@
 # ~*~ coding: utf-8 ~*~
 from __future__ import unicode_literals
+
 from rest_framework import serializers
 
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
         fields = '__all__'
 
-
+    @staticmethod
+    def get_user(obj):
+        if obj.admin_user:
+            return obj.admin_user.username
+        if obj.system_user:
+            return obj.system_user.username
