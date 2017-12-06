3be9b16c631713209818eb1995f8e1285d694ef4
VinCiLord <VinCiLord@users.noreply.github.com>
2017-12-06 12:40:43 +0800

    [Bugfix] ops Add app_name (#845)

--- apps/ops/urls/api_urls.py
+++ apps/ops/urls/api_urls.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from rest_framework.routers import DefaultRouter
 from .. import api
 
+app_name = 'ops'
 
 router = DefaultRouter()
 router.register(r'v1/tasks', api.TaskViewSet, 'task')
