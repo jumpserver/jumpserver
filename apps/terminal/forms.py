ea27d05c587fc8112ce52d7ded5b26ecb17ddd2e
liuzheng712 <liuzheng712@gmail.com>
2018-02-26 18:05:41 +0800

    feat: update app setting

--- apps/terminal/forms.py
+++ apps/terminal/forms.py
@@ -16,13 +16,23 @@ def get_all_command_storage():
         yield (k, k)
 
 
+def get_all_replay_storage():
+    # storage_choices = []
+    from common.models import Setting
+    Setting.refresh_all_settings()
+    for k, v in settings.TERMINAL_REPLAY_STORAGE.items():
+        yield (k, k)
+
+
 class TerminalForm(forms.ModelForm):
     command_storage = forms.ChoiceField(choices=get_all_command_storage(),
                                         label=_("Command storage"))
+    replay_storage = forms.ChoiceField(choices=get_all_replay_storage(),
+                                       label=_("Replay storage"))
 
     class Meta:
         model = Terminal
-        fields = ['name', 'remote_addr', 'ssh_port', 'http_port', 'comment', 'command_storage']
+        fields = ['name', 'remote_addr', 'ssh_port', 'http_port', 'comment', 'command_storage', 'replay_storage']
         help_texts = {
             'ssh_port': _("Coco ssh listen port"),
             'http_port': _("Coco http/ws listen port"),
