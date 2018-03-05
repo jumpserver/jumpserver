efcf31b2a965cda7db728535d971b5dacccd552a
ibuler <ibuler@qq.com>
2018-03-05 13:09:00 +0800

    [Delete] 删除build 页面

--- docs/_build/html/_sources/installation.rst.txt
+++
@@ -1,9 +0,0 @@
-安装文档
-++++++++++++++++++++++++
-
-.. toctree::
-   :maxdepth: 1
-
-   quickstart
-   step_by_step
-   upgrade
--- docs/_build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0.5.0',
-    LANGUAGE: 'zh_CN',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
