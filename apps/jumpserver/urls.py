967800391e97f24d7294c7f94cf18b5175b73a0c
ibuler <ibuler@qq.com>
2018-04-12 17:31:37 +0800

    [Update] 更新api

--- apps/jumpserver/urls.py
+++ apps/jumpserver/urls.py
@@ -36,9 +36,11 @@ urlpatterns = [
     url(r'^captcha/', include('captcha.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+      + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += [
         url(r'^docs/', schema_view, name="docs"),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-      + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
 
