fba9001d56471b1f3e413e93acb4fe464c4e1b47
litian6363 <litian6363@163.com>
2018-04-16 17:58:09 +0800

    fix 19

--- apps/assets/views/asset.py
+++ apps/assets/views/asset.py
@@ -211,7 +211,7 @@ def asset_more_detail_view(request, asset_id, node):
 
 
 def asset_more_detail_update(request, asset_id):
-    asset_id = get_object_or_404(Asset, id=asset_id).id
+    asset_id = get_object_or_404(Asset, id=asset_id)
 
     node = request.POST['node']
     project = request.POST['project']
