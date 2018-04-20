a34d51c9d06ef2533ef18eb3cbded379e2bd947d
litian6363 <litian6363@163.com>
2018-04-20 17:32:29 +0800

    fix 5

--- apps/assets/forms/user.py
+++ apps/assets/forms/user.py
@@ -95,8 +95,9 @@ class SystemUserForm(PasswordAndKeyAuthForm):
         password = self.cleaned_data.get('password', '') or None
         auto_generate_key = self.cleaned_data.get('auto_generate_key', False)
         private_key, public_key = super().gen_keys()
+        logger.info('pppppppppppppppppppp>>>>>>>>>>>>>>>>>>>>' + password + 'hhhhhhhhhhhhhhhhhhhhhh<<<<<<<<<<<<<<<<<<<<<<<<<<')
 
-        if auto_generate_key and (password is None or password == ''):
+        if auto_generate_key and not password:
             logger.info('Auto generate key and set system user auth')
             system_user.auto_gen_auth()
         else:
