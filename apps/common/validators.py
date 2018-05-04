4ed9e110909c1ec07f0f3b2d0a6f2dba6300d0b3
ibuler <ibuler@qq.com>
2018-05-04 14:58:52 +0800

    [Update] 修改用户名校验规则

--- apps/common/validators.py
+++ apps/common/validators.py
@@ -4,4 +4,4 @@ from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 
 
-alphanumeric = RegexValidator(r'^[0-9a-zA-Z_-]*$', _('Special char not allowed'))
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z_@\-\.]*$', _('Special char not allowed'))
