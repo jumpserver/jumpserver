8e74a042820f5b2cb8bc18d41f0ddaa1a5bae964
ibuler <ibuler@qq.com>
2018-05-09 17:35:46 +0800

    [Update] 修改一些bug

--- apps/assets/models/base.py
+++ apps/assets/models/base.py
@@ -104,6 +104,11 @@ class AssetUser(models.Model):
         if update_fields:
             self.save(update_fields=update_fields)
 
+    def clear_auth(self):
+        self._password = ''
+        self._private_key = ''
+        self.save()
+
     def auto_gen_auth(self):
         password = str(uuid.uuid4())
         private_key, public_key = ssh_key_gen(
