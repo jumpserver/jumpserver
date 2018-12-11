9c0f00f625a220777c6b18ffc0d21aa59ddd323a
老广 <ibuler@qq.com>
2018-12-11 20:32:55 +0800

    [Update] 修改命令执行 (#2148)
    * [Update] 修改命令执行
    
    * Update forms.py

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -260,8 +260,6 @@ class User(AbstractUser):
             self.role = 'Admin'
             self.is_active = True
         super().save(*args, **kwargs)
-        if current_org and current_org.is_real():
-            self.orgs.add(current_org.id)
 
     @property
     def private_token(self):
