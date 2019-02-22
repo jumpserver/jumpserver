3e6e0153cf12c6422e1e99bd4bf745bfab44b2b7
ibuler <ibuler@qq.com>
2019-02-22 16:14:50 +0800

    [Update] 修改entrypoint并增加debug日志

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,19 @@
 function cleanup()
 {
     local pids=`jobs -p`
-    if [[ "$pids" != ""  ]]; then
-        kill $pids >/dev/null 2>/dev/null
+    if [[ "${pids}" != ""  ]]; then
+        kill ${pids} >/dev/null 2>/dev/null
     fi
 }
 
 service="all"
-if [ "$1" != "" ];then
+if [[ "$1" != "" ]];then
     service=$1
 fi
 
+if [[ "$1" == "bash" ]];then
+    bash
+fi
+
 trap cleanup EXIT
-python jms start $service
+python jms start ${service}
