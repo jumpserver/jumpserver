6004ef3f0df707a8b0d498457496444c9a98233d
ibuler <ibuler@qq.com>
2019-03-04 10:53:49 +0800

    [Update] 修改settings

--- apps/settings/migrations/0002_migrate_data.py
+++ apps/settings/migrations/0002_migrate_data.py
@@ -10,8 +10,10 @@ class Migration(migrations.Migration):
     ]
     sql = "INSERT INTO setting(name, value, category, encrypted, enabled, comment) " \
           "SELECT name, value, category, encrypted, enabled, comment from settings"
+    rename_sql = "RENAME TABLE settings TO settings_bak"
     settings_table_exist = 'settings' in connection.introspection.table_names()
 
     operations = []
     if settings_table_exist:
         operations.append(migrations.RunSQL(sql))
+        operations.append(migrations.RunSQL(rename_sql))
