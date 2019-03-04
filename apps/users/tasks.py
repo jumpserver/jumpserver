53f0b2e9b072b45f36fd5d5bbbf636b558fc34d2
ibuler <ibuler@qq.com>
2019-03-04 10:39:44 +0800

    [Update] 增加清理登陆日志的任务

--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 #
 
+import datetime
+from django.utils import timezone
+from django.conf import settings
 from celery import shared_task
 
 from ops.celery.utils import create_or_update_celery_periodic_tasks
-from ops.celery.decorator import after_app_ready_start
-from .models import User
+from ops.celery.decorator import after_app_ready_start, register_as_period_task
 from common.utils import get_logger
+from .models import User, LoginLog
 from .utils import write_login_log, send_password_expiration_reminder_mail
 
 
@@ -43,3 +46,15 @@ def check_password_expired_periodic():
         }
     }
     create_or_update_celery_periodic_tasks(tasks)
+
+
+@register_as_period_task(interval=3600*24)
+@shared_task
+def clean_login_log_period():
+    now = timezone.now()
+    try:
+        days = int(settings.LOGIN_LOG_KEEP_DAYS)
+    except ValueError:
+        days = 90
+    expired_day = now - datetime.timedelta(days=days)
+    LoginLog.objects.filter(datetime__lt=expired_day).delete()
