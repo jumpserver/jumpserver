9bb58afee1670ac330ef33214c7685dfa660d48d
ibuler <ibuler@qq.com>
2019-03-11 10:27:40 +0800

    [Update] bugfix

--- apps/assets/models/node.py
+++ apps/assets/models/node.py
@@ -276,7 +276,8 @@ class Node(OrgModelMixin):
     @classmethod
     def default_node(cls):
         defaults = {'value': 'Default'}
-        return cls.objects.get_or_create(defaults=defaults, key='1')
+        obj, created = cls.objects.get_or_create(defaults=defaults, key='1')
+        return obj
 
     def as_tree_node(self):
         from common.tree import TreeNode
