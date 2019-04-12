78e89aca76a279fb9294e76c7f46d4a0d76368d6
BaiJiangJie <bugatti_it@163.com>
2019-04-12 18:12:12 +0800

    [Feature] 4. perms actions - 授权规则创建页面: 设置 actions 默认 all

--- apps/perms/const.py
+++ apps/perms/const.py
@@ -3,9 +3,14 @@
 
 from django.utils.translation import ugettext_lazy as _
 
+PERMS_ACTION_NAME_ALL = 'all'
+PERMS_ACTION_NAME_CONNECT = 'connect'
+PERMS_ACTION_NAME_UPLOAD_FILE = 'upload_file'
+PERMS_ACTION_NAME_DOWNLOAD_FILE = 'download_file'
+
 PERMS_ACTION_NAME_CHOICES = (
-    ('all', _('All')),
-    ('connect', _('Connect')),
-    ('upload_file', _('Upload file')),
-    ('download_file', _('Download file')),
+    (PERMS_ACTION_NAME_ALL, _('All')),
+    (PERMS_ACTION_NAME_CONNECT, _('Connect')),
+    (PERMS_ACTION_NAME_UPLOAD_FILE, _('Upload file')),
+    (PERMS_ACTION_NAME_DOWNLOAD_FILE, _('Download file')),
 )
