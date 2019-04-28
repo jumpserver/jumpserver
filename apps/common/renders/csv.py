a60594e7d6896631d23f580d90856d7492645fe6
BaiJiangJie <bugatti_it@163.com>
2019-04-28 17:55:08 +0800

    [Update] 优化renders csv代码逻辑

--- apps/common/renders/csv.py
+++ apps/common/renders/csv.py
@@ -1,41 +1,57 @@
-from rest_framework_csv.renderers import CSVStreamingRenderer
+# ~*~ coding: utf-8 ~*~
+#
+
+import unicodecsv
+
+from six import BytesIO
+from rest_framework.renderers import BaseRenderer
 
 
-class JMSCSVRender(CSVStreamingRenderer):
+class JMSCSVRender(BaseRenderer):
+
+    media_type = 'text/csv'
+    format = 'csv'
+    serializer = None
+    action = None
+
+    def _get_labels(self):
+        fields = self.serializer.get_fields()
+        labels = {k: v.label for k, v in fields.items() if v.label}
+        return labels
+
+    def _get_header(self):
+        fields = self.serializer.get_fields()
+        if self.action == 'import':
+            header = [k for k, v in fields.items() if not v.read_only]
+        elif self.action == 'update':
+            header = [k for k, v in fields.items() if not v.read_only]
+            header.insert(0, 'id')
+        else:
+            header = [k for k, v in fields.items() if not v.write_only]
+        return header
+
+    @staticmethod
+    def _gen_table(data, header, labels=None):
+        labels = labels or {}
+        yield [labels.get(k, k) for k in header]
+
+        for item in data:
+            row = [item.get(key) for key in header]
+            yield row
+
     def render(self, data, media_type=None, renderer_context=None):
-        if renderer_context is None:
-            renderer_context = {}
-        view = renderer_context.get("view")
-        if not view:
-            return super().render(data, media_type=media_type,
-                                  renderer_context=renderer_context)
-        serializer = view.get_serializer()
-        if not serializer:
-            return super().render(data, media_type=media_type,
-                                  renderer_context=renderer_context)
-        request = renderer_context.get("request")
-        if not request:
-            return super().render(data, media_type=media_type,
-                                  renderer_context=renderer_context)
-        labels = {}
-        header = []
-        csv_fields = getattr(serializer.Meta, "csv_fields", None)
+        renderer_context = renderer_context or {}
+        encoding = renderer_context.get('encoding', 'utf-8')
+        self.serializer = renderer_context['view'].get_serializer_class()()
+        self.action = renderer_context['request'].query_params.get('action', 'export')
 
-        for name, field in serializer.get_fields().items():
-            if request.query_params.get('action') == 'import':
-                if field.read_only:
-                    continue
-            else:
-                if field.write_only:
-                    continue
-            if csv_fields and name not in csv_fields:
-                continue
-            header.append(name)
-            if field.label:
-                labels[name] = field.label
-            else:
-                labels[name] = name
-        self.labels = labels
-        self.header = header
-        return super().render(data, media_type=media_type,
-                              renderer_context=renderer_context)
+        header = self._get_header()
+        labels = self._get_labels()
+        table = self._gen_table(data, header, labels)
+
+        csv_buffer = BytesIO()
+        csv_writer = unicodecsv.writer(csv_buffer, encoding=encoding)
+        for row in table:
+            csv_writer.writerow(row)
+
+        return csv_buffer.getvalue()
