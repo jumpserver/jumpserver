09ddcd1028c7d70f1921d4c823fb63d74ad6e97f
BaiJiangJie <bugatti_it@163.com>
2019-04-28 17:54:48 +0800

    [Update] 优化parsers csv代码结构

--- apps/common/parsers/csv.py
+++ apps/common/parsers/csv.py
@@ -12,7 +12,7 @@ logger = get_logger(__file__)
 
 def universal_newlines(stream):
     """
-    保证在通用换行模式下打开文件
+    保证在`通用换行模式`下打开文件
     """
     for line in stream.splitlines():
         yield line
@@ -33,43 +33,53 @@ class JMSCSVParser(BaseParser):
     """
 
     media_type = 'text/csv'
-    view = None
+    serializer = None
 
     @staticmethod
-    def _get_data(header, rows):
-        data = []
-        for row in rows:
-            row_data = dict(zip(header, row))
-            row_data = {
-                k: v for k, v in row_data.items() if k.strip() and v.strip()
-            }
-            data.append(row_data)
-        return data
-
-    def _get_header_fields_map(self):
+    def _get_fields_map(serializer):
         fields_map = {}
-        serializer = self.view.get_serializer_class()()
         fields = serializer.get_fields()
         fields_map.update({v.label: k for k, v in fields.items()})
         fields_map.update({k: k for k, _ in fields.items()})
         return fields_map
 
-    def _convert_header(self, header):
-        fields_map = self._get_header_fields_map()
-        _header = [fields_map.get(name, '') for name in header]
-        return _header
+    @staticmethod
+    def _gen_rows(csv_data, charset='utf-8', **kwargs):
+        csv_reader = unicodecsv.reader(csv_data, encoding=charset, **kwargs)
+        for row in csv_reader:
+            if not any(row):  # 空行
+                continue
+
+            yield row
+
+    @staticmethod
+    def _universal_newlines(stream):
+        """
+        保证在`通用换行模式`下打开文件
+        """
+        for line in stream.splitlines():
+            yield line
 
     def parse(self, stream, media_type=None, parser_context=None):
         parser_context = parser_context or {}
         encoding = parser_context.get('encoding', 'utf-8')
-        self.view = parser_context.get("view")
+        serializer = parser_context["view"].get_serializer_class()()
+
         try:
             stream_data = stream.read()
-            binary = universal_newlines(stream_data)
-            rows = generate_rows(binary, charset=encoding)
+            binary = self._universal_newlines(stream_data)
+            rows = self._gen_rows(binary, charset=encoding)
+
             header = next(rows)
-            header = self._convert_header(header)
-            data = self._get_data(header, rows)
+            fields_map = self._get_fields_map(serializer)
+            header = [fields_map.get(name, '') for name in header]
+
+            data = []
+            for row in rows:
+                row_data = dict(zip(header, row))
+                row_data = {k: v for k, v in row_data.items() if k.strip() and v.strip()}
+                data.append(row_data)
             return data
+
         except Exception as e:
             logger.debug(e, exc_info=True)
