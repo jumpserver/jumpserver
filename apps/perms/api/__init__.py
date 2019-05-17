bd93820a9b5818ae8e84e3dbc948a69e9887a46b
BaiJiangJie <bugatti_it@163.com>
2019-05-17 17:42:48 +0800

    [Feature] Perms模块修改目录结构

--- apps/perms/api/__init__.py
+++ apps/perms/api/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
 
-from .permission import *
+from .asset_permission import *
 from .user_permission import *
 from .user_group_permission import *
---
+++ apps/perms/views/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+#
+
+from .asset_permission import *
