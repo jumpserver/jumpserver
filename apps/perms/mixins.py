60db7a5f4f37686d72ace28965fe7db827cdd263
BaiJiangJie <bugatti_it@163.com>
2019-06-20 11:34:38 +0800

    [Bugfix] 修复普通用户加载被授权的RemoteApp为空的bug

--- apps/perms/mixins.py
+++ apps/perms/mixins.py
@@ -1,6 +1,7 @@
 # ~*~ coding: utf-8 ~*~
 #
 
+from orgs.utils import set_to_root_org
 
 from orgs.utils import set_to_root_org
 
@@ -116,4 +117,3 @@ class ChangeOrgIfNeedMixin(object):
     def get(self, request, *args, **kwargs):
         self.change_org_if_need(request, kwargs)
         return super().get(request, *args, **kwargs)
-
