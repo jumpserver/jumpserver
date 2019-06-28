2cef505295dfca23938045db22ef4efede289fc0
BaiJiangJie <bugatti_it@163.com>
2019-06-28 15:38:30 +0800

    [Update] 优化OpenID细节

--- apps/authentication/backends/openid/decorator.py
+++ apps/authentication/backends/openid/decorator.py
@@ -8,15 +8,19 @@ import requests
 from urllib3.exceptions import InsecureRequestWarning
 from django.conf import settings
 
-old_merge_environment_settings = requests.Session.merge_environment_settings
-
 __all__ = [
     'ssl_verification',
 ]
 
+old_merge_environment_settings = requests.Session.merge_environment_settings
+
 
 @contextlib.contextmanager
 def no_ssl_verification():
+    """
+    https://stackoverflow.com/questions/15445981/
+    how-do-i-disable-the-security-certificate-check-in-python-requests
+    """
     opened_adapters = set()
 
     def merge_environment_settings(self, url, proxies, stream, verify, cert):
@@ -24,21 +28,19 @@ def no_ssl_verification():
         # all the opened adapters once we're done. Otherwise, the effects of
         # verify=False persist beyond the end of this context manager.
         opened_adapters.add(self.get_adapter(url))
-
-        settings = old_merge_environment_settings(self, url, proxies, stream, verify, cert)
-        settings['verify'] = False
-
-        return settings
+        _settings = old_merge_environment_settings(
+            self, url, proxies, stream, verify, cert
+        )
+        _settings['verify'] = False
+        return _settings
 
     requests.Session.merge_environment_settings = merge_environment_settings
-
     try:
         with warnings.catch_warnings():
             warnings.simplefilter('ignore', InsecureRequestWarning)
             yield
     finally:
         requests.Session.merge_environment_settings = old_merge_environment_settings
-
         for adapter in opened_adapters:
             try:
                 adapter.close()
