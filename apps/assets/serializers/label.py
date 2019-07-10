59eccfec4d801e68ba5e2f85ae6531eb3b3712e5
jym503558564 <503558564@qq.com>
2019-07-10 16:08:28 +0800

    [Update] 创建/更新 标签 使用api

--- apps/assets/serializers/label.py
+++ apps/assets/serializers/label.py
@@ -13,7 +13,10 @@ class LabelSerializer(BulkOrgResourceModelSerializer):
 
     class Meta:
         model = Label
-        fields = '__all__'
+        fields = [
+            'id', 'name', 'value', 'category', 'is_active', 'comment',
+            'date_created', 'asset_count', 'assets', 'get_category_display'
+        ]
         list_serializer_class = AdaptedBulkListSerializer
 
     @staticmethod
