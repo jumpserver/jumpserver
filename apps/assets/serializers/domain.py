63f3fa98db89fda70626392bcd405f543a0896a6
八千流 <40739051+jym503558564@users.noreply.github.com>
2019-07-11 18:21:19 +0800

    [Update] 创建/更新 网域使用api (#2915)
    * [Update] 创建/更新 网域使用api
    
    * [Update] 修改小问题
    
    * [Update] 修改小问题

--- apps/assets/serializers/domain.py
+++ apps/assets/serializers/domain.py
@@ -15,7 +15,11 @@ class DomainSerializer(BulkOrgResourceModelSerializer):
 
     class Meta:
         model = Domain
-        fields = '__all__'
+        fields = [
+            'id', 'name', 'asset_count', 'gateway_count', 'comment', 'assets',
+            'date_created'
+        ]
+        read_only_fields = ( 'asset_count', 'gateway_count', 'date_created')
         list_serializer_class = AdaptedBulkListSerializer
 
     @staticmethod
