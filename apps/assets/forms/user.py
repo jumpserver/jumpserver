40de7d64c669cc1bb4e5c4a80922a8150dfe7f57
ibuler <ibuler@qq.com>
2019-07-17 13:41:26 +0800

    [Update] 修改private创建

--- apps/assets/forms/user.py
+++ apps/assets/forms/user.py
@@ -26,39 +26,39 @@ class PasswordAndKeyAuthForm(forms.ModelForm):
         label=_("Password"),
     )
     # Need use upload private key file except paste private key content
-    private_key_file = forms.FileField(required=False, label=_("Private key"))
+    private_key = forms.FileField(required=False, label=_("Private key"))
 
-    def clean_private_key_file(self):
-        private_key_file = self.cleaned_data['private_key_file']
+    def clean_private_key(self):
+        private_key_f = self.cleaned_data['private_key']
         password = self.cleaned_data['password']
 
-        if private_key_file:
-            key_string = private_key_file.read()
-            private_key_file.seek(0)
+        if private_key_f:
+            key_string = private_key_f.read()
+            private_key_f.seek(0)
             key_string = key_string.decode()
 
             if not validate_ssh_private_key(key_string, password):
                 msg = _('Invalid private key, Only support '
                         'RSA/DSA format key')
                 raise forms.ValidationError(msg)
-        return private_key_file
+        return private_key_f
 
     def validate_password_key(self):
         password = self.cleaned_data['password']
-        private_key_file = self.cleaned_data.get('private_key_file', '')
+        private_key_f = self.cleaned_data.get('private_key', '')
 
-        if not password and not private_key_file:
+        if not password and not private_key_f:
             raise forms.ValidationError(_(
                 'Password and private key file must be input one'
             ))
 
     def gen_keys(self):
         password = self.cleaned_data.get('password', '') or None
-        private_key_file = self.cleaned_data['private_key_file']
+        private_key_f = self.cleaned_data['private_key']
         public_key = private_key = None
 
-        if private_key_file:
-            private_key = private_key_file.read().strip().decode('utf-8')
+        if private_key_f:
+            private_key = private_key_f.read().strip().decode('utf-8')
             public_key = ssh_pubkey_gen(private_key=private_key, password=password)
         return private_key, public_key
 
@@ -69,7 +69,7 @@ class AdminUserForm(PasswordAndKeyAuthForm):
 
     class Meta:
         model = AdminUser
-        fields = ['name', 'username', 'password', 'private_key_file', 'comment']
+        fields = ['name', 'username', 'password', 'private_key', 'comment']
         widgets = {
             'name': forms.TextInput(attrs={'placeholder': _('Name')}),
             'username': forms.TextInput(attrs={'placeholder': _('Username')}),
@@ -87,7 +87,7 @@ class SystemUserForm(OrgModelForm, PasswordAndKeyAuthForm):
         model = SystemUser
         fields = [
             'name', 'username', 'protocol', 'auto_generate_key',
-            'password', 'private_key_file', 'auto_push', 'sudo',
+            'password', 'private_key', 'auto_push', 'sudo',
             'comment', 'shell', 'priority', 'login_mode', 'cmd_filters',
         ]
         widgets = {
