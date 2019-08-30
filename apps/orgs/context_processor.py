42e0fe94a2a5328aa97ae236c99c5f9ec6b5467e
BaiJiangJie <bugatti_it@163.com>
2019-08-30 10:15:55 +0800

    [Update] 修改OrgProcessor Anonymous问题

--- apps/orgs/context_processor.py
+++ apps/orgs/context_processor.py
@@ -9,9 +9,9 @@ def org_processor(request):
     context = {
         # 'ADMIN_ORGS': request.user.admin_orgs,
         # 'AUDIT_ORGS': request.user.audit_orgs,
-        'ADMIN_OR_AUDIT_ORGS': request.user.admin_or_audit_orgs,
+        'ADMIN_OR_AUDIT_ORGS': Organization.get_user_admin_or_audit_orgs(request.user),
         'CURRENT_ORG': get_org_from_request(request),
-        'HAS_ORG_PERM': request.user.can_admin_current_org,
+        # 'HAS_ORG_PERM': current_org.can_admin_by(request.user),
     }
     return context
 
