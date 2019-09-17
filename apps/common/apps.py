85234664b46fe943c0644000da10e9b758c534d2
ibuler <ibuler@qq.com>
2019-09-17 11:38:35 +0800

    [Update] 修改setting无法生效的问题

--- apps/common/apps.py
+++ apps/common/apps.py
@@ -2,16 +2,6 @@ from __future__ import unicode_literals
 
 import sys
 from django.apps import AppConfig
-from django.dispatch import receiver
-from django.db.backends.signals import connection_created
-
-
-@receiver(connection_created)
-def on_db_connection_ready(sender, **kwargs):
-    from .signals import django_ready
-    if 'migrate' not in sys.argv:
-        django_ready.send(CommonConfig)
-        connection_created.disconnect(on_db_connection_ready)
 
 
 class CommonConfig(AppConfig):
@@ -19,3 +9,6 @@ class CommonConfig(AppConfig):
 
     def ready(self):
         from . import signals_handlers
+        from .signals import django_ready
+        if 'migrate' not in sys.argv:
+            django_ready.send(CommonConfig)
