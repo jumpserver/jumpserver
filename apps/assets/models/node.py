e781e9cb293cf76b64be403bc8284f934370ef10
ibuler <ibuler@qq.com>
2019-09-25 16:01:58 +0800

    [Bugfix] 修复用户树显示资产数量不对的bug

--- apps/assets/models/node.py
+++ apps/assets/models/node.py
@@ -257,11 +257,6 @@ class NodeAssetsMixin:
 
     @lazyproperty
     def assets_amount(self):
-        """
-        获取节点下所有资产数量速度太慢，所以需要重写，使用cache等方案,
-        这里不能用lazyproperty, 因为用户树，会设置 _assets_amount的值
-        :return:
-        """
         amount = self.tree().assets_amount(self.key)
         return amount
 
