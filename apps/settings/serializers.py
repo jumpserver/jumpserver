c9ca81e80f451e6f55f70e23e40d54a90b119831
BaiJiangJie <bugatti_it@163.com>
2019-10-08 17:14:33 +0800

    [Update] 邮件设置添加测试邮件收件人配置项

--- apps/settings/serializers.py
+++ apps/settings/serializers.py
@@ -7,6 +7,7 @@ class MailTestSerializer(serializers.Serializer):
     EMAIL_HOST_USER = serializers.CharField(max_length=1024)
     EMAIL_HOST_PASSWORD = serializers.CharField(required=False, allow_blank=True)
     EMAIL_FROM = serializers.CharField(required=False, allow_blank=True)
+    EMAIL_RECIPIENT = serializers.CharField(required=False, allow_blank=True)
     EMAIL_USE_SSL = serializers.BooleanField(default=False)
     EMAIL_USE_TLS = serializers.BooleanField(default=False)
 
