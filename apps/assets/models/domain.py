aa428b02995fd3903738e6cf116669ab51d6b53a
ibuler <ibuler@qq.com>
2019-11-13 16:40:34 +0800

    [Update] 修改测试网关提示，不能包含特殊字符

--- apps/assets/models/domain.py
+++ apps/assets/models/domain.py
@@ -3,9 +3,9 @@
 
 import uuid
 import random
+import re
 
 import paramiko
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -63,6 +63,9 @@ class Gateway(AssetUser):
     def test_connective(self, local_port=None):
         if local_port is None:
             local_port = self.port
+        if not re.match(r'\w+$', self.password):
+            return False, _("Password should not contain special characters")
+
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         proxy = paramiko.SSHClient()
