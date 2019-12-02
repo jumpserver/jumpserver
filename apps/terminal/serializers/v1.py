0f95f63e206a6d0ee658fc2575be616690a85a08
BaiJiangJie <bugatti_it@163.com>
2019-12-02 17:10:48 +0800

    [Update] 限制更新存储时不覆盖原有AK SK 等字段

--- apps/terminal/serializers/v1.py
+++ apps/terminal/serializers/v1.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #
+import copy
 from rest_framework import serializers
 
 from orgs.mixins.serializers import BulkOrgResourceModelSerializer
@@ -68,8 +69,41 @@ class ReplayStorageMetaDictField(CustomMetaDictField):
     need_convert_key = True
 
 
-class ReplayStorageSerializer(serializers.ModelSerializer):
+class BaseStorageSerializerMixin:
+    type_map_fields = None
+
+    def process_meta(self, instance, validated_data):
+        new_meta = copy.deepcopy(validated_data.get('meta', {}))
+        tp = validated_data.get('type', '')
+
+        if tp != instance.type:
+            return new_meta
+
+        old_meta = instance.meta
+        fields = self.type_map_fields.get(instance.type, [])
+        for field in fields:
+            if not field.get('write_only', False):
+                continue
+            field_name = field['name']
+            new_value = new_meta.get(field_name, '')
+            old_value = old_meta.get(field_name, '')
+            field_value = new_value if new_value else old_value
+            new_meta[field_name] = field_value
+
+        return new_meta
+
+    def update(self, instance, validated_data):
+        meta = self.process_meta(instance, validated_data)
+        validated_data['meta'] = meta
+        return super().update(instance, validated_data)
+
+
+class ReplayStorageSerializer(BaseStorageSerializerMixin,
+                              serializers.ModelSerializer):
+
     meta = ReplayStorageMetaDictField()
+
+    type_map_fields = const.REPLAY_STORAGE_TYPE_MAP_FIELDS
 
     class Meta:
         model = ReplayStorage
@@ -82,8 +116,12 @@ class CommandStorageMetaDictField(CustomMetaDictField):
     need_convert_key = True
 
 
-class CommandStorageSerializer(serializers.ModelSerializer):
+class CommandStorageSerializer(BaseStorageSerializerMixin,
+                               serializers.ModelSerializer):
+
     meta = CommandStorageMetaDictField()
+
+    type_map_fields = const.COMMAND_STORAGE_TYPE_MAP_FIELDS
 
     class Meta:
         model = CommandStorage
