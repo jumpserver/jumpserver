ea3689ed3c709341064e03c9c2f35100b0fa2a55
ibuler <ibuler@qq.com>
2019-12-02 10:42:31 +0800

    [Update] 修改一些css

--- apps/audits/api.py
+++ apps/audits/api.py
@@ -11,4 +11,4 @@ class FTPLogViewSet(OrgModelViewSet):
     model = FTPLog
     serializer_class = FTPLogSerializer
     permission_classes = (IsOrgAdminOrAppUser | IsOrgAuditor,)
-
+    http_method_names = ['get', 'post', 'head', 'options']
