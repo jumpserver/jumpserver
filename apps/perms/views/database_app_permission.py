5eacda1e21f80754c12ee8bbd041097b1b308ed0
BaiJiangJie <bugatti_it@163.com>
2019-12-12 12:28:04 +0800

    [Update] 添加数据库应用授权View（待续）

---
+++ apps/perms/views/database_app_permission.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+#
+
+from django.views.generic import (
+    TemplateView, CreateView, UpdateView, DetailView, ListView
+)
+
+from common.permissions import PermissionsMixin
+
+
+__all__ = []
+
+
+class DatabaseAppPermissionListView():
+    pass
+
+
+class DatabaseAppPermissionCreateView():
+    pass
+
+
+class DatabaseAppPermissionUpdateView():
+    pass
+
+
+class DatabaseAppPermissionDetailView():
+    pass
+
+
+class DatabaseAppPermissionUserView():
+    pass
+
+
+class DatabaseAppPermissionDatabaseAppView():
+    pass
