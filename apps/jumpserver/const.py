9556d33d1c13849243c3dd9f824c62fcf26131bd
BaiJiangJie <bugatti_it@163.com>
2019-12-31 18:49:16 +0800

    [Update] 修改版本号（v1.5.6）

--- apps/jumpserver/const.py
+++ apps/jumpserver/const.py
@@ -7,6 +7,6 @@ __all__ = ['BASE_DIR', 'PROJECT_DIR', 'VERSION', 'CONFIG', 'DYNAMIC']
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(BASE_DIR)
-VERSION = '1.5.5'
+VERSION = '1.5.6'
 CONFIG = ConfigManager.load_user_config()
 DYNAMIC = ConfigManager.get_dynamic_config(CONFIG)
