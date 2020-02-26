5e9adb4ca57c447372147aa7bf416e4fad69d706
BaiJiangJie <32935519+BaiJiangJie@users.noreply.github.com>
2020-02-26 15:11:20 +0800

    [Update] LDAP Test Util (#3720)
    * [Update] LDAPTestUtil 1
    
    * [Update] LDAPTestUtil 2
    
    * [Update] LDAPTestUtil 3
    
    * [Update] LDAPTestUtil 4
    
    * [Update] LDAPTestUtil 5
    
    * [Update] LDAPTestUtil 6
    
    * [Update] LDAPTestUtil 7

--- apps/settings/serializers/ldap.py
+++ apps/settings/serializers/ldap.py
@@ -3,10 +3,12 @@
 
 from rest_framework import serializers
 
-__all__ = ['LDAPTestSerializer', 'LDAPUserSerializer']
+__all__ = [
+    'LDAPTestConfigSerializer', 'LDAPUserSerializer', 'LDAPTestLoginSerializer'
+]
 
 
-class LDAPTestSerializer(serializers.Serializer):
+class LDAPTestConfigSerializer(serializers.Serializer):
     AUTH_LDAP_SERVER_URI = serializers.CharField(max_length=1024)
     AUTH_LDAP_BIND_DN = serializers.CharField(max_length=1024, required=False, allow_blank=True)
     AUTH_LDAP_BIND_PASSWORD = serializers.CharField(required=False, allow_blank=True)
@@ -14,6 +16,12 @@ class LDAPTestSerializer(serializers.Serializer):
     AUTH_LDAP_SEARCH_FILTER = serializers.CharField()
     AUTH_LDAP_USER_ATTR_MAP = serializers.CharField()
     AUTH_LDAP_START_TLS = serializers.BooleanField(required=False)
+    AUTH_LDAP = serializers.BooleanField(required=False)
+
+
+class LDAPTestLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=1024, required=True)
+    password = serializers.CharField(max_length=2014, required=True)
 
 
 class LDAPUserSerializer(serializers.Serializer):
