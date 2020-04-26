17542112782f7bdffa9ee05fd11053b138ba468b
Bai <bugatti_it@163.com>
2020-04-26 20:35:21 +0800

    [Update] oidc认证模块说明

--- apps/authentication/backends/oidc/__init__.py
+++ apps/authentication/backends/oidc/__init__.py
@@ -1,14 +1,4 @@
 """
-Work list
-
-1. remove 之前对 oidc 的调试支持依赖（基本完成）
-
-2. 添加 AUTH_OPENID 的配置的兼容，并只使用新的配置（基本完成）
-
-3. remove 之前对 keycloak 的依赖 （基本完成）
-
-3. 使用 jumpserver-django-oidc-rp 作为依赖（同时修改直到适配 jumpserver)
-
-4. 完全支持标准 OIDC
-
+使用下面的工程，进行jumpserver 的 oidc 认证
+https://github.com/BaiJiangJie/jumpserver-django-oidc-rp
 """
