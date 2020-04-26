41bd352192bbf7e3989b1c6344acc62a3dea50c2
Bai <bugatti_it@163.com>
2020-04-26 15:19:15 +0800

    [Update] 添加oidc user登录成功的信号机制

--- apps/authentication/signals_handlers.py
+++ apps/authentication/signals_handlers.py
@@ -5,6 +5,6 @@ from jms_oidc_rp.signals import oidc_user_login_success
 from .signals import post_auth_success
 
 
-@oidc_user_login_success
+@receiver(oidc_user_login_success)
 def on_oidc_user_login_success(sender, request, user, **kwargs):
     post_auth_success.send(sender, user=user, request=request)
