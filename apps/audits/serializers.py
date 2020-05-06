8c0bf0b71bb1b87efef2a44e9a485e418d55e10e
ibuler <ibuler@qq.com>
2020-05-06 19:35:19 +0800

    [Update] Login log 添加display field

--- apps/audits/serializers.py
+++ apps/audits/serializers.py
@@ -15,11 +15,14 @@ class FTPLogSerializer(serializers.ModelSerializer):
 
 
 class UserLoginLogSerializer(serializers.ModelSerializer):
+    type_display = serializers.ReadOnlyField(source='get_type_display')
+    status_display = serializers.ReadOnlyField(source='get_status_display')
+
     class Meta:
         model = models.UserLoginLog
         fields = (
-            'username', 'type', 'ip', 'city', 'user_agent',
-            'mfa', 'reason', 'status', 'datetime'
+            'username', 'type', 'type_display', 'ip', 'city', 'user_agent',
+            'mfa', 'reason', 'status', 'status_display', 'datetime'
         )
 
 
