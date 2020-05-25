93453cc8c3c56f5c63aad2a51c72a627e70ea623
xinwen <coderWen@126.com>
2020-05-25 14:50:07 +0800

    [Update] perms.serializers.asset_permission.AssetPermissionSerializer 添加字段

--- apps/perms/serializers/asset_permission.py
+++ apps/perms/serializers/asset_permission.py
@@ -43,7 +43,8 @@ class AssetPermissionSerializer(BulkOrgResourceModelSerializer):
         model = AssetPermission
         mini_fields = ['id', 'name']
         small_fields = mini_fields + [
-            'is_active', 'is_expired', 'is_valid', 'actions', 'created_by', 'date_created'
+            'is_active', 'is_expired', 'is_valid', 'actions', 'created_by', 'date_created',
+            'date_expired', 'date_start', 'comment'
         ]
         m2m_fields = [
             'users', 'user_groups', 'assets', 'nodes', 'system_users',
