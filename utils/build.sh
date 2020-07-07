061f461c3f9a047c31d6f5ce768a5415bf2f5563
ibuler <ibuler@qq.com>
2020-07-07 13:02:47 +0800

    ci(release&build): 添加 github workflows, 自动构建 release
    - 添加 utils/build.sh 脚本，构建后放到 release 目录中
    - 当 push tags时，自动创建 Release Draft
    - 自动生成 Release Change Log
    - 自动构建包，上传到 Release Assets

---
+++ utils/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# 该build基于registry.fit2cloud.com/public/python:3
+utils_dir=$(pwd)
+project_dir=$(dirname "$utils_dir")
+release_dir=${project_dir}/release
+
+# 编译翻译文件
+cd "${project_dir}/apps" && python manage.py compilemessages
+
+# 打包
+cd "${project_dir}" || exit 3
+rm -rf "${release_dir:?}/*"
+to_dir="${release_dir}/jumpserver"
+mkdir -p "${to_dir}"
+git archive --format tar HEAD | tar x -C "${to_dir}"
+
+# 修改版本号文件
+if [[ -n ${VERSION} ]]; then
+  sed -i "" "s@VERSION = .*@VERSION = \"${VERSION}\"@g" "${release_dir}/jumpserver/apps/jumpserver/const.py"
+fi
+
