8e16e35341bd826acb37eabb53f712c06d54494b
xinwen <coderWen@126.com>
2020-07-21 16:31:35 +0800

    s

--- apps/perms/models/asset_permission.py
+++ apps/perms/models/asset_permission.py
@@ -29,6 +29,7 @@ class Action:
     CLIPBOARD_PASTE = 0b1 << 4
     ALL = 0xff
     UPDOWNLOAD = UPLOAD | DOWNLOAD
+    CLIPBOARD_COPY_PASTE = CLIPBOARD_COPY | CLIPBOARD_PASTE
 
     DB_CHOICES = (
         (ALL, _('All')),
@@ -38,6 +39,7 @@ class Action:
         (UPDOWNLOAD, _("Upload download")),
         (CLIPBOARD_COPY, _('Clipboard copy')),
         (CLIPBOARD_PASTE, _('Clipboard paste')),
+        (CLIPBOARD_COPY_PASTE, _('Clipboard copy paste'))
     )
 
     NAME_MAP = {
@@ -48,6 +50,7 @@ class Action:
         UPDOWNLOAD: "updownload",
         CLIPBOARD_COPY: 'clipboard_copy',
         CLIPBOARD_PASTE: 'clipboard_paste',
+        CLIPBOARD_COPY_PASTE: 'clipboard_copy_paste'
     }
 
     NAME_MAP_REVERSE = {v: k for k, v in NAME_MAP.items()}
