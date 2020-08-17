3c159eba296b6a2f21df2d35eb38a4a686381956
xinwen <coderWen@126.com>
2020-08-17 18:26:49 +0800

    s

--- apps/perms/signals_handler.py
+++ apps/perms/signals_handler.py
@@ -2,12 +2,15 @@
 #
 from django.db.models.signals import m2m_changed, post_save, post_delete
 from django.dispatch import receiver
-from django.db.models import F, Count
+from django.db.models import Q
 
+from users.models import User
+from assets.models import Node, Asset
 from common.utils import get_logger
 from common.decorator import on_transaction_commit
 from .models import AssetPermission, RemoteAppPermission
 from .utils.asset_permission import AssetPermissionUtil
+from .utils import update_users_tree_for_add, ADD, REMOVE
 
 
 logger = get_logger(__file__)
@@ -17,6 +20,7 @@ logger = get_logger(__file__)
 @on_transaction_commit
 def on_permission_change(sender, action='', **kwargs):
     logger.debug('Asset permission changed, refresh user tree cache')
+
     AssetPermissionUtil.expire_all_user_tree_cache()
 
 # Todo: 检查授权规则到期，从而修改授权规则
@@ -24,10 +28,36 @@ def on_permission_change(sender, action='', **kwargs):
 
 @receiver(m2m_changed, sender=AssetPermission.nodes.through)
 def on_permission_nodes_changed(sender, instance=None, action='', reverse=None, **kwargs):
+    pk_set = kwargs.get('pk_set', [])
+
+    user_ap_query_name = AssetPermission.users.field.related_query_name()
+    group_ap_query_name = AssetPermission.user_groups.field.related_query_name()
+
+    if isinstance(instance, AssetPermission):
+        user_ap_q = Q(**{f'{user_ap_query_name}': instance})
+        group_ap_q = Q(**{f'groups__{group_ap_query_name}': instance})
+        users = list(User.objects.filter(user_ap_q | group_ap_q).distinct())
+        nodes = list(Node.objects.filter(id__in=pk_set))
+    else:
+        user_ap_q = Q(**{f'{user_ap_query_name}__id__in': pk_set})
+        group_ap_q = Q(**{f'groups__{group_ap_query_name}__id__in': pk_set})
+        users = list(User.objects.filter(user_ap_q | group_ap_q).distinct())
+        nodes = [instance]
+
+    if action == 'post_add':
+        _action = ADD
+    elif action == 'post_remove':
+        _action = REMOVE
+    else:
+        # Not support `clear`
+        _action = None
+    if _action:
+        update_users_tree_for_add(users, nodes=nodes, action=_action)
+
     if action != 'post_add' and reverse:
         return
     logger.debug("Asset permission nodes change signal received")
-    nodes = kwargs['model'].objects.filter(pk__in=kwargs['pk_set'])
+    nodes = kwargs['model'].objects.filter(pk__in=pk_set)
     system_users = instance.system_users.all()
     for system_user in system_users:
         system_user.nodes.add(*tuple(nodes))
@@ -35,6 +65,32 @@ def on_permission_nodes_changed(sender, instance=None, action='', reverse=None, 
 
 @receiver(m2m_changed, sender=AssetPermission.assets.through)
 def on_permission_assets_changed(sender, instance=None, action='', reverse=None, **kwargs):
+    pk_set = kwargs.get('pk_set', [])
+
+    user_ap_query_name = AssetPermission.users.field.related_query_name()
+    group_ap_query_name = AssetPermission.user_groups.field.related_query_name()
+
+    if isinstance(instance, AssetPermission):
+        user_ap_q = Q(**{f'{user_ap_query_name}': instance})
+        group_ap_q = Q(**{f'groups__{group_ap_query_name}': instance})
+        users = list(User.objects.filter(user_ap_q | group_ap_q).distinct())
+        assets = list(Asset.objects.filter(id__in=pk_set))
+    else:
+        user_ap_q = Q(**{f'{user_ap_query_name}__id__in': pk_set})
+        group_ap_q = Q(**{f'groups__{group_ap_query_name}__id__in': pk_set})
+        users = list(User.objects.filter(user_ap_q | group_ap_q).distinct())
+        assets = [instance]
+
+    if action == 'post_add':
+        _action = ADD
+    elif action == 'post_remove':
+        _action = REMOVE
+    else:
+        # Not support `clear`
+        _action = None
+    if _action:
+        update_users_tree_for_add(users, assets=assets, action=_action)
+
     if action != 'post_add' and reverse:
         return
     logger.debug("Asset permission assets change signal received")
@@ -66,6 +122,27 @@ def on_asset_permission_system_users_changed(sender, instance=None, action='',
 @receiver(m2m_changed, sender=AssetPermission.users.through)
 def on_asset_permission_users_changed(sender, instance=None, action='',
                                       reverse=False, **kwargs):
+    pk_set = kwargs.get('pk_set', [])
+
+    if isinstance(instance, AssetPermission):
+        nodes = list(instance.nodes.all())
+        assets = list(instance.assets.all())
+        users = list(User.objects.filter(id__in=pk_set).distinct())
+    else:
+        nodes = list(Node.objects.filter(granted_by_permissions__id__in=pk_set))
+        assets = list(Asset.objects.filter(granted_by_permissions__id__in=pk_set))
+        users = [instance]
+
+    if action == 'post_add':
+        _action = ADD
+    elif action == 'post_remove':
+        _action = REMOVE
+    else:
+        # Not support `clear`
+        _action = None
+    if _action:
+        update_users_tree_for_add(users, nodes=nodes, assets=assets, action=_action)
+
     if action != 'post_add' and reverse:
         return
     logger.debug("Asset permission users change signal received")
@@ -80,6 +157,27 @@ def on_asset_permission_users_changed(sender, instance=None, action='',
 @receiver(m2m_changed, sender=AssetPermission.user_groups.through)
 def on_asset_permission_user_groups_changed(sender, instance=None, action='',
                                             reverse=False, **kwargs):
+    pk_set = kwargs.get('pk_set', [])
+
+    if isinstance(instance, AssetPermission):
+        nodes = list(instance.nodes.all())
+        assets = list(instance.assets.all())
+        users = list(User.objects.filter(groups__id__in=pk_set).distinct())
+    else:
+        nodes = list(Node.objects.filter(granted_by_permissions__id__in=pk_set))
+        assets = list(Asset.objects.filter(granted_by_permissions__id__in=pk_set))
+        users = list(User.objects.filter(groups=instance).distinct())
+
+    if action == 'post_add':
+        _action = ADD
+    elif action == 'post_remove':
+        _action = REMOVE
+    else:
+        # Not support `clear`
+        _action = None
+    if _action:
+        update_users_tree_for_add(users, nodes=nodes, assets=assets, action=_action)
+
     if action != 'post_add' and reverse:
         return
     logger.debug("Asset permission user groups change signal received")
@@ -122,110 +220,3 @@ def on_remoteapps_permission_user_groups_changed(sender, instance=None, action='
                                             action=action, reverse=reverse, **kwargs)
 
 
-from itertools import chain
-from assets.models import Node, Asset
-from perms.models import GrantedNode
-from users.models import User
-from typing import List
-
-
-def inc_granted_count(obj, value=1):
-    obj._granted_count = getattr(obj, '_granted_count', 0) + value
-
-
-def inc_asset_granted_count(obj, value=1):
-    obj._granted_count = getattr(obj, '_asset_granted_count', 0) + value
-
-
-ADD = object()
-REMOVE = object()
-
-
-def update_users_tree_for_add(granted_nodes: List[Node], assets: List[Asset],
-                              users: List[User], action=ADD):
-    """
-    `_granted_count` 授权次数，等于节点数或者资产数
-    """
-
-    # 查询授权`Asset`关联的 `Node`
-    asset_granted_nodes = Node.objects.filter(
-        assets__in=assets
-    ).annotate(
-        _granted_count=Count('assets', distinct=True),
-        _asset_granted_count=Count('assets', distinct=True),
-        _granted=False
-    ).distinct()
-    # 授权的 `Node`
-    for n in granted_nodes:
-        inc_granted_count(n)
-        n._granted = True
-    # 资产授权节点与直接授权节点总共的祖先`key`，因为两者可能会重叠，所以字典的键复杂
-    ancestor_keys_map = {(n, n._granted): n.get_ancestor_keys() for n in chain(asset_granted_nodes, granted_nodes)}
-    ancestors_map = {node.key: node for node in
-                     Node.objects.filter(key__in=chain(*ancestor_keys_map.values()))}
-    for (node, _), keys in ancestor_keys_map:
-        for key in keys:
-            ancestor = ancestors_map[key]  # TODO 404
-            inc_granted_count(ancestor, node._granted_count)
-    keys = ancestors_map.keys() | {n.key for n in ancestor_keys_map.keys()}
-    # 资产授权节点和直接授权节点，两者会有重叠
-    all_nodes = [*granted_nodes, *asset_granted_nodes]
-
-    for user in users:
-        # 每个用户单独处理自己的树
-        to_create = {}
-        to_update = []
-        _granted_nodes = GrantedNode.objects.filter(key__in=keys, user=user)
-        granted_nodes_map = {gn.key: gn for gn in _granted_nodes}
-        for node in all_nodes:
-            _granted = getattr(node, '_granted', False),
-            _asset_granted_count = getattr(node, '_asset_granted_count', 0)
-            _granted_count = getattr(node, '_granted_count')
-            if node.key in granted_nodes_map:
-                granted_node = granted_nodes_map[node.key]
-                if action is ADD:
-                    if _granted:
-                        if granted_node.granted:
-                            #相同节点不能授权两次
-                            raise ValueError('')
-                        granted_node.granted = True
-
-                    inc_asset_granted_count(granted_node, _asset_granted_count)
-                    inc_granted_count(granted_node, _granted_count)
-                elif action is REMOVE:
-                    if _granted:
-                        if not granted_node.granted:
-                            # 数据有问题
-                            raise ValueError('')
-                        granted_node.granted = False
-                    inc_asset_granted_count(granted_node, -_asset_granted_count)
-                    inc_granted_count(granted_node, -_granted_count)
-
-                to_update.append(granted_node)
-            else:
-                if action is REMOVE:
-                    # 数据有问题
-                    raise ValueError('')
-                if node.key not in to_create:
-                    granted_node = GrantedNode(
-                        key=node.key,
-                        user=user,
-                        granted=_granted,
-                        granted_count=_granted_count,
-                        asset_granted_count=_asset_granted_count
-                    )
-                    to_create[node.key] = granted_node
-                else:
-                    granted_node = to_create[node.key]
-                    granted_node.granted_count += _granted_count
-                    granted_node.asset_granted_count += _asset_granted_count
-                    if _granted:
-                        if granted_node.granted:
-                            raise ValueError()
-                        granted_node.granted = True
-
-        for gn in to_update:
-            gn.granted_count = F('granted_count') + gn._granted_count
-            gn.asset_granted_count = F('asset_granted_count') + gn._asset_granted_count
-        GrantedNode.objects.bulk_update(to_update)
-        GrantedNode.objects.bulk_create(to_create.values())
---
+++ apps/perms/utils/user_node_tree.py
@@ -0,0 +1,116 @@
+from itertools import chain
+
+from django.db.models import F, Count, Value, BooleanField
+
+from assets.models import Node, Asset
+from perms.models import GrantedNode
+from users.models import User
+
+
+def inc_granted_count(obj, value=1):
+    obj._granted_count = getattr(obj, '_granted_count', 0) + value
+
+
+def inc_asset_granted_count(obj, value=1):
+    obj._granted_count = getattr(obj, '_asset_granted_count', 0) + value
+
+
+ADD = 'add'
+REMOVE = 'remove'
+
+
+def update_users_tree_for_add(users,
+                              nodes=(),
+                              assets=(),
+                              action=ADD):
+    """
+    `_granted_count` 授权次数，等于节点数或者资产数
+    """
+
+    # 查询授权`Asset`关联的 `Node`
+    asset_granted_nodes_qs = Node.objects.filter(
+        assets__in=assets
+    ).annotate(
+        _granted_count=Count('assets', distinct=True),
+        _asset_granted_count=Count('assets', distinct=True),
+    ).distinct()
+
+    asset_granted_nodes = []
+    for n in asset_granted_nodes_qs:
+        n._granted = False
+        asset_granted_nodes.append(n)
+
+    # 授权的 `Node`
+    for n in nodes:
+        inc_granted_count(n)
+        n._granted = True
+    # 资产授权节点与直接授权节点总共的祖先`key`，因为两者可能会重叠，所以字典的键复杂
+    ancestor_keys_map = {(n, n._granted): n.get_ancestor_keys() for n in chain(asset_granted_nodes, nodes)}
+    ancestors_map = {node.key: node for node in
+                     Node.objects.filter(key__in=chain(*ancestor_keys_map.values()))}
+    for (node, _), keys in ancestor_keys_map.items():
+        for key in keys:
+            ancestor = ancestors_map[key]  # TODO 404
+            inc_granted_count(ancestor, node._granted_count)
+    keys = ancestors_map.keys() | {n.key for n, _ in ancestor_keys_map.keys()}
+    # 资产授权节点和直接授权节点，两者会有重叠
+    all_nodes = [*nodes, *asset_granted_nodes, *ancestors_map.values()]
+
+    for user in users:
+        # 每个用户单独处理自己的树
+        to_create = {}
+        to_update = []
+        _granted_nodes = GrantedNode.objects.filter(key__in=keys, user=user)
+        granted_nodes_map = {gn.key: gn for gn in _granted_nodes}
+        for node in all_nodes:
+            _granted = getattr(node, '_granted', False)
+            _asset_granted_count = getattr(node, '_asset_granted_count', 0)
+            _granted_count = getattr(node, '_granted_count')
+            if node.key in granted_nodes_map:
+                granted_node = granted_nodes_map[node.key]
+                if action == ADD:
+                    if _granted:
+                        if granted_node.granted:
+                            #相同节点不能授权两次
+                            raise ValueError('')
+                        granted_node.granted = True
+
+                    inc_asset_granted_count(granted_node, _asset_granted_count)
+                    inc_granted_count(granted_node, _granted_count)
+                elif action == REMOVE:
+                    if _granted:
+                        if not granted_node.granted:
+                            # 数据有问题
+                            raise ValueError('')
+                        granted_node.granted = False
+                    inc_asset_granted_count(granted_node, -_asset_granted_count)
+                    inc_granted_count(granted_node, -_granted_count)
+
+                to_update.append(granted_node)
+            else:
+                if action == REMOVE:
+                    # 数据有问题
+                    raise ValueError('')
+                if node.key not in to_create:
+                    granted_node = GrantedNode(
+                        key=node.key,
+                        user=user,
+                        granted=_granted,
+                        granted_count=_granted_count,
+                        asset_granted_count=_asset_granted_count
+                    )
+                    to_create[node.key] = granted_node
+                else:
+                    granted_node = to_create[node.key]
+                    granted_node.granted_count += _granted_count
+                    granted_node.asset_granted_count += _asset_granted_count
+                    if _granted:
+                        if granted_node.granted:
+                            raise ValueError()
+                        granted_node.granted = True
+
+        for gn in to_update:
+            gn.granted_count = F('granted_count') + gn._granted_count
+            gn.asset_granted_count = F('asset_granted_count') + getattr(gn, '_asset_granted_count', 0)
+        GrantedNode.objects.bulk_update(to_update, ('granted', 'granted_count', 'asset_granted_count'))
+        GrantedNode.objects.bulk_create(to_create.values())
