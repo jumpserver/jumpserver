d6a3c360657a4d44c0070a5353ca57248bebd1bd
xinwen <coderWen@126.com>
2020-08-21 16:04:14 +0800

    s

--- apps/assets/models/node.py
+++ apps/assets/models/node.py
@@ -502,6 +502,9 @@ class Node(OrgModelMixin, SomeNodesMixin, TreeMixin, FamilyMixin, FullValueMixin
     value = models.CharField(max_length=128, verbose_name=_("Value"))
     child_mark = models.IntegerField(default=0)
     date_create = models.DateTimeField(auto_now_add=True)
+    parent_key = models.CharField(unique=True, max_length=64, verbose_name=_("Parent key"),
+                                  db_index=True, default='')
+    assets_amount = models.IntegerField(default=0)
 
     objects = OrgManager.from_queryset(NodeQuerySet)()
     is_node = True
