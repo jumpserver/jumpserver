45c609f4db48b56a4a7fbcedbb7dcf04f2cc1563
Bai <bugatti_it@163.com>
2020-09-07 20:33:44 +0800

    feat(ldap): 获取ldap用户列表，采用线程方式

--- apps/settings/tasks/ldap.py
+++ apps/settings/tasks/ldap.py
@@ -1,17 +1,14 @@
 # coding: utf-8
 # 
 
-from celery import shared_task
-
 from common.utils import get_logger
 from ..utils import LDAPSyncUtil
 
-__all__ = ['sync_ldap_user_task']
+__all__ = ['sync_ldap_user']
 
 
 logger = get_logger(__file__)
 
 
-@shared_task
-def sync_ldap_user_task():
+def sync_ldap_user():
     LDAPSyncUtil().perform_sync()
