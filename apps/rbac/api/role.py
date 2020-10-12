82bf5df4baa0d07f04a7926b3bed984ae5294744
ibuler <ibuler@qq.com>
2020-10-12 14:35:10 +0800

    perf(rbac): 优化rabc

--- apps/rbac/api/role.py
+++ apps/rbac/api/role.py
@@ -1,34 +1,26 @@
 from common.permissions import RBACPermission
 from rest_framework.viewsets import ModelViewSet
 
-from rbac.models import Role, RoleBinding
-from rbac.serializers import RoleSerializer, RoleBindingSerializer
+from ..models import Role, RoleBinding
+from ..serializers import RoleSerializer, RoleBindingSerializer
 
 
 __all__ = ['RoleViewSet', 'RoleBindingViewSet']
 
 
 class RoleViewSet(ModelViewSet):
-
     permission_classes = (RBACPermission,)
-
     filter_fields = ('name', 'type', 'is_build_in')
     search_fields = filter_fields
     ordering_fields = ('type',)
-
-    model = Role
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
 
 
 class RoleBindingViewSet(ModelViewSet):
-
     permission_classes = (RBACPermission,)
-
     filter_fields = ('user', 'role')
     search_fields = filter_fields
     ordering_fields = ('role',)
-
-    model = RoleBinding
     queryset = RoleBinding.objects.all()
     serializer_class = RoleBindingSerializer
