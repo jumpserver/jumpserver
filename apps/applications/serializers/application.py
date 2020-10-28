9c4eeb7c619786259c152ef6883d1b55d9e7399e
Bai <bugatti_it@163.com>
2020-10-28 12:44:19 +0800

    perf(perms): 添加应用/应用授权API的type_display/category_display字段

--- apps/applications/serializers/application.py
+++ apps/applications/serializers/application.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 #
 
+from rest_framework import serializers
 from orgs.mixins.serializers import BulkOrgResourceModelSerializer
 
 from .. import models
@@ -11,10 +12,13 @@ __all__ = [
 
 
 class ApplicationSerializer(BulkOrgResourceModelSerializer):
+    category_display = serializers.ReadOnlyField(source='get_category_display')
+    type_display = serializers.ReadOnlyField(source='get_type_display')
+
     class Meta:
         model = models.Application
         fields = [
-            'id', 'name', 'category', 'type', 'get_type_display', 'attrs',
+            'id', 'name', 'category', 'category_display', 'type', 'type_display', 'attrs',
             'domain', 'created_by', 'date_created', 'date_updated', 'comment'
         ]
         read_only_fields = [
