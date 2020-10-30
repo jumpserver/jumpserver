676ee93837aace3e9448c6c49fadbba3d9c95285
Bai <bugatti_it@163.com>
2020-10-30 10:29:47 +0800

    perf(perms): 优化方法名称；授权查询语句；

--- apps/perms/utils/application/user_permission.py
+++ apps/perms/utils/application/user_permission.py
@@ -1,21 +1,18 @@
+from django.db.models import Q
 from perms.models import ApplicationPermission
 from applications.models import Application
 
 
-def get_user_all_applicationpermission_ids(user):
-    application_perm_ids = set()
-    application_perm_ids.update(
-        ApplicationPermission.objects.valid().filter(users=user).distinct().values_list('id', flat=True)
-    )
-    application_perm_ids.update(
-        ApplicationPermission.objects.valid().filter(user_groups__users=user).distinct().values_list('id', flat=True)
-    )
+def get_user_all_applicationpermissions_id(user):
+    application_perm_ids = ApplicationPermission.objects.valid().filter(
+        Q(users=user) | Q(user_groups__users=user)
+    ).distinct().values_list('id', flat=True)
     return application_perm_ids
 
 
 def get_user_granted_all_applications(user):
-    application_perm_ids = get_user_all_applicationpermission_ids(user)
+    application_perms_id = get_user_all_applicationpermissions_id(user)
     applications = Application.objects.filter(
-        granted_by_permissions__id__in=application_perm_ids
+        granted_by_permissions__id__in=application_perms_id
     ).distinct()
     return applications
