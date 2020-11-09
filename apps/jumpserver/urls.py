38f912ceb903f0a246353d11c1d2b1e1479ebc8a
ibuler <ibuler@qq.com>
2020-11-09 18:02:26 +0800

    fix(i18n): 修复重置密码那的i18n问题

--- apps/jumpserver/urls.py
+++ apps/jumpserver/urls.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import JavaScriptCatalog
 
 from . import views, api
 
@@ -66,7 +68,11 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
             + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += js_i18n_patterns
+
+js_i18n_patterns = i18n_patterns(
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+)
+urlpatterns += js_i18n_patterns
 
 handler404 = 'jumpserver.views.handler404'
 handler500 = 'jumpserver.views.handler500'
