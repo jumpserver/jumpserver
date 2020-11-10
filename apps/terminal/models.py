a1ca9ce015a224a2777d31dee2afba972ec9f896
xinwen <coderWen@126.com>
2020-11-10 19:20:57 +0800

    fix(terminal): 扩展 Terminal name 长度

--- apps/terminal/models.py
+++ apps/terminal/models.py
@@ -25,7 +25,7 @@ from . import const
 
 class Terminal(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    name = models.CharField(max_length=32, verbose_name=_('Name'))
+    name = models.CharField(max_length=128, verbose_name=_('Name'))
     remote_addr = models.CharField(max_length=128, blank=True, verbose_name=_('Remote Address'))
     ssh_port = models.IntegerField(verbose_name=_('SSH Port'), default=2222)
     http_port = models.IntegerField(verbose_name=_('HTTP Port'), default=5000)
