31cd441a34e4823065eacc049e451a5569c669a8
ibuler <ibuler@qq.com>
2020-11-16 17:15:58 +0800

    fix(assets): 修复资产导入时填写节点引起的空节点名称的问题

--- apps/assets/models/node.py
+++ apps/assets/models/node.py
@@ -210,6 +210,7 @@ class FamilyMixin:
         if not full_value:
             return []
         nodes_family = full_value.split('/')
+        nodes_family = [v for v in nodes_family if v]
         org_root = cls.org_root()
         if nodes_family[0] == org_root.value:
             nodes_family = nodes_family[1:]
@@ -217,6 +218,7 @@ class FamilyMixin:
 
     @classmethod
     def create_nodes_recurse(cls, values, parent=None):
+        values = [v for v in values if v]
         if not values:
             return None
         if parent is None:
