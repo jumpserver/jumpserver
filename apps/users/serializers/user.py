8ced6f1168e2cc1125c50d38c24c1b1b75b81a6d
Bai <bugatti_it@163.com>
2020-12-11 15:40:39 +0800

    fix: 用户ProfileAPI设置is_first_login不是可读写

--- apps/users/serializers/user.py
+++ apps/users/serializers/user.py
@@ -237,6 +237,9 @@ class UserProfileSerializer(UserSerializer):
             'public_key_comment', 'public_key_hash_md5', 'admin_or_audit_orgs', 'current_org_roles',
             'guide_url', 'user_all_orgs'
         ]
+        read_only_fields = [
+            'date_joined', 'last_login', 'created_by', 'source'
+        ]
         extra_kwargs = dict(UserSerializer.Meta.extra_kwargs)
         extra_kwargs.update({
             'name': {'read_only': True, 'max_length': 128},
