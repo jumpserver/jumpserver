795d1b59e061d50412ef52b87f4e2a60a4918eec
huangchao <357831369@qq.com>
2020-12-27 23:00:45 +0800

    fix:资产自动推送UUIDD数组格式化字符串失败

--- apps/common/db/utils.py
+++ apps/common/db/utils.py
@@ -35,6 +35,6 @@ def get_objects(model, pks):
     if len(objs) != len(pks):
         pks = set(pks)
         exists_pks = {o.id for o in objs}
-        not_found_pks = ','.join(pks - exists_pks)
+        not_found_pks = pks - exists_pks
         logger.error(f'DoesNotExist: <{model.__name__}: {not_found_pks}>')
     return objs
