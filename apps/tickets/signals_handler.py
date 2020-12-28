ab4846dd4db24870aa95f4d1532f0b9856aba5e0
Bai <bugatti_it@163.com>
2020-12-28 17:03:44 +0800

    reactor: 重构工单模块26

--- apps/tickets/signals_handler.py
+++ apps/tickets/signals_handler.py
@@ -3,6 +3,7 @@
 from django.dispatch import receiver
 from django.db.models.signals import m2m_changed, post_save, pre_save
 
+from users.models import User
 from common.utils import get_logger
 from .models import Ticket, Comment
 from .utils import (
@@ -38,14 +39,21 @@ def on_ticket_processed(sender, instance=None, created=False, **kwargs):
 
 @receiver(m2m_changed, sender=Ticket.assignees.through)
 def on_ticket_assignees_changed(sender, instance=None, action=None, reverse=False, model=None, pk_set=None, **kwargs):
-    if action == 'post_add':
-        logger.debug('New ticket create, send mail: {}'.format(instance.title))
-        assignees = model.objects.filter(pk__in=pk_set)
-        send_ticket_applied_mail_to_assignees(instance, assignees)
-    if action.startswith('post') and not reverse:
-        assignees_display = [str(assignee) for assignee in instance.assignees.all()]
-        instance.assignees_display = ', '.join(assignees_display)
-        instance.save()
+    if reverse:
+        return
+    if action != 'post_add':
+        return
+    ticket = instance
+    assignees_display = [str(assignee) for assignee in ticket.assignees.all()]
+    logger.debug(
+        'Receives ticket and assignees changed signal, ticket: {}, assignees: {}'
+        ''.format(ticket.title, assignees_display)
+    )
+    ticket.assignees_display = ', '.join(assignees_display)
+    ticket.save()
+    logger.debug('Send applied email to assignees: {}'.format(assignees_display))
+    assignees = model.objects.filter(pk__in=pk_set)
+    send_ticket_applied_mail_to_assignees(ticket, assignees)
 
 
 @receiver(pre_save, sender=Comment)
