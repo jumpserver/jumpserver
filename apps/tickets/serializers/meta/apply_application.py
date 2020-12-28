828bd24a08c9faa8306a5343c2a3ca9e272629a6
Bai <bugatti_it@163.com>
2020-12-28 19:26:07 +0800

    reactor: 重构工单模块30

--- apps/tickets/serializers/meta/apply_application.py
+++ apps/tickets/serializers/meta/apply_application.py
@@ -1,11 +1,52 @@
+from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
 
-class TicketMetaApplyApplicationSerializer():
-    pass
-
-
-class TicketMetaApplyApplicationApplySerializer():
-    pass
+from .mixin import TicketMetaSerializerMixin
+from applications.models import Category
 
 
-class TicketMetaApplyApplicationApproveSerializer():
-    pass
+class TicketMetaApplyApplicationSerializer(TicketMetaSerializerMixin, serializers.Serializer):
+    # 申请信息
+    apply_category = serializers.ChoiceField(
+        choices=Category.choices, required=True, allow_blank=True, label=_('Category')
+    )
+    apply_type = serializers.ChoiceField(
+        choices=Category.get_all_type_choices(), required=True, allow_blank=True,
+        label=_('Category')
+    )
+    apply_application_group = serializers.ListField(
+        child=serializers.CharField(), allow_null=True, default=list, label=_('Application group')
+    )
+    apply_system_user_group = serializers.ListField(
+        child=serializers.CharField(), allow_null=True, default=list, label=_('System user group')
+    )
+    apply_date_start = serializers.DateTimeField(
+        allow_null=True, required=True, label=_('Date start')
+    )
+    apply_date_expired = serializers.DateTimeField(
+        allow_null=True, required=True, label=_('Date expired')
+    )
+
+    # 审批信息
+    approve_applications = serializers.ListField(
+        child=serializers.UUIDField(), required=True, allow_null=True,
+        label=_('Approve applications')
+    )
+    approve_system_users = serializers.ListField(
+        child=serializers.UUIDField(), required=True, allow_null=True,
+        label=_('Approve system users')
+    )
+    approve_date_start = serializers.DateTimeField(
+        required=True, allow_null=True, label=_('Date start')
+    )
+    approve_date_expired = serializers.DateTimeField(
+        required=True, allow_null=True, label=_('Date expired')
+    )
+
+
+class TicketMetaApplyApplicationApplySerializer(TicketMetaApplyApplicationSerializer):
+    need_fields_prefix = 'apply_'
+
+
+class TicketMetaApplyApplicationApproveSerializer(TicketMetaApplyApplicationSerializer):
+    need_fields_prefix = 'approve_'
