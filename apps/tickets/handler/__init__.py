b19bc8c1140ee96866c3b1eed397eefb832cff70
Bai <bugatti_it@163.com>
2021-01-13 16:05:46 +0800

    perf: 重构工单处理流程 (6)

--- apps/tickets/handler/__init__.py
+++ apps/tickets/handler/__init__.py
@@ -1,11 +1,7 @@
 from django.utils.module_loading import import_string
 
 
-def load_handler_class(path):
-    return import_string(path)
-
-
 def get_ticket_handler(ticket):
     handler_class_path = 'tickets.handler.{}.Handler'.format(ticket.type)
-    handler_class = load_handler_class(handler_class_path)
+    handler_class = import_string(handler_class_path)
     return handler_class(ticket=ticket)
