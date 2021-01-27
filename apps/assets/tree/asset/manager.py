dc62290eedaab70dd8a498735e86982efe3146ce
Bai <bugatti_it@163.com>
2021-01-28 01:46:39 +0800

    tree: v0.23

--- apps/assets/tree/asset/manager.py
+++ apps/assets/tree/asset/manager.py
@@ -12,8 +12,8 @@ class AssetTreeManager(object):
     """ 资产树管理器 """
 
     class ActionChoices(TextChoices):
-        initial = 'initial', 'Initial tree'
         refresh = 'refresh', 'Refresh tree'
+        destroy = 'destroy', 'Destroy tree'
 
     def __init__(self):
         self._org_tree_mapping = {}
@@ -21,14 +21,14 @@ class AssetTreeManager(object):
     def get_tree(self, org_id):
         _tree = self.__get_tree(org_id=org_id)
         if _tree is None:
-            _tree = self.__initial_tree(org_id=org_id)
-            self.__set_tree(org_id=org_id, tree=_tree)
+            _tree = self.refresh_tree(org_id=org_id)
         return _tree
 
     def refresh_tree(self, org_id):
         self.__destroy_tree(org_id)
         _tree = self.__initial_tree(org_id)
         self.__set_tree(_tree, org_id)
+        org_asset_tree_change.send(action=self.ActionChoices.refresh, org_id=org_id)
         return _tree
 
     def __set_tree(self, org_id, tree):
@@ -37,10 +37,10 @@ class AssetTreeManager(object):
     def __get_tree(self, org_id):
         return self._org_tree_mapping.get(org_id)
 
-    def __initial_tree(self, org_id):
+    @staticmethod
+    def __initial_tree(org_id):
         _tree = AssetTree(org_id=org_id)
         _tree.initial()
-        org_asset_tree_change.send(action=self.ActionChoices, org_id=org_id)
         return _tree
 
     def __destroy_tree(self, org_id):
