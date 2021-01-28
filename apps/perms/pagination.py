c4836c8028d1698009f2c4474064e759a837f86f
xinwen <coderWen@126.com>
2021-01-28 20:30:44 +0800

    增加 union 查询的支持

--- apps/perms/pagination.py
+++ apps/perms/pagination.py
@@ -12,16 +12,19 @@ class GrantedAssetLimitOffsetPagination(LimitOffsetPagination):
             self.limit_query_param,
             self.offset_query_param,
             'key', 'all', 'show_current_asset',
-            'cache_policy', 'display', 'draw'
+            'cache_policy', 'display', 'draw',
+            'order',
         }
         for k, v in self._request.query_params.items():
             if k not in exclude_query_params and v is not None:
+                logger.warn(f'Not hit node.assets_amount because find a unknow query_param `{k}` -> {self._request.get_full_path()}')
                 return super().get_count(queryset)
         node = getattr(self._view, 'pagination_node', None)
         if node:
-            logger.debug(f'{self._request.get_full_path()} hit node.assets_amount[{node.assets_amount}]')
+            logger.debug(f'Hit node.assets_amount[{node.assets_amount}] -> {self._request.get_full_path()}')
             return node.assets_amount
         else:
+            logger.warn(f'Not hit node.assets_amount[{node.assets_amount}] because {self._view} not has `pagination_node` -> {self._request.get_full_path()}')
             return super().get_count(queryset)
 
     def paginate_queryset(self, queryset, request: Request, view=None):
