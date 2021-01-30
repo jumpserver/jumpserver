4ded4c28313d68fd4d9530bc8798c7f4aa480cf3
xinwen <coderWen@126.com>
2021-01-30 20:24:45 +0800

    开始优化计算授权树节点资产数量不用冗余表

--- apps/perms/tree.py
+++ apps/perms/tree.py
@@ -1,25 +1,28 @@
 from typing import List
 from common.struct import Stack
 
-from perms.models import UserGrantedMappingNode
-from assets.tree import Tree
+from perms.models import UserAssetGrantedTreeNodeRelation
+from assets.tree import Tree, TreeNode
+
+
+NodeFrom = UserAssetGrantedTreeNodeRelation.NodeFrom
 
 
 class GrantedTree(Tree):
     def get_node_descendant(self, key, id_node_mapper):
-        mnode: UserGrantedMappingNode
-
         granted = []
         asset_granted = []
 
         def _group_node(id):
-            mnode = id_node_mapper[id]
-            if mnode.granted:
-                granted.append(mnode)
-            elif mnode.asset_granted:
-                asset_granted.append(mnode)
+            node: UserAssetGrantedTreeNodeRelation
+            node = id_node_mapper[id]
+            if node.node_from == NodeFrom.granted:
+                granted.append(node)
+            elif node.node_from == NodeFrom.asset:
+                asset_granted.append(node)
 
         stack = Stack()
+        node: TreeNode
         node = self.key_tree_node_mapper[key]
         stack.push(node)
 
