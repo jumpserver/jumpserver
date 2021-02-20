b03642847e78f92f5869b0adefa07a8be6c748e1
ibuler <ibuler@qq.com>
2021-02-20 15:46:13 +0800

    perf: 去掉 data_tree

--- apps/common/utils/common.py
+++ apps/common/utils/common.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 #
 import re
-import data_tree
 from collections import OrderedDict
 from itertools import chain
 import logging
@@ -11,8 +10,6 @@ from functools import wraps
 import time
 import ipaddress
 import psutil
-from django.utils.translation import ugettext_lazy as _
-from ..exceptions import JMSException
 
 
 UUID_PATTERN = re.compile(r'\w{8}(-\w{4}){3}-\w{12}')
