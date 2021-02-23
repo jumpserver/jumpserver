5af74a78edf6ce2611233cd874fe37106052de0b
ibuler <ibuler@qq.com>
2021-02-23 14:37:42 +0800

    feat: 为rdp 添加一个api

--- apps/perms/utils/application/permission.py
+++ apps/perms/utils/application/permission.py
@@ -7,8 +7,14 @@ logger = get_logger(__file__)
 
 
 def get_application_system_users_id(user, application):
-    queryset = ApplicationPermission.objects\
-        .filter(Q(users=user) | Q(user_groups__users=user), Q(applications=application))\
-        .valid()\
-        .values_list('system_users', flat=True)
+    queryset = ApplicationPermission.objects.valid()\
+        .filter(
+            Q(users=user) | Q(user_groups__users=user),
+            Q(applications=application)
+        ).values_list('system_users', flat=True)
     return queryset
+
+
+def has_application_system_permission(user, application, system_user):
+    system_users_id = get_application_system_users_id(user, application)
+    return system_user.id in system_users_id
