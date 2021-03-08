893182ce0ce53bc0026eac10ad27ac936cef8ea5
Bai <bugatti_it@163.com>
2021-03-08 10:49:51 +0800

    feature: acl (v0.4)

--- apps/acls/models/login.py
+++ apps/acls/models/login.py
@@ -5,15 +5,15 @@ from .base import BaseACL
 
 
 class LoginACL(BaseACL):
-
     class ActionChoices(models.TextChoices):
         reject = 'reject', _('Reject')
 
-    users = models.ManyToManyField('users.User', related_name='login_acl', verbose_name=_('User'))
-
+    # 条件
     ip_group = models.JSONField(default=list, verbose_name=_('Login IP'))
-
+    # 动作
     action = models.CharField(
         max_length=64, choices=ActionChoices.choices, default=ActionChoices.reject,
         verbose_name=_('Action')
     )
+    # 关联
+    users = models.ManyToManyField('users.User', related_name='login_acl', verbose_name=_('User'))
