c55e2db75e370d1fb1e4f336a467c9e16d6a169e
ibuler <ibuler@qq.com>
2021-03-17 17:08:49 +0800

    perf: 优化license判断

--- apps/jumpserver/utils.py
+++ apps/jumpserver/utils.py
@@ -2,6 +2,8 @@
 #
 from functools import partial
 from werkzeug.local import LocalProxy
+
+from django.conf import settings
 from common.local import thread_local
 
 
@@ -15,6 +17,13 @@ def _find(attr):
 
 def get_current_request():
     return _find('current_request')
+
+
+def has_valid_xpack_license():
+    if not settings.XPACK_ENABLED:
+        return False
+    from xpack.plugins.license.models import License
+    return License.has_valid_license()
 
 
 current_request = LocalProxy(partial(_find, 'current_request'))
