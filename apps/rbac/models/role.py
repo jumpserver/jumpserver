fd0f4a043fb7fcfdef7a5eb5abffbdd84cda97a2
Bai <bugatti_it@163.com>
2021-03-26 18:09:19 +0800

    feat: Add Role API 1.8

--- apps/rbac/models/role.py
+++ apps/rbac/models/role.py
@@ -2,23 +2,51 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from common.mixins.models import CommonModelMixin
 from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 
-__all__ = ['Role']
+__all__ = ['Role', 'RoleTypeChoices']
+
+
+class RoleTypeChoices(models.TextChoices):
+    safe = 'safe', _('Safe')
+
+    # Method for apps models mapping
+    @classmethod
+    def get_apps_models_mapping_for_safe(cls):
+        return {
+            'accounts': ['safe', 'account']
+        }
+
+    @classmethod
+    def get_permissions(cls, tp):
+        method_apps_models_mapping_for_tp = f'get_apps_models_mapping_for_{tp}'
+        assert hasattr(cls, method_apps_models_mapping_for_tp), (
+            f'Method`{method_apps_models_mapping_for_tp}` is not implemented'
+        )
+        apps_models_mapping = getattr(cls, method_apps_models_mapping_for_tp)()
+
+        apps_labels = list(apps_models_mapping.keys())
+        models_names = []
+        for _models_names in list(apps_models_mapping.values()):
+            models_names.extend(_models_names)
+
+        content_types_ids = ContentType.objects \
+            .filter(app_label__in=apps_labels, model__in=models_names) \
+            .values_list('id', flat=True)
+        permissions = Permission.objects.filter(content_type__in=content_types_ids)
+        return permissions
 
 
 class Role(CommonModelMixin):
     """ 角色: 相当于权限的集合 """
-    class TypeChoices(models.TextChoices):
-        safe = 'safe', _('Safe')
-
     # 内置角色:
     # - 账号(Safe)管理员: 拥有所有safe相关的权限
     display_name = models.CharField(max_length=256, verbose_name=_('Display name'))
     name = models.CharField(max_length=128, unique=True, verbose_name=_('Name'))
     # 角色类型: system / org / safe
     type = models.CharField(
-        choices=TypeChoices.choices, default=TypeChoices.safe, max_length=128,
+        choices=RoleTypeChoices.choices, default=RoleTypeChoices.safe, max_length=128,
         verbose_name=_('Type')
     )
     # 权限项
--- apps/rbac/serializers/role.py
+++ apps/rbac/serializers/role.py
@@ -1,7 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 from common.drf.serializers import BulkModelSerializer
-from .. import models
+from ..models import Role, RoleTypeChoices
 
 
 __all__ = ['RoleSerializer']
@@ -16,7 +16,7 @@ class RoleSerializer(BulkModelSerializer):
     )
 
     class Meta:
-        model = models.Role
+        model = Role
         fields = [
             'id', 'display_name', 'name', 'type', 'type_display', 'permissions',
             'permissions_display', 'is_builtin', 'comment', 'created_by', 'date_created',
@@ -26,5 +26,25 @@ class RoleSerializer(BulkModelSerializer):
             'created_by': {'read_only': True},
             'date_created': {'read_only': True},
             'date_updated': {'read_only': True},
-            'is_builtin': {'read_only': True}
+            'is_builtin': {'read_only': True},
+            'type': {'required': True},
         }
+
+    def validate_permissions(self, permissions):
+        if self.instance:
+            role_type = self.instance.type
+        else:
+            role_type = self.initial_data.get('type')
+
+        if role_type not in RoleTypeChoices.names:
+            raise serializers.ValidationError('The `type` is invalid: {}'.format(role_type))
+
+        permissions_ids = [permission.id for permission in permissions]
+        allowed_permissions_ids = RoleTypeChoices\
+            .get_permissions(role_type)\
+            .values_list('id', flat=True)
+        disallowed_permissions_ids = set(permissions_ids) - set(allowed_permissions_ids)
+        if disallowed_permissions_ids:
+            error = _('These permissions are not allowed: {}'.format(disallowed_permissions_ids))
+            raise serializers.ValidationError(error)
+        return permissions
