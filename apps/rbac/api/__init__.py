3f8ffdfb587946d407d3ce414280e6d26fd68ae7
Bai <bugatti_it@163.com>
2021-03-26 11:04:04 +0800

    feat: Add API 1.4

---
+++ apps/rbac/api/__init__.py
@@ -0,0 +1,2 @@
+from .role import *
+from .role_binding import *
--- apps/rbac/models/role_binding.py
+++ apps/rbac/models/role_binding.py
@@ -2,34 +2,11 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-__all__ = ['SystemRoleBinding', 'OrgRoleBinding', 'SafeRoleBinding']
-
-
-class SystemRoleBinding(models.Model):
-    """ 用户-系统角色绑定 """
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name=_('User'))
-    # only system-role
-    role = models.ForeignKey('rbac.Role', on_delete=models.PROTECT, verbose_name=_('Role'))
-
-    class Meta:
-        unique_together = ('user', 'role')
-
-
-class OrgRoleBinding(models.Model):
-    """ 用户-组织角色绑定 """
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name=_('User'))
-    # only org-role
-    role = models.ForeignKey('rbac.Role', on_delete=models.PROTECT, verbose_name=_('Role'))
-    org = models.ForeignKey(
-        'orgs.Organization', on_delete=models.PROTECT, verbose_name=_('Organization')
-    )
-
-    class Meta:
-        unique_together = ('user', 'role', 'org')
+__all__ = ['SafeRoleBinding']
 
 
 class SafeRoleBinding(models.Model):
-    """ 用户-Safe(保险箱)角色绑定 """
+    """ 用户-保险箱级别的角色绑定 """
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name=_('User'))
     role = models.ForeignKey('rbac.Role', on_delete=models.PROTECT, verbose_name=_('Role'))
     # only safe-role
