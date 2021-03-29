a5179d159667d3c0036d2dad17f5db0d25367a24
xinwen <coderWen@126.com>
2021-03-29 12:01:24 +0800

    feat: 增加 es 忽略 https 证书验证

--- apps/terminal/serializers/storage.py
+++ apps/terminal/serializers/storage.py
@@ -181,7 +181,10 @@ class CommandStorageTypeESSerializer(serializers.Serializer):
         max_length=1024, default='jumpserver', label=_('Index'), allow_null=True
     )
     DOC_TYPE = ReadableHiddenField(default='command', label=_('Doc type'), allow_null=True)
-
+    ignore_verify_certs = serializers.BooleanField(
+        default=False, label=_('Ignore Certificate Verification'),
+        source='OTHER.ignore_verify_certs', allow_null=True,
+    )
 
 # mapping
 
