fb7de7eaaa10240472cd327cf22ed667cbad55a4
Bai <bugatti_it@163.com>
2021-04-07 18:11:06 +0800

    feat: Refactor SafeRolePermission  4.3

--- apps/accounts/api/mixins.py
+++ apps/accounts/api/mixins.py
@@ -2,31 +2,8 @@ from rbac.permissions import SafeRolePermission
 from rbac.models import SafeRoleBinding
 
 
-class SafeViewSetMixin(object):
+class ViewSetMixin:
     model = None
-    permission_classes = (SafeRolePermission, )
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = self.filter_permission_safes(queryset)
-        return queryset
-
-    def filter_permission_safes(self, queryset):
-        safes = self.get_user_permission_safes()
-        queryset = queryset.filter(safe__in=safes)
-        return queryset
-
-    def get_user_permission_safes(self):
-        permission_safes = []
-        safes = SafeRoleBinding.get_user_safes(user=self.request.user)
-        for safe in safes:
-            has_permission = SafeRolePermission.check_user_permission(
-                self.request.user, safe, model=self.model, view_action=self.action
-            )
-            if not has_permission:
-                continue
-            permission_safes.append(safe)
-        return permission_safes
 
     def perform_create(self, serializer):
         objs = self.get_to_create_objects(serializer)
@@ -63,4 +40,30 @@ class SafeViewSetMixin(object):
             objs = serializer.instance.filter(**{'{}__in'.format(id_attr): objs_ids})
         return objs
 
+
+class SafeViewSetMixin(ViewSetMixin):
+    model = None
+    permission_classes = (SafeRolePermission, )
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = self.filter_permission_safes(queryset)
+        return queryset
+
+    def filter_permission_safes(self, queryset):
+        safes = self.get_user_permission_safes()
+        queryset = queryset.filter(safe__in=safes)
+        return queryset
+
+    def get_user_permission_safes(self):
+        permission_safes = []
+        safes = SafeRoleBinding.get_user_safes(user=self.request.user)
+        for safe in safes:
+            has_permission = SafeRolePermission.check_user_permission(
+                self.request.user, safe, model=self.model, view_action=self.action
+            )
+            if not has_permission:
+                continue
+            permission_safes.append(safe)
+        return permission_safes
 
