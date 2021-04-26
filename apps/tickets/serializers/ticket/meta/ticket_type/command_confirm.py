3eceeb8f6c8de147350456c5e22636b26937918f
Bai <bugatti_it@163.com>
2021-04-26 18:52:25 +0800

    feat: 添加命令复核逻辑; 添加命令复核工单; 2

--- apps/tickets/serializers/ticket/meta/ticket_type/command_confirm.py
+++ apps/tickets/serializers/ticket/meta/ticket_type/command_confirm.py
@@ -15,8 +15,8 @@ class ApplySerializer(serializers.Serializer):
         required=True, max_length=64, label=_('Run system user')
     )
     apply_run_command = serializers.CharField(required=True, label=_('Run command'))
-    apply_from_session = serializers.UUIDField(required=False, label=_('From session'))
-    apply_from_cmd_filter_rule = serializers.UUIDField(
+    apply_from_session_id = serializers.UUIDField(required=False, label=_('From session'))
+    apply_from_cmd_filter_rule_id = serializers.UUIDField(
         required=False, label=_('From cmd filter rule')
     )
 
