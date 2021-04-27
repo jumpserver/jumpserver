3e94e788ec9c47d3929d0fa8d4aa7b9439127843
xinwen <coderWen@126.com>
2021-04-27 14:21:48 +0800

    fix: 添加启动失效缓存

---
+++ apps/common/management/commands/expire_caches.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from assets.signals_handler.node_assets_mapping import expire_node_assets_mapping_for_memory
+from orgs.models import Organization
+
+
+def expire_node_assets_mapping():
+    org_ids = Organization.objects.all().values_list('id', flat=True)
+    org_ids = [*org_ids, '00000000-0000-0000-0000-000000000000']
+
+    for org_id in org_ids:
+        print(f'--> Expire node assets mapping: org_id={org_id}')
+        expire_node_assets_mapping_for_memory(org_id)
+
+
+class Command(BaseCommand):
+    help = 'Expire caches'
+
+    def handle(self, *args, **options):
+        expire_node_assets_mapping()
