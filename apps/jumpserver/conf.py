926fd7e68cdafd04b1f12505250b01a7469a017e
ibuler <ibuler@qq.com>
2021-05-20 15:55:15 +0800

    fix: 修复周期监测任务配置的bug

--- apps/jumpserver/conf.py
+++ apps/jumpserver/conf.py
@@ -279,7 +279,7 @@ class Config(dict):
         'WINDOWS_SSH_DEFAULT_SHELL': 'cmd',
         'FLOWER_URL': "127.0.0.1:5555",
         'DEFAULT_ORG_SHOW_ALL_USERS': True,
-        'PERIOD_TASK_ENABLE': True,
+        'PERIOD_TASK_ENABLED': True,
         'FORCE_SCRIPT_NAME': '',
         'LOGIN_CONFIRM_ENABLE': False,
         'WINDOWS_SKIP_ALL_MANUAL_PASSWORD': False,
