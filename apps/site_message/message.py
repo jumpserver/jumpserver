3000e5406b4d3740291c02bc3265839c8d9936bb
xinwen <coderWen@126.com>
2021-05-24 18:09:51 +0800

    重构了一些

--- apps/site_message/message.py
+++ apps/site_message/message.py
@@ -1,97 +1,79 @@
-from django.db.models import Q
-
 from common.utils.timezone import now
 from users.models import User
 from .models import SiteMessage, SiteMessageUsers
 
 
-def send_msg(subject, message, user_ids=None, group_ids=None, sender=None, is_broadcast=False):
-    if not any((user_ids, group_ids, is_broadcast)):
-        raise ValueError('No recipient is specified')
+class Message:
 
-    site_msg = SiteMessage.objects.create(
-        subject=subject, message=message,
-        is_broadcast=is_broadcast, sender=sender
-    )
+    @classmethod
+    def send_msg(cls, subject, message, user_ids=(), group_ids=(), sender=None, is_broadcast=False):
+        if not any((user_ids, group_ids, is_broadcast)):
+            raise ValueError('No recipient is specified')
 
-    if not is_broadcast:
-        if user_ids:
-            site_msg.users.add(user_ids)
-        if group_ids:
-            site_msg.groups.add(group_ids)
+        site_msg = SiteMessage.objects.create(
+            subject=subject, message=message,
+            is_broadcast=is_broadcast, sender=sender
+        )
 
+        if is_broadcast:
+            user_ids = User.objects.all().values_list('id', flat=True)
+        else:
+            if group_ids:
+                site_msg.groups.add(group_ids)
 
-def _associate_msgs(user_id):
-    group_ids = User.get_group_ids_by_user_id(user_id)
-    site_msg_ids = SiteMessage.objects.filter(
-        Q(is_broadcast=True) | Q(groups__id__in=group_ids)
-    ).filter(users__id__isnull=True).values_list(id, flat=True)
+                user_ids_from_group = User.groups.through.objects.filter(
+                    usergroup_id__in=group_ids
+                ).values_list('user_id', flat=True)
 
-    if not site_msg_ids:
-        return
+                user_ids = [*user_ids, *user_ids_from_group]
 
-    to_created = []
-    for msg_id in site_msg_ids:
-        to_created.append(SiteMessageUsers(
-            sitemessage_id=msg_id,
-            user_id=user_id
-        ))
-    SiteMessageUsers.objects.bulk_create(to_created)
+        site_msg.users.add(user_ids)
 
+    @classmethod
+    def get_user_all_msgs(cls, user_id):
+        site_msgs = SiteMessage.objects.filter(
+            m2m_sitemessageusers__user_id=user_id
+        ).distinct().annotate(
+            has_read='m2m_sitemessageusers__has_read',
+            read_at='m2m_sitemessageusers__read_at'
+        ).order_by('-date_created')
 
-def get_user_all_msgs(user_id):
-    _associate_msgs(user_id)
+        return site_msgs
 
-    site_msgs = SiteMessage.objects.filter(
-        m2m_sitemessageusers__user_id=user_id
-    ).distinct().annotate(
-        has_read='m2m_sitemessageusers__has_read',
-        read_at='m2m_sitemessageusers__read_at'
-    ).order_by('-date_created')
+    @classmethod
+    def get_user_all_msgs_count(cls, user_id):
+        site_msgs_count = SiteMessage.objects.filter(
+            m2m_sitemessageusers__user_id=user_id
+        ).distinct().count()
+        return site_msgs_count
 
-    return site_msgs
+    @classmethod
+    def get_user_unread_msgs(cls, user_id):
+        site_msgs = SiteMessage.objects.filter(
+            m2m_sitemessageusers__user_id=user_id,
+            m2m_sitemessageusers__has_read=False
+        ).distinct().annotate(
+            has_read='m2m_sitemessageusers__has_read',
+            read_at='m2m_sitemessageusers__read_at'
+        ).order_by('-date_created')
 
+        return site_msgs
 
-def get_user_all_msgs_count(user_id):
-    _associate_msgs(user_id)
+    @classmethod
+    def get_user_unread_msgs_count(cls, user_id):
+        site_msgs_count = SiteMessage.objects.filter(
+            m2m_sitemessageusers__user_id=user_id,
+            m2m_sitemessageusers__has_read=False
+        ).distinct().count()
+        return site_msgs_count
 
-    site_msgs_count = SiteMessage.objects.filter(
-        m2m_sitemessageusers__user_id=user_id
-    ).distinct().count()
-    return site_msgs_count
+    @classmethod
+    def mark_msgs_as_read(cls, user_id, msg_ids):
+        sitemsg_user = SiteMessageUsers.objects.filter(
+            user_id=user_id, sitemessage_id__in=msg_ids
+        )
 
-
-def get_user_unread_msgs(user_id):
-    _associate_msgs(user_id)
-
-    site_msgs = SiteMessage.objects.filter(
-        m2m_sitemessageusers__user_id=user_id,
-        m2m_sitemessageusers__has_read=False
-    ).distinct().annotate(
-        has_read='m2m_sitemessageusers__has_read',
-        read_at='m2m_sitemessageusers__read_at'
-    ).order_by('-date_created')
-
-    return site_msgs
-
-
-def get_user_unread_msgs_count(user_id):
-    _associate_msgs(user_id)
-
-    site_msgs_count = SiteMessage.objects.filter(
-        m2m_sitemessageusers__user_id=user_id,
-        m2m_sitemessageusers__has_read=False
-    ).distinct().count()
-    return site_msgs_count
-
-
-def mark_msg_as_read(user_id, msg_id):
-    sitemsg_user = SiteMessageUsers.objects.filter(user_id=user_id, sitemessage_id=msg_id).first()
-
-    if not sitemsg_user:
-        raise SiteMessage.DoesNotExist
-
-    if not sitemsg_user.has_read:
-        sitemsg_user.has_read = True
-        sitemsg_user.read_at = now()
-        sitemsg_user.save()
+        if not sitemsg_user.has_read:
+            sitemsg_user.has_read = True
+            sitemsg_user.read_at = now()
+            sitemsg_user.save()
