cc0438ba27c6f66ee1d180b8f34da3a507778c72
xinwen <coderWen@126.com>
2021-05-26 10:58:34 +0800

    s

--- apps/site_message/urls.py
+++ apps/site_message/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from . import api
 
-app_name = 'site-message'
+app_name = 'site_message'
 
 router = DefaultRouter()
 router.register('site-message', api.SiteMessageViewSet, 'site-message')
