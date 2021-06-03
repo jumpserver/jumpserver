daca197425d944b13ad46c35ec5687c62af50926
xinwen <coderWen@126.com>
2021-06-03 18:09:26 +0800

    添加gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,4 @@
-from apps.jumpserver.const import CONFIG
+from jumpserver.const import CONFIG
 
 bind = '{}:{}'.format(
     CONFIG.HTTP_BIND_HOST or '127.0.0.1',
@@ -8,3 +8,18 @@ bind = '{}:{}'.format(
 worker_class = 'gthread'
 
 threads = 10
+
+# https://docs.gunicorn.org/en/latest/settings.html#workers
+workers = 4
+
+# https://docs.gunicorn.org/en/latest/settings.html#max-requests
+max_requests = 4096
+
+# https://docs.gunicorn.org/en/latest/settings.html#access-log-format
+access_log_format = '%(h)s %(t)s %(L)ss "%(r)s" %(s)s %(b)s '
+
+# https://docs.gunicorn.org/en/latest/settings.html#accesslog
+accesslog = '-'
+
+# https://docs.gunicorn.org/en/latest/settings.html#reload
+reload = CONFIG.DEBUG
