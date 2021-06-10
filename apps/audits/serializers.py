66de791f71d32ec3f7ec1b76a133759681cfe12b
ibuler <ibuler@qq.com>
2021-06-10 18:02:55 +0800

    perf(auth): 授权token形式登录，支持记录登录日志

--- apps/audits/serializers.py
+++ apps/audits/serializers.py
@@ -82,7 +82,7 @@ class CommandExecutionSerializer(serializers.ModelSerializer):
         model = CommandExecution
         fields_mini = ['id']
         fields_small = fields_mini + [
-            'run_as', 'command', 'user', 'is_finished',
+            'run_as', 'command', 'is_finished', 'user',
             'date_start', 'result', 'is_success', 'org_id'
         ]
         fields = fields_small + ['hosts', 'hosts_display', 'run_as_display', 'user_display']
