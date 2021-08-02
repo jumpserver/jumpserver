74136ae64b6a1503b053638c4eb7805736cb7965
Bai <bugatti_it@163.com>
2021-08-02 14:26:38 +0800

    feat: 添加management包

---
+++ apps/jumpserver/management/commands/start.py
@@ -0,0 +1,38 @@
+from django.db.models import TextChoices
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Services(TextChoices):
+    gunicorn = 'gunicorn', 'gunicorn'
+    daphne = 'daphne', 'daphne'
+    celery_ansible = 'celery_ansible', 'celery_ansible'
+    celery_default = 'celery_default', 'celery_default'
+    beat = 'beat', 'beat'
+    flower = 'flower', 'flower'
+
+    @property
+    def web(self):
+        return [self.gunicorn.value, self.daphne.value]
+
+    @property
+    def celery(self):
+        return [self.celery_ansible.value, self.celery_default.value]
+
+    @property
+    def task(self):
+        return self.celery + [self.beat.value]
+
+    @property
+    def all(self):
+        return self.__class__.values
+
+
+class Command(BaseCommand):
+    help = 'Start services'
+
+    def add_arguments(self, parser):
+        parser.add_argument('service', choices=Services.all, type=str, help='Service name')
+
+    def handle(self, *args, **options):
+        service_name = ''
+        pass
