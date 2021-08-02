8ad87637bad59ed957c4f0b80a585fa4fd994db1
jiangweidong <weidong.jiang@fit2cloud.com>
2021-08-02 14:45:43 +0800

    改密计划支持数据库(mysql、oracle、postgre)改密

--- apps/assets/signals_handler/authbook.py
+++ apps/assets/signals_handler/authbook.py
@@ -39,4 +39,7 @@ def on_authbook_pre_create(sender, instance, **kwargs):
     # 升级版本号
     instance.version = instance.history.all().count() + 1
     # 即使在 root 组织也不怕
-    instance.org_id = instance.asset.org_id
+    if instance.database:
+        instance.org_id = instance.database.org_id
+    else:
+        instance.org_id = instance.asset.org_id
