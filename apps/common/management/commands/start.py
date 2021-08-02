10fc6ce4cf847fd9bb3ecb5df50e4bec50b889ae
Bai <bugatti_it@163.com>
2021-08-02 19:54:37 +0800

    feat: 添加 start 模块

--- apps/common/management/commands/start.py
+++ apps/common/management/commands/start.py
@@ -9,29 +9,60 @@ class Services(TextChoices):
     celery_default = 'celery_default', 'celery_default'
     beat = 'beat', 'beat'
     flower = 'flower', 'flower'
+    web = 'web', 'web'
+    celery = 'celery', 'celery'
+    task = 'task', 'task'
+    all = 'all', 'all'
 
-    @property
-    def web(self):
-        return [self.gunicorn.value, self.daphne.value]
+    @classmethod
+    def web_services(cls):
+        return [cls.gunicorn, cls.daphne]
 
-    @property
-    def celery(self):
-        return [self.celery_ansible.value, self.celery_default.value]
+    @classmethod
+    def celery_services(cls):
+        return [cls.celery_ansible, cls.celery_default]
 
-    @property
-    def task(self):
-        return self.celery + [self.beat.value]
+    @classmethod
+    def task_services(cls):
+        return cls.celery_services() + [cls.beat]
 
-    @property
-    def all(self):
-        return self.__class__.values
+    @classmethod
+    def all_services(cls):
+        return cls.web_services() + cls.task_services()
+
+    @classmethod
+    def get_services(cls, names):
+        services = set()
+        for name in names:
+            method_name = f'{name}_services'
+            if hasattr(cls, method_name):
+                _services = getattr(cls, method_name)()
+            elif hasattr(cls, name):
+                _services = [getattr(cls, name)]
+            else:
+                continue
+            services.update(set(_services))
+        return services
+
+
+class ServicesUtil(object):
+
+    @classmethod
+    def start_services(cls, services):
+        pass
 
 
 class Command(BaseCommand):
     help = 'Start services'
 
     def add_arguments(self, parser):
-        parser.add_argument('service', choices=Services.all, type=str, help='Service name')
+        parser.add_argument(
+            'services',  nargs='+', choices=Services.values, default=Services.all, help='Service'
+        )
 
     def handle(self, *args, **options):
+        service_names = options.get('services')
+        services = Services.get_services(service_names)
+        ServicesUtil.start_services(services)
         print('>>>>>>', options)
+        print('>>>>>>', services)
