50a1eb133a31e4909a4cbc279e28fc1178bd5d59
feng626 <1304903146@qq.com>
2021-08-17 18:50:15 +0800

    fix: 修复user筛选用户bug

--- apps/users/filters.py
+++ apps/users/filters.py
@@ -1,11 +1,14 @@
+from django_filters import rest_framework as filters
+from django.db.models import Q
 from rest_framework.compat import coreapi, coreschema
-from rest_framework import filters
+from rest_framework.filters import BaseFilterBackend
 
+from common.drf.filters import BaseFilterSet
 from users.models.user import User
 from orgs.utils import current_org
 
 
-class OrgRoleUserFilterBackend(filters.BaseFilterBackend):
+class OrgRoleUserFilterBackend(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         org_role = request.query_params.get('org_role')
         if not org_role:
@@ -30,3 +33,16 @@ class OrgRoleUserFilterBackend(filters.BaseFilterBackend):
                 )
             )
         ]
+
+
+class UserFilter(BaseFilterSet):
+    role = filters.CharFilter(method='filter_role')
+
+    class Meta:
+        model = User
+        fields = (
+            'id', 'username', 'email', 'name', 'source', 'role'
+        )
+
+    def filter_role(self, queryset, name, value):
+        return queryset.filter(Q(role=value) | Q(m2m_org_members__role=value))
