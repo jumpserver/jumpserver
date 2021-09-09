654ec4970ef7b1c61a625f34ca37f1a9d16635c3
xinwen <coderWen@126.com>
2021-09-09 21:11:57 +0800

    更改设置接口

--- apps/settings/serializers/auth/sms.py
+++ apps/settings/serializers/auth/sms.py
@@ -8,7 +8,12 @@ __all__ = ['SMSSettingSerializer', 'AlibabaSMSSettingSerializer', 'TencentSMSSet
 
 class SMSSettingSerializer(serializers.Serializer):
     SMS_ENABLED = serializers.BooleanField(default=False, label=_('Enable SMS'))
-    SMS_BACKEND = serializers.ChoiceField(choices=BACKENDS.choices, default=BACKENDS.ALIBABA)
+    SMS_BACKEND = serializers.ChoiceField(choices=BACKENDS.choices, default=BACKENDS.ALIBABA, label=_('SMS provider'))
+
+
+class SignAndTmplPairSerializer(serializers.Serializer):
+    sign_name = serializers.CharField(max_length=256, required=True, label=_('Signature'))
+    template_code = serializers.CharField(max_length=256, required=True, label=_('Template'))
 
 
 class BaseSMSSettingSerializer(serializers.Serializer):
@@ -20,20 +25,16 @@ class BaseSMSSettingSerializer(serializers.Serializer):
         return data
 
 
+class SignAndTmplSerializer(serializers.Serializer):
+    verification_code = SignAndTmplPairSerializer(default={'sign_name': '', 'template_code': ''})
+
+
 class AlibabaSMSSettingSerializer(BaseSMSSettingSerializer):
     ALIBABA_ACCESS_KEY_ID = serializers.CharField(max_length=256, required=True, label='AccessKeyId')
     ALIBABA_ACCESS_KEY_SECRET = serializers.CharField(
         max_length=256, required=False, label='AccessKeySecret', write_only=True)
-    ALIBABA_SMS_SIGN_AND_TEMPLATES = serializers.DictField(
-        label=_('Signatures and Templates'), required=True, help_text=_('''
-        Filling in JSON Data: 
-        {
-            "verification_code": {
-                "sign_name": "<Your signature name>", 
-                "template_code": "<Your template code>"
-            }
-        }
-        ''')
+    ALIBABA_SMS_SIGN_AND_TEMPLATES = SignAndTmplSerializer(
+        label=_('Signatures and Templates'), required=True
     )
 
 
@@ -41,13 +42,6 @@ class TencentSMSSettingSerializer(BaseSMSSettingSerializer):
     TENCENT_SECRET_ID = serializers.CharField(max_length=256, required=True, label='Secret id')
     TENCENT_SECRET_KEY = serializers.CharField(max_length=256, required=False, label='Secret key', write_only=True)
     TENCENT_SDKAPPID = serializers.CharField(max_length=256, required=True, label='SDK app id')
-    TENCENT_SMS_SIGN_AND_TEMPLATES = serializers.DictField(
-        label=_('Signatures and Templates'), required=True, help_text=_('''
-        Filling in JSON Data: 
-        {
-            "verification_code": {
-                "sign_name": "<Your signature name>", 
-                "template_code": "<Your template code>"
-            }
-        }
-        '''))
+    TENCENT_SMS_SIGN_AND_TEMPLATES = SignAndTmplSerializer(
+        label=_('Signatures and Templates'), required=True
+    )
