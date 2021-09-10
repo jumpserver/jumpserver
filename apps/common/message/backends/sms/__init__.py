3e737c8cb8c221f761d45c4c6415a42170fab2ba
xinwen <coderWen@126.com>
2021-09-10 14:39:02 +0800

    fix: 修复短信问题

--- apps/common/message/backends/sms/__init__.py
+++ apps/common/message/backends/sms/__init__.py
@@ -67,6 +67,12 @@ class SMS:
     client: BaseSMSClient
 
     def __init__(self, backend=None):
+        backend = backend or settings.SMS_BACKEND
+        if backend not in BACKENDS:
+            raise JMSException(
+                code='sms_provider_not_support',
+                detail=_('SMS provider not support: {}').format(backend)
+            )
         m = importlib.import_module(f'.{backend or settings.SMS_BACKEND}', __package__)
         self.client = m.client.new_from_settings()
 
