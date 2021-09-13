f024346a94a87ae2b5427ca76546a37cfcc3983a
Michael Bai <baijiangjie@gmail.com>
2021-09-13 13:47:34 +0800

    perf: 取消API Token的配置

--- apps/settings/serializers/auth/base.py
+++ apps/settings/serializers/auth/base.py
@@ -17,9 +17,9 @@ class AuthSettingSerializer(serializers.Serializer):
     FORGOT_PASSWORD_URL = serializers.CharField(
         required=False, max_length=1024, label=_("Forgot password url")
     )
-    HEALTH_CHECK_TOKEN = serializers.CharField(
-        required=False, max_length=1024, label=_("Health check token")
-    )
+    # HEALTH_CHECK_TOKEN = serializers.CharField(
+    #     required=False, max_length=1024, label=_("Health check token")
+    # )
     LOGIN_REDIRECT_MSG_ENABLED = serializers.BooleanField(
         required=False, label=_("Enable login redirect msg")
     )
