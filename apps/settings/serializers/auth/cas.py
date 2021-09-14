73dc44e361cb4adc8c910d056ec506aec314e4d3
Michael Bai <baijiangjie@gmail.com>
2021-09-14 16:38:47 +0800

    fix: 修改CAS配置项和CAS登录失败问题

--- apps/settings/serializers/auth/cas.py
+++ apps/settings/serializers/auth/cas.py
@@ -1,4 +1,4 @@
-
+import json
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
@@ -10,6 +10,7 @@ __all__ = [
 class CASSettingSerializer(serializers.Serializer):
     AUTH_CAS = serializers.BooleanField(required=False, label=_('Enable CAS Auth'))
     CAS_SERVER_URL = serializers.CharField(required=False, max_length=1024, label=_('Server url'))
+    CAS_ROOT_PROXIED_AS = serializers.CharField(required=False, max_length=1024, label=_('Proxy server url'))
     CAS_LOGOUT_COMPLETELY = serializers.BooleanField(required=False, label=_('Logout completely'))
     CAS_VERSION = serializers.IntegerField(required=False, label=_('Version'))
     CAS_USERNAME_ATTRIBUTE = serializers.CharField(required=False, max_length=1024, label=_('Username attr'))
