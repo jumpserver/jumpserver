c861a57c77688079bf73017654eeb3da8e5ad83b
ibuler <ibuler@qq.com>
2021-10-13 17:44:02 +0800

    perf: 修改ip城市获取算法
    perf: 优化使用lfs

--- apps/common/utils/__init__.py
+++ apps/common/utils/__init__.py
@@ -5,8 +5,8 @@ from .common import *
 from .django import *
 from .encode import *
 from .http import *
-from .ipip import *
 from .crypto import *
 from .random import *
 from .jumpserver import *
 from .ip import *
+from .geoip import *
