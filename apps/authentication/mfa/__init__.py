973e84f2932e8d63b510e9c98f95d55d11ac2eae
ibuler <ibuler@qq.com>
2021-11-09 10:17:49 +0800

    perf: remove init function

--- apps/authentication/mfa/__init__.py
+++ apps/authentication/mfa/__init__.py
@@ -4,17 +4,3 @@ from .radius import MFARadius
 
 
 MFA_BACKENDS = [MFAOtp, MFASms, MFARadius]
-
-
-def get_enabled_mfa_backends():
-    backends = [cls for cls in MFA_BACKENDS if cls.enabled()]
-    backends_enabled = [backend for backend in backends if backend.enabled()]
-    return backends_enabled
-
-
-def get_user_set_mfa_backends(user):
-    backends = get_enabled_mfa_backends()
-    backends_instance = [cls(user) for cls in backends]
-    backends_set = [backend.__class__ for backend in backends_instance if backend.has_set()]
-    return backends_set
-
