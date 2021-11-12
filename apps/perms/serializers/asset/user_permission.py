e9dc1ad86a8af50761dd5fddc36a7d91690e295e
Eric <xplzv@126.com>
2021-11-12 14:49:25 +0800

    fix: 修复 ssh 工具连接无法二级登录的问题

--- apps/perms/serializers/asset/user_permission.py
+++ apps/perms/serializers/asset/user_permission.py
@@ -28,7 +28,7 @@ class AssetSystemUserSerializer(serializers.ModelSerializer):
         model = SystemUser
         only_fields = (
             'id', 'name', 'username', 'priority', 'protocol', 'login_mode',
-            'sftp_root', 'username_same_with_user',
+            'sftp_root', 'username_same_with_user', 'su_enabled', 'su_from',
         )
         fields = list(only_fields) + ["actions"]
         read_only_fields = fields
