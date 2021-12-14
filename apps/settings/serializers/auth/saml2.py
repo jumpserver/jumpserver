0c2e5f3f2a55994b190fea7bb28ef502f8fbf97f
ibuler <ibuler@qq.com>
2021-12-14 17:57:39 +0800

    perf: 优化 saml2 i18n

--- apps/settings/serializers/auth/saml2.py
+++ apps/settings/serializers/auth/saml2.py
@@ -12,21 +12,21 @@ class SAML2SettingSerializer(serializers.Serializer):
         default=False, required=False, label=_('Enable SAML2 Auth')
     )
     SAML2_IDP_METADATA_URL = serializers.URLField(
-        allow_blank=True, required=False, label=_('IDP Metadata URL')
+        allow_blank=True, required=False, label=_('IDP metadata URL')
     )
     SAML2_IDP_METADATA_XML = serializers.CharField(
-        allow_blank=True, required=False, label=_('IDP Metadata XML')
+        allow_blank=True, required=False, label=_('IDP metadata XML')
     )
     SAML2_SP_ADVANCED_SETTINGS = serializers.JSONField(
-        required=False, label=_('SP ADVANCED SETTINGS')
+        required=False, label=_('SP advanced settings')
     )
     SAML2_SP_KEY_CONTENT = serializers.CharField(
         allow_blank=True, required=False,
-        write_only=True, label=_('SP Private Key')
+        write_only=True, label=_('SP private key')
     )
     SAML2_SP_CERT_CONTENT = serializers.CharField(
         allow_blank=True, required=False,
-        write_only=True, label=_('SP Public Cert')
+        write_only=True, label=_('SP cert')
     )
     SAML2_RENAME_ATTRIBUTES = serializers.DictField(required=False, label=_('Rename attr'))
     SAML2_LOGOUT_COMPLETELY = serializers.BooleanField(required=False, label=_('Logout completely'))
