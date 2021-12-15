b113eeb1d695e81d868b3106d26303573ba31014
ibuler <ibuler@qq.com>
2021-12-15 18:44:51 +0800

    perf: 修复基本设置中的 base_site 可能带 / 的问题

--- apps/settings/serializers/basic.py
+++ apps/settings/serializers/basic.py
@@ -44,3 +44,10 @@ class BasicSettingSerializer(serializers.Serializer):
     TICKETS_ENABLED = serializers.BooleanField(required=False, default=True, label=_("Enable tickets"))
     ANNOUNCEMENT_ENABLED = serializers.BooleanField(label=_('Enable announcement'), default=True)
     ANNOUNCEMENT = AnnouncementSerializer(label=_("Announcement"))
+
+    @staticmethod
+    def validate_SITE_URL(s):
+        if not s:
+            return 'http://127.0.0.1'
+        return s.strip('/')
+
