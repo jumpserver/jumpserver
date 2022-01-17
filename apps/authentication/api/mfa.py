efb855e9aee4b1b5541c07e2932f56ed307491db
Jiangjie.Bai <32935519+BaiJiangJie@users.noreply.github.com>
2022-01-17 19:02:46 +0800

    Update mfa.py

--- apps/authentication/api/mfa.py
+++ apps/authentication/api/mfa.py
@@ -62,8 +62,6 @@ class MFASendCodeApi(AuthMixin, CreateAPIView):
         username = serializer.validated_data.get('username', '')
         mfa_type = serializer.validated_data['type']
 
-        self.ip = self.get_request_ip()
-        self.check_mfa_is_block(username, self.ip)
         if not username:
             user = self.get_user_from_session()
         else:
