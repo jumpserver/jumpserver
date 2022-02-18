d7e15af835bc93cf6987e4cdf5941c84e654f701
ibuler <ibuler@qq.com>
2022-02-18 18:12:47 +0800

    perf: 修改 migrations role permissions

--- apps/rbac/signal_handlers.py
+++ apps/rbac/signal_handlers.py
@@ -1,13 +1,14 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_migrate
+from django.apps import apps
 
 from .builtin import BuiltinRole
 
 
 @receiver(post_migrate)
 def after_migrate_update_builtin_role_permissions(sender, app_config, **kwargs):
-    # FBI WARNING: 为啥是 simple_history, 因为 simple_history 是最后一个 app,
-    # 保证在最后一个 app 后执行, 并且不会相互依赖
-    if app_config.name == 'simple_history':
+    # 最后一个 app migrations 后执行, 更新内置角色的权限
+    last_app = list(apps.get_app_configs())[-1]
+    if app_config.name == last_app.name:
         print("After migration, update builtin role permissions")
         BuiltinRole.sync_to_db()
