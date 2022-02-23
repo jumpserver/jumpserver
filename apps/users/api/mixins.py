6a644268f624101dd23ece25d9e7f1dd872135d0
feng626 <1304903146@qq.com>
2022-02-23 18:11:28 +0800

    fix: 修复用户列表过滤bug

--- apps/users/api/mixins.py
+++ apps/users/api/mixins.py
@@ -5,5 +5,5 @@ from users.models import User
 
 class UserQuerysetMixin:
     def get_queryset(self):
-        queryset = User.get_org_users()
+        queryset = User.get_org_users().filter(is_service_account=False)
         return queryset
