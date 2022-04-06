f00ed45680ca42966caa9ab96d618c644cde3796
Jiangjie.Bai <bugatti_it@163.com>
2022-04-06 17:04:45 +0800

    feat: modify protocols field

--- apps/terminal/models/endpoint.py
+++ apps/terminal/models/endpoint.py
@@ -1,117 +1,21 @@
 from django.db import models
-from urllib.parse import urlparse
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinValueValidator, MaxValueValidator
 from common.db.models import JMSModel
-from django.conf import settings
-
-
-class ProtocolChoices(models.TextChoices):
-    http = 'http', 'HTTP'
-    https = 'https', 'HTTPS'
-    ssh = 'ssh', 'SSH'
-    rdp = 'rdp', 'RDP'
-    mysql = 'mysql', 'MySQL'
-    mariadb = 'mariadb', 'MariaDB'
-    postgresql = 'postgresql', 'PostgreSQL'
-    # oracle = 'oracle', 'Oracle'
-    # sqlserver = 'sqlserver', 'SQLServer'
-    # redis = 'redis', 'Redis'
-    # mongodb = 'mongodb', 'MongoDB'
-
-    @property
-    def default_port(self):
-        default_port_mapper: dict = {
-            self.http: 80,
-            self.https: 8443,
-            self.ssh: 22,
-            self.rdp: 3389,
-            self.mysql: 3306,
-            self.mariadb: 3306,
-            self.postgresql: 5432,
-            # self.oracle: 1521,
-            # self.sqlserver: 1433,
-            # self.redis: 6379,
-            # self.mongodb: 27017
-        }
-        assert self.name in default_port_mapper, 'No support protocol: {}'.format(self.name)
-        return default_port_mapper[self.name]
-
-
-class EndpointProtocol(models.Model):
-    name = models.CharField(
-        max_length=64, choices=ProtocolChoices.choices, null=False, blank=False,
-        verbose_name=_('Name')
-    )
-    port = models.IntegerField(
-        null=False, blank=False, verbose_name=_('Port'),
-        validators=[MinValueValidator(1), MaxValueValidator(65535)]
-    )
-    enabled = models.BooleanField(default=True, verbose_name=_('Enabled'))
-    builtin = models.BooleanField(default=False, verbose_name=_('Builtin'))
-
-    class Meta:
-        verbose_name = _('Endpoint protocol')
-        unique_together = (('name', 'port', 'enabled'),)
-        ordering = ('name',)
-
-    def __str__(self):
-        builtin = ' [built-in]' if self.builtin else ''
-        enabled = ' [Enabled]' if self.enabled else ''
-        return f'{self.name}/{self.port}{builtin}{enabled}'
-
-    @classmethod
-    def get_merged_protocols_with_default(cls, protocols, default_enabled=True):
-        protocol_names = protocols.values_list('name', flat=True)
-        default_protocols = cls.get_or_create_default_protocols(enabled=default_enabled)
-        to_default_protocol_ids = [p.id for p in default_protocols if p.name not in protocol_names]
-        protocol_ids = list(protocols.values_list('id', flat=True))
-        protocol_ids.extend(to_default_protocol_ids)
-        return cls.objects.filter(id__in=protocol_ids)
-
-    @classmethod
-    def get_or_create_default_protocols(cls, name=None, enabled=None):
-        if name is None:
-            protocols = ProtocolChoices
-        else:
-            assert name in ProtocolChoices.names, (
-                _('No support protocol: {}, Options: {}').format(name, ProtocolChoices.names)
-            )
-            protocols = [getattr(ProtocolChoices, name)]
-        data = []
-        for p in protocols:
-            d = {'name': p.name, 'port': p.default_port, 'builtin': True}
-            if enabled is None or enabled is True:
-                d.update({'enabled': True})
-                data.append(d)
-            elif enabled is None or enabled is False:
-                d.update({'enabled': False})
-                data.append(d)
-            else:
-                continue
-        return cls.get_or_create_protocols(data)
-
-    @classmethod
-    def get_or_create_protocols(cls, data):
-        # TODO: data is queryset
-        protocol_ids = []
-        for d in data:
-            protocol = cls.get_or_create_protocol(d)
-            protocol_ids.append(protocol.id)
-        return cls.objects.filter(id__in=protocol_ids)
-
-    @classmethod
-    def get_or_create_protocol(cls, data):
-        protocol, created = cls.objects.get_or_create(**data, defaults=data)
-        return protocol
+from common.fields.model import PortField
 
 
 class Endpoint(JMSModel):
-    name = models.CharField(max_length=128, verbose_name=_('Name'), unique=True)
+    name = models.CharField(max_length=256, verbose_name=_('Name'), unique=True)
     host = models.CharField(max_length=256, null=True, blank=True, verbose_name=_('Host'))
-    protocols = models.ManyToManyField(
-        'terminal.EndpointProtocol', related_name='endpoint', verbose_name=_('ProtocolChoices')
-    )
+    # disabled value=0
+    https_port = PortField(default=8443, verbose_name=_('HTTPS Port'))
+    http_port = PortField(default=80, verbose_name=_('HTTP Port'))
+    ssh_port = PortField(default=22, verbose_name=_('SSH Port'))
+    rdp_port = PortField(default=3389, verbose_name=_('RDP Port'))
+    mysql_port = PortField(default=3306, verbose_name=_('MySQL Port'))
+    mariadb_port = PortField(default=3306, verbose_name=_('MariaDB Port'))
+    postgresql_port = PortField(default=5432, verbose_name=_('PostgreSQL Port'))
     comment = models.TextField(default='', blank=True, verbose_name=_('Comment'))
 
     class Meta:
@@ -121,18 +25,9 @@ class Endpoint(JMSModel):
     def __str__(self):
         return self.name
 
-    def reset_protocols_to_default(self, only_nonexistent=True, enabled=True):
-        default_protocols = EndpointProtocol.get_or_create_default_protocols(enabled=enabled)
-        if not only_nonexistent:
-            self.protocols.set(default_protocols)
-
-        current_protocols = self.protocols.all().values_list('name', flat=True)
-        to_protocols = [p for p in default_protocols if p.name not in current_protocols]
-        self.protocols.set(to_protocols)
-
 
 class EndpointRule(JMSModel):
-    name = models.CharField(max_length=128, verbose_name=_('Name'), unique=True)
+    name = models.CharField(max_length=256, verbose_name=_('Name'), unique=True)
     ip_group = models.TextField(default='', blank=True, verbose_name=_('IP group'))
     priority = models.IntegerField(
         verbose_name=_("Priority"), validators=[MinValueValidator(1), MaxValueValidator(100)],
--- apps/terminal/serializers/endpoint.py
+++ apps/terminal/serializers/endpoint.py
@@ -1,38 +1,24 @@
-from rest_framework import serializers
 from django.utils.translation import ugettext_lazy as _
 from common.drf.serializers import BulkModelSerializer
-from ..models import Endpoint, EndpointRule, EndpointProtocol
+from ..models import Endpoint, EndpointRule
 
 __all__ = ['EndpointSerializer', 'EndpointRuleSerializer']
 
 
-class ProtocolSerializer(serializers.ModelSerializer):
-    name_display = serializers.ReadOnlyField(source='get_name_display', label=_('Name'))
-
-    class Meta:
-        model = EndpointProtocol
-        fields = ['name', 'name_display', 'port', 'enabled']
-
-
 class EndpointSerializer(BulkModelSerializer):
-    protocols_group = serializers.ManyRelatedField(
-        source='protocols', child_relation=ProtocolSerializer(), label=_('Protocol'),
-        default=EndpointProtocol.get_or_create_default_protocols(enabled=True)
-    )
 
     class Meta:
         model = Endpoint
         fields_mini = ['id', 'name']
-        fields_m2m = ['protocols_group']
-        fields = fields_mini + fields_m2m + [
-            'date_created', 'date_updated', 'created_by'
+        fields_small = [
+            'host',
+            'http_port', 'https_port', 'ssh_port',
+            'rdp_port', 'mysql_port', 'mariadb_port',
+            'postgresql_port',
         ]
-
-    @staticmethod
-    def validate_protocols_group(protocols):
-        protocols = EndpointProtocol.get_or_create_protocols(protocols)
-        protocols = EndpointProtocol.get_merged_protocols_with_default(protocols)
-        return protocols
+        fields = fields_mini + fields_small + [
+            'comment', 'date_created', 'date_updated', 'created_by'
+        ]
 
 
 class EndpointRuleSerializer(BulkModelSerializer):
@@ -42,5 +28,5 @@ class EndpointRuleSerializer(BulkModelSerializer):
         fields_small = fields_mini + ['ip_group', 'priority']
         fields_fk = ['endpoint']
         fields = fields_mini + fields_small + fields_fk + [
-            'date_created', 'date_updated', 'created_by'
+            'comment', 'date_created', 'date_updated', 'created_by'
         ]
