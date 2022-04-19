832228e1849aef41c51572f1e75dba844d94040b
ibuler <ibuler@qq.com>
2022-04-19 15:30:56 +0800

    stash

--- apps/assets/models/asset/host.py
+++ apps/assets/models/asset/host.py
@@ -1,15 +1,12 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
+# from django.db import models
+# from django.utils.translation import gettext_lazy as _
 
 from assets.const import Category
 from .common import Asset
-from .device_info import DeviceInfo
+# from .device_info import DeviceInfo
 
 
 class Host(Asset):
-    device_info = models.ForeignKey(DeviceInfo, on_delete=models.SET_NULL,
-                                    null=True, verbose_name=_("Device info"))
-
     def save(self, *args, **kwargs):
         self.category = Category.HOST
         return super().save(*args, **kwargs)
