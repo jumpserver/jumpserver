a8f2a69a53fd1139e5dd64af05f99a5c2b1d5609
ibuler <ibuler@qq.com>
2022-06-14 17:36:18 +0800

    pref: 修改使用 fk

--- apps/jumpserver/rewriting/db.py
+++ apps/jumpserver/rewriting/db.py
@@ -1,12 +1,8 @@
 import os
+import sys
 
 from django.db import models, transaction
-from django.utils.translation import gettext_lazy as _
 from django.db.transaction import atomic as db_atomic
-from django.db.models.fields.related import OneToOneRel
-from django.db.models.fields.related_descriptors import (
-    ReverseOneToOneDescriptor, ForwardOneToOneDescriptor
-)
 
 
 def atomic(using=None, savepoint=False):
@@ -19,50 +15,22 @@ class ForeignKey(models.ForeignKey):
         super().__init__(*args, **kwargs)
 
 
-class OneToOneField(ForeignKey):
-    # Field flags
-    many_to_many = False
-    many_to_one = False
-    one_to_many = False
-    one_to_one = True
-
-    related_accessor_class = ReverseOneToOneDescriptor
-    forward_related_accessor_class = ForwardOneToOneDescriptor
-    rel_class = OneToOneRel
-
-    description = _("One-to-one relationship")
-
-    def __init__(self, to, on_delete, to_field=None, **kwargs):
+class OneToOneField(models.OneToOneField, ForeignKey):
+    def __init__(self, *args, **kwargs):
         kwargs['unique'] = False
-        super().__init__(to, on_delete, to_field=to_field, **kwargs)
-
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
-        if "unique" in kwargs:
-            del kwargs['unique']
-        return name, path, args, kwargs
-
-    def formfield(self, **kwargs):
-        if self.remote_field.parent_link:
-            return None
-        return super().formfield(**kwargs)
-
-    def save_form_data(self, instance, data):
-        if isinstance(data, self.remote_field.model):
-            setattr(instance, self.name, data)
-        else:
-            setattr(instance, self.attname, data)
-            # Remote field object must be cleared otherwise Model.save()
-            # will reassign attname using the related object pk.
-            if data is None:
-                setattr(instance, self.name, data)
-
-    def _check_unique(self, **kwargs):
-        # Override ForeignKey since check isn't applicable here.
-        return []
+        kwargs['db_constraint'] = False
+        ForeignKey.__init__(self, *args, **kwargs)
 
 
-if os.getenv('FK_CONSTRAINT', '1') == '0':
+def set_db_constraint_false():
+    if os.getenv('FK_CONSTRAINT', '1') != '0':
+        return
+    if sys.argv == 2 and sys.argv[1] == 'makemigrations':
+        return
+    print("Set foreignkey db constraint False")
     transaction.atomic = atomic
     models.ForeignKey = ForeignKey
     models.OneToOneField = OneToOneField
+
+
+set_db_constraint_false()
