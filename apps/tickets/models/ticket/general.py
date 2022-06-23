6dcd4344a03ee6e25a51ab225ec46b884939922c
ibuler <ibuler@qq.com>
2022-06-23 11:54:30 +0800

    perf: ticket migrations

--- apps/tickets/models/ticket/general.py
+++ apps/tickets/models/ticket/general.py
@@ -11,6 +11,7 @@ from django.db.models.fields import related
 from common.exceptions import JMSException
 from common.utils.timezone import as_current_tz
 from common.mixins.models import CommonModelMixin
+from common.db.encoder import ModelJSONFieldEncoder
 from orgs.models import Organization
 from tickets.const import (
     TicketType, TicketStatus, TicketState,
@@ -283,7 +284,7 @@ class Ticket(StatusMixin, CommonModelMixin):
     )
     serial_num = models.CharField(_('Serial number'), max_length=128, unique=True, null=True)
     rel_snapshot = models.JSONField(verbose_name=_('Relation snapshot'), default=dict)
-    meta = models.JSONField(default=dict)
+    meta = models.JSONField(encoder=ModelJSONFieldEncoder, default=dict, verbose_name=_("Meta"))
     org_id = models.CharField(
         max_length=36, blank=True, default='', verbose_name=_('Organization'), db_index=True
     )
