f61ea340dc116dc18164191931fc9a86f81c952f
halo <wuyihuangw@gmail.com>
2022-07-28 15:33:24 +0800

    perf: 消息返回

--- apps/settings/urls/ws_urls.py
+++ apps/settings/urls/ws_urls.py
@@ -5,6 +5,5 @@ from .. import ws
 app_name = 'common'
 
 urlpatterns = [
-    path('ws/setting/ping/', ws.PingMsgWebsocket.as_asgi(), name='setting-ping-ws'),
-    path('ws/setting/telnet/', ws.TelnetMsgWebsocket.as_asgi(), name='setting-telnet-ws'),
+    path('ws/setting/tools/', ws.ToolsWebsocket.as_asgi(), name='setting-tools-ws'),
 ]
--- apps/settings/ws.py
+++ apps/settings/ws.py
@@ -2,17 +2,17 @@
 #
 
 import json
-import socket
 
 from channels.generic.websocket import JsonWebsocketConsumer
 
-from .utils import ping, telnet
+from common.db.utils import close_old_connections
 from common.utils import get_logger
+from .utils import ping, telnet
 
 logger = get_logger(__name__)
 
 
-class PingMsgWebsocket(JsonWebsocketConsumer):
+class ToolsWebsocket(JsonWebsocketConsumer):
 
     def connect(self):
         user = self.scope["user"]
@@ -21,59 +21,57 @@ class PingMsgWebsocket(JsonWebsocketConsumer):
         else:
             self.close()
 
-    def imitate_ping(self, dest_addr, timeout=2, count=5, psize=64):
+    def imitate_ping(self, dest_addr, timeout=3, count=5, psize=64):
         """
         Send `count' ping with `psize' size to `dest_addr' with
         the given `timeout' and display the result.
         """
-        logger.debug('receive request ping %s' % dest_addr)
+        logger.info('receive request ping {}'.format(dest_addr))
+        self.send_json({'msg': 'Trying {0}...\r\n'.format(dest_addr)})
         for i in range(count):
-            msg = 'ping %s with ...' % dest_addr
+            msg = 'ping {0} with ...{1}\r\n'
             try:
                 delay = ping(dest_addr, timeout, psize)
             except Exception as e:
-                msg += 'failed. (socket error: %s)' % str(e)
-                self.send_json({'ping': msg})
+                msg = msg.format(dest_addr, 'failed. (socket error: {})'.format(str(e)))
+                logger.error(msg)
+                self.send_json({'msg': msg})
                 break
             if delay is None:
-                msg += 'failed. (timeout within %ssec.)' % timeout
+                msg = msg.format(dest_addr, 'failed. (timeout within {}sec.)'.format(timeout))
             else:
                 delay = delay * 1000
-                msg += 'get ping in %0.4fms' % delay
-            self.send_json({'ping': msg})
-        self.close()
+                msg = msg.format(dest_addr, 'get ping in %0.4fms' % delay)
+            self.send_json({'msg': msg})
 
-    def receive(self, text_data=None, bytes_data=None, **kwargs):
-        data = json.loads(text_data)
-        destination_ip = data.get('destination_ip')
-        self.imitate_ping(destination_ip)
-
-
-class TelnetMsgWebsocket(JsonWebsocketConsumer):
-
-    def connect(self):
-        user = self.scope["user"]
-        if user.is_authenticated:
-            self.accept()
-        else:
-            self.close()
-
-    def imitate_telnet(self, dest_addr, port_number=23, timeout=10):
-        logger.debug('receive request telnet %s' % dest_addr)
-        self.send_json({'telnet': 'Trying {0} {1}...'.format(dest_addr, port_number)})
+    def imitate_telnet(self, dest_addr, port_num=23, timeout=10):
+        logger.info('receive request telnet {}'.format(dest_addr))
+        self.send_json({'msg': 'Trying {0} {1}...\r\n'.format(dest_addr, port_num)})
+        msg = 'Telnet: {}'
         try:
-            is_connective, msg = telnet(dest_addr, port_number, timeout)
+            is_connective, resp = telnet(dest_addr, port_num, timeout)
             if is_connective:
-                self.send_json({'telnet': 'Telnet: Connected to {0} {1}\n{2}'.format(dest_addr, port_number, msg)})
+                msg = msg.format('Connected to {0} {1}\r\n{2}'.format(dest_addr, port_num, resp))
             else:
-                self.send_json({'telnet': 'Telnet: Connect to {0} {1} : {2}'.format(dest_addr, port_number, msg)})
-                self.send_json({'telnet': 'Telnet: Unable to connect to remote host'})
+                msg = msg.format('Connect to {0} {1} {2}\r\nTelnet: Unable to connect to remote host'
+                                 .format(dest_addr, port_num, resp))
         except Exception as e:
-            self.send_json({'telnet': 'Telnet: {0}'.format(str(e))})
-        self.close()
+            logger.error(msg)
+            msg = msg.format(str(e))
+        finally:
+            self.send_json({'msg': msg})
 
     def receive(self, text_data=None, bytes_data=None, **kwargs):
         data = json.loads(text_data)
-        destination_ip = data.get('destination_ip')
-        port_number = data.get('port_number')
-        self.imitate_telnet(destination_ip, port_number)
+        tool_type = data.get('tool_type', 'Ping')
+        dest_addr = data.get('dest_addr')
+        if tool_type == 'Ping':
+            self.imitate_ping(dest_addr)
+        else:
+            port_num = data.get('port_num')
+            self.imitate_telnet(dest_addr, port_num)
+        self.close()
+
+    def disconnect(self, code):
+        self.close()
+        close_old_connections()
