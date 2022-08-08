193281df8de2b0e4de4b3491a09503600fb0649d
feng626 <1304903146@qq.com>
2022-08-08 11:28:38 +0800

    fix: 修复用户自动登录bug

--- apps/jumpserver/conf.py
+++ apps/jumpserver/conf.py
@@ -160,7 +160,7 @@ class Config(dict):
         'SESSION_COOKIE_DOMAIN': None,
         'CSRF_COOKIE_DOMAIN': None,
         'SESSION_COOKIE_NAME_PREFIX': None,
-        'SESSION_COOKIE_AGE': 3600,
+        'SESSION_COOKIE_AGE': 3600 * 24,
         'SESSION_EXPIRE_AT_BROWSER_CLOSE': False,
         'LOGIN_URL': reverse_lazy('authentication:login'),
         'CONNECTION_TOKEN_EXPIRATION': 5 * 60,
