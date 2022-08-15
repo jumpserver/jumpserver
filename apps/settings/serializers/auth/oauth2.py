d75b77a5d18f326e3560e3f6629e5dff86f992c4
Jiangjie.Bai <bugatti_it@163.com>
2022-08-15 16:52:45 +0800

    fix: 修改 OAuth2.0 认证的字段的必填项

--- apps/settings/serializers/auth/oauth2.py
+++ apps/settings/serializers/auth/oauth2.py
@@ -17,39 +17,39 @@ class SettingImageField(serializers.ImageField):
 
 class OAuth2SettingSerializer(serializers.Serializer):
     AUTH_OAUTH2 = serializers.BooleanField(
-        default=False, required=False, label=_('Enable OAuth2 Auth')
+        default=False, label=_('Enable OAuth2 Auth')
     )
     AUTH_OAUTH2_LOGO_PATH = SettingImageField(
         allow_null=True, required=False, label=_('Logo')
     )
     AUTH_OAUTH2_PROVIDER = serializers.CharField(
-        required=False, max_length=16, label=_('Service provider')
+        required=True, max_length=16, label=_('Service provider')
     )
     AUTH_OAUTH2_CLIENT_ID = serializers.CharField(
-        required=False, max_length=1024, label=_('Client Id')
+        required=True, max_length=1024, label=_('Client Id')
     )
     AUTH_OAUTH2_CLIENT_SECRET = EncryptedField(
         required=False, max_length=1024, label=_('Client Secret')
     )
     AUTH_OAUTH2_SCOPE = serializers.CharField(
-        required=False, max_length=1024, label=_('Scope'), allow_blank=True
+        required=True, max_length=1024, label=_('Scope'), allow_blank=True
     )
     AUTH_OAUTH2_PROVIDER_AUTHORIZATION_ENDPOINT = serializers.CharField(
-        required=False, max_length=1024, label=_('Provider auth endpoint')
+        required=True, max_length=1024, label=_('Provider auth endpoint')
     )
     AUTH_OAUTH2_ACCESS_TOKEN_ENDPOINT = serializers.CharField(
-        required=False, max_length=1024, label=_('Provider token endpoint')
+        required=True, max_length=1024, label=_('Provider token endpoint')
     )
     AUTH_OAUTH2_ACCESS_TOKEN_METHOD = serializers.ChoiceField(
         default='GET', label=_('Client authentication method'),
         choices=(('GET', 'GET'), ('POST', 'POST'))
     )
     AUTH_OAUTH2_PROVIDER_USERINFO_ENDPOINT = serializers.CharField(
-        required=False, max_length=1024, label=_('Provider userinfo endpoint')
+        required=True, max_length=1024, label=_('Provider userinfo endpoint')
     )
     AUTH_OAUTH2_USER_ATTR_MAP = serializers.DictField(
-        required=False, label=_('User attr map')
+        required=True, label=_('User attr map')
     )
     AUTH_OAUTH2_ALWAYS_UPDATE_USER = serializers.BooleanField(
-        required=False, label=_('Always update user')
+        default=True, label=_('Always update user')
     )
