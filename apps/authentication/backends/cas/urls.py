68841d1f15e7877b50877d8394d8cfb7ec7b51da
fit2bot <68588906+fit2bot@users.noreply.github.com>
2022-08-16 17:24:58 +0800

    fix: 配置仅已存在用户登录后 cas用户首次登录报403 (#8752)
    Co-authored-by: feng626 <1304903146@qq.com>

--- apps/authentication/backends/cas/urls.py
+++ apps/authentication/backends/cas/urls.py
@@ -3,9 +3,10 @@
 from django.urls import path
 import django_cas_ng.views
 
+from .views import CASLoginView
 
 urlpatterns = [
-    path('login/', django_cas_ng.views.LoginView.as_view(), name='cas-login'),
+    path('login/', CASLoginView.as_view(), name='cas-login'),
     path('logout/', django_cas_ng.views.LogoutView.as_view(), name='cas-logout'),
     path('callback/', django_cas_ng.views.CallbackView.as_view(), name='cas-proxy-callback'),
 ]
