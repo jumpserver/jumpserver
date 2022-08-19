cd5d860fb46d24a73b8fb78d6ac6299e399719a7
吴小白 <296015668@qq.com>
2022-08-19 14:21:40 +0800

    fix: 添加 openssh-client 依赖包

--- requirements/deb_pkg.sh
+++ requirements/deb_pkg.sh
@@ -2,4 +2,4 @@
 apt install \
     g++ make iputils-ping default-libmysqlclient-dev libpq-dev \
     libffi-dev libldap2-dev libsasl2-dev sshpass pkg-config libxml2-dev \
-    libxmlsec1-dev libxmlsec1-openssl libaio-dev freetds-dev
+    libxmlsec1-dev libxmlsec1-openssl libaio-dev openssh-client freetds-dev
