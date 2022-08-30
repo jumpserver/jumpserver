28541c48f7233ea3f2f417a5b5735b1839581006
feng626 <1304903146@qq.com>
2022-08-30 12:50:01 +0800

    修复迁移文件

--- apps/applications/migrations/0022_auto_20220817_1346.py
+++ apps/applications/migrations/0022_auto_20220817_1346.py
@@ -3,13 +3,22 @@
 from django.db import migrations
 
 
-class Migration(migrations.Migration):
+def migrate_db_oracle_version_to_attrs(apps, schema_editor):
+    db_alias = schema_editor.connection.alias
+    model = apps.get_model("applications", "Application")
+    oracles = list(model.objects.using(db_alias).filter(type='oracle'))
+    for o in oracles:
+        o.attrs['version'] = '12c'
+    model.objects.using(db_alias).bulk_update(oracles, ['attrs'])
 
+
+class Migration(migrations.Migration):
     dependencies = [
         ('applications', '0021_auto_20220629_1826'),
     ]
 
     operations = [
+        migrations.RunPython(migrate_db_oracle_version_to_attrs),
         migrations.AlterUniqueTogether(
             name='account',
             unique_together=None,
