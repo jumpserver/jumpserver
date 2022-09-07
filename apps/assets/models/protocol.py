746c6e424253b917d747af8bcec649eab390fd0e
Jiangjie.Bai <bugatti_it@163.com>
2022-09-07 18:33:34 +0800

    fix: 修改资产、授权创建时的小问题

--- apps/assets/models/protocol.py
+++ apps/assets/models/protocol.py
@@ -5,7 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class Protocol(models.Model):
     name = models.CharField(max_length=32, verbose_name=_("Name"))
     port = models.IntegerField(verbose_name=_("Port"))
-
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
-        pass
