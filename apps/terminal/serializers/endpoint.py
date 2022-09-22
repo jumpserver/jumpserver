92fa23b502ebee25acb6187e53a4e22417b99fa2
Jiangjie.Bai <bugatti_it@163.com>
2022-09-22 17:23:17 +0800

    feat: 修改 DBPortManager 处理逻辑

--- apps/terminal/serializers/endpoint.py
+++ apps/terminal/serializers/endpoint.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.utils.translation import ugettext_lazy as _
 from common.drf.serializers import BulkModelSerializer
 from acls.serializers.rules import ip_group_child_validator, ip_group_help_text
-from django.conf import settings
+from applications.utils import db_port_manager
 from ..models import Endpoint, EndpointRule
 
 __all__ = ['EndpointSerializer', 'EndpointRuleSerializer']
@@ -11,7 +11,13 @@ __all__ = ['EndpointSerializer', 'EndpointRuleSerializer']
 class EndpointSerializer(BulkModelSerializer):
     # 解决 luna 处理繁琐的问题, 返回 magnus 监听的当前 db 的 port
     magnus_listen_db_port = serializers.SerializerMethodField(label=_('Magnus listen db port'))
-    magnus_listen_port_range = serializers.SerializerMethodField(label=_('Magnus Listen port range'))
+    magnus_listen_port_range = serializers.CharField(
+        max_length=128, default=db_port_manager.magnus_listen_port_range, read_only=True,
+        label=_('Magnus Listen port range'),
+        help_text=_(
+            'The range of ports that Magnus listens on is modified in the configuration file'
+        )
+    )
 
     class Meta:
         model = Endpoint
@@ -19,6 +25,7 @@ class EndpointSerializer(BulkModelSerializer):
         fields_small = [
             'host',
             'https_port', 'http_port', 'ssh_port', 'rdp_port',
+            'magnus_listen_db_port', 'magnus_listen_port_range',
         ]
         fields = fields_mini + fields_small + [
             'comment', 'date_created', 'date_updated', 'created_by'
@@ -35,13 +42,6 @@ class EndpointSerializer(BulkModelSerializer):
         if not view or view.action not in ['smart']:
             return 0
         return obj.get_port(view.target_instance, view.target_protocol)
-
-    @staticmethod
-    def get_magnus_listen_port_range(obj: Endpoint):
-        port_start = settings.MAGNUS_DB_PORTS_START
-        port_limit = settings.MAGNUS_DB_PORTS_LIMIT_COUNT
-        port_end = port_start + port_limit + 1
-        return f'{port_start} - {port_end}'
 
 
 class EndpointRuleSerializer(BulkModelSerializer):
