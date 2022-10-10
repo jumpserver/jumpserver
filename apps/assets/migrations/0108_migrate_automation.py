6e0d211645df57ca696732b39a4713cb6f24c82e
feng626 <1304903146@qq.com>
2022-10-10 17:08:06 +0800

    perf: automation migrate

--- apps/assets/migrations/0108_migrate_automation.py
+++ apps/assets/migrations/0108_migrate_automation.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('assets', '0106_auto_20220916_1556'),
+        ('assets', '0107_account_history'),
     ]
 
     operations = [
