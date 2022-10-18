4711813af814a41820647f0023d7d483a6a35091
Jiangjie.Bai <bugatti_it@163.com>
2022-10-18 17:47:13 +0800

    fix: 修复创建目录时指定权限为 755

--- apps/common/utils/common.py
+++ apps/common/utils/common.py
@@ -389,3 +389,10 @@ def static_or_direct(logo_path):
         return static(logo_path)
     else:
         return logo_path
+
+
+def make_dirs(name, mode=None, exist_ok=False):
+    """ 默认权限设置为 0o755 """
+    if mode is None:
+        mode = 0o755
+    return os.makedirs(name, mode=mode, exist_ok=exist_ok)
