cb83df6bd47006c7b0aee25ff294f6e8b2ba5ae2
jiangweidong <weidong.jiang@fit2cloud.com>
2022-11-16 09:37:08 +0800

    perf: 增加clickhouse端口帮助文案及注释

--- apps/applications/serializers/attrs/application_type/clickhouse.py
+++ apps/applications/serializers/attrs/application_type/clickhouse.py
@@ -7,4 +7,10 @@ __all__ = ['ClickHouseSerializer']
 
 
 class ClickHouseSerializer(DBSerializer):
-    port = serializers.IntegerField(default=9000, label=_('Port'), allow_null=True)
+    port = serializers.IntegerField(
+        default=9000, label=_('Port'), allow_null=True,
+        help_text=_(
+            'Typically, the port is 9000，'
+            'the HTTP interface and the native interface use different ports'
+        ),
+    )
