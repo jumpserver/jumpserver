0b802b1782468a3c390b2345714169a760e8bc18
feng <1304903146@qq.com>
2022-11-26 19:07:12 +0800

    perf: navigation page

--- apps/jumpserver/api.py
+++ apps/jumpserver/api.py
@@ -3,45 +3,51 @@ import time
 from django.core.cache import cache
 from django.utils import timezone
 from django.utils.timesince import timesince
-from django.db.models import Count, Max
+from django.db.models import Count, Max, F
 from django.http.response import JsonResponse, HttpResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from collections import Counter
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from users.models import User
 from assets.models import Asset
+from assets.const import AllTypes
 from terminal.models import Session
 from terminal.utils import ComponentsPrometheusMetricsUtil
 from orgs.utils import current_org
 from common.utils import lazyproperty
+from common.utils.timezone import local_now, local_zero_hour
 from orgs.caches import OrgResourceStatisticsCache
-
 
 __all__ = ['IndexApi']
 
 
 class DatesLoginMetricMixin:
+    request: Request
+
     @lazyproperty
     def days(self):
         query_params = self.request.query_params
-        if query_params.get('monthly'):
-            return 30
-        return 7
+        # monthly
+        count = query_params.get('days')
+        return count if count else 0
 
     @lazyproperty
     def sessions_queryset(self):
-        days = timezone.now() - timezone.timedelta(days=self.days)
-        sessions_queryset = Session.objects.filter(date_start__gt=days)
+        days = self.days
+        if days == 0:
+            t = local_zero_hour()
+        else:
+            t = local_now() - timezone.timedelta(days=days)
+        sessions_queryset = Session.objects.filter(date_start__gte=t)
         return sessions_queryset
 
     @lazyproperty
     def session_dates_list(self):
-        now = timezone.now()
+        now = local_now()
         dates = [(now - timezone.timedelta(days=i)).date() for i in range(self.days)]
         dates.reverse()
-        # dates = self.sessions_queryset.dates('date_start', 'day')
         return dates
 
     def get_dates_metrics_date(self):
@@ -63,7 +69,7 @@ class DatesLoginMetricMixin:
 
     def __set_data_to_cache(self, date, tp, count):
         cache_key = self.get_cache_key(date, tp)
-        cache.set(cache_key, count, 3600*24*7)
+        cache.set(cache_key, count, 3600 * 24 * 7)
 
     @staticmethod
     def get_date_start_2_end(d):
@@ -162,40 +168,45 @@ class DatesLoginMetricMixin:
     def dates_total_count_disabled_assets(self):
         return Asset.objects.filter(is_active=False).count()
 
-    # 以下是从week中而来
-    def get_dates_login_times_top5_users(self):
-        users = self.sessions_queryset.values_list('user_id', flat=True)
-        users = [
-            {'user': user, 'total': total}
-            for user, total in Counter(users).most_common(5)
-        ]
-        return users
-
     def get_dates_total_count_login_users(self):
         return len(set(self.sessions_queryset.values_list('user_id', flat=True)))
 
     def get_dates_total_count_login_times(self):
         return self.sessions_queryset.count()
 
-    def get_dates_login_times_top10_assets(self):
+    @lazyproperty
+    def get_type_to_assets(self):
+        result = Asset.objects.annotate(type=F('platform__type')). \
+            values('type').order_by('type').annotate(total=Count(1))
+        all_types_dict = dict(AllTypes.choices())
+        result = list(result)
+        for i in result:
+            tp = i['type']
+            i['label'] = all_types_dict[tp]
+        return result
+
+    def get_dates_login_times_assets(self):
         assets = self.sessions_queryset.values("asset") \
-                     .annotate(total=Count("asset")) \
-                     .annotate(last=Max("date_start")).order_by("-total")[:10]
+            .annotate(total=Count("asset")) \
+            .annotate(last=Max("date_start")).order_by("-total")
+        assets = assets[:10]
         for asset in assets:
             asset['last'] = str(asset['last'])
         return list(assets)
 
-    def get_dates_login_times_top10_users(self):
+    def get_dates_login_times_users(self):
         users = self.sessions_queryset.values("user_id") \
-                    .annotate(total=Count("user_id")) \
-                    .annotate(user=Max('user')) \
-                    .annotate(last=Max("date_start")).order_by("-total")[:10]
+            .annotate(total=Count("user_id")) \
+            .annotate(user=Max('user')) \
+            .annotate(last=Max("date_start")).order_by("-total")
+        users = users[:10]
         for user in users:
             user['last'] = str(user['last'])
         return list(users)
 
-    def get_dates_login_record_top10_sessions(self):
-        sessions = self.sessions_queryset.order_by('-date_start')[:10]
+    def get_dates_login_record_sessions(self):
+        sessions = self.sessions_queryset.order_by('-date_start')
+        sessions = sessions[:10]
         for session in sessions:
             session.avatar_url = User.get_avatar_url("")
         sessions = [
@@ -229,11 +240,13 @@ class IndexApi(DatesLoginMetricMixin, APIView):
         if _all or query_params.get('total_count') or query_params.get('total_count_users'):
             data.update({
                 'total_count_users': caches.users_amount,
+                'total_count_users_this_week': caches.new_users_amount_this_week,
             })
 
         if _all or query_params.get('total_count') or query_params.get('total_count_assets'):
             data.update({
                 'total_count_assets': caches.assets_amount,
+                'total_count_assets_this_week': caches.new_assets_amount_this_week,
             })
 
         if _all or query_params.get('total_count') or query_params.get('total_count_online_users'):
@@ -244,6 +257,23 @@ class IndexApi(DatesLoginMetricMixin, APIView):
         if _all or query_params.get('total_count') or query_params.get('total_count_online_sessions'):
             data.update({
                 'total_count_online_sessions': caches.total_count_online_sessions,
+            })
+
+        if _all or query_params.get('total_count') or query_params.get('total_count_today_failed_sessions'):
+            data.update({
+                'total_count_today_failed_sessions': caches.total_count_today_failed_sessions,
+            })
+        if _all or query_params.get('total_count') or query_params.get('total_count_today_login_users'):
+            data.update({
+                'total_count_today_login_users': caches.total_count_today_login_users,
+            })
+        if _all or query_params.get('total_count') or query_params.get('total_count_today_active_assets'):
+            data.update({
+                'total_count_today_active_assets': caches.total_count_today_active_assets,
+            })
+        if _all or query_params.get('total_count') or query_params.get('total_count_type_to_assets_amount'):
+            data.update({
+                'total_count_type_to_assets_amount': self.get_type_to_assets,
             })
 
         if _all or query_params.get('dates_metrics'):
@@ -274,24 +304,19 @@ class IndexApi(DatesLoginMetricMixin, APIView):
                 'dates_total_count_login_times': self.get_dates_total_count_login_times(),
             })
 
-        if _all or query_params.get('dates_login_times_top5_users'):
-            data.update({
-                'dates_login_times_top5_users': self.get_dates_login_times_top5_users(),
-            })
-
         if _all or query_params.get('dates_login_times_top10_assets'):
             data.update({
-                'dates_login_times_top10_assets': self.get_dates_login_times_top10_assets(),
+                'dates_login_times_top10_assets': self.get_dates_login_times_assets(),
             })
 
         if _all or query_params.get('dates_login_times_top10_users'):
             data.update({
-                'dates_login_times_top10_users': self.get_dates_login_times_top10_users(),
+                'dates_login_times_top10_users': self.get_dates_login_times_users(),
             })
 
         if _all or query_params.get('dates_login_record_top10_sessions'):
             data.update({
-                'dates_login_record_top10_sessions': self.get_dates_login_record_top10_sessions()
+                'dates_login_record_top10_sessions': self.get_dates_login_record_sessions()
             })
 
         return JsonResponse(data, status=200)
@@ -353,4 +378,3 @@ class PrometheusMetricsApi(HealthApiMixin):
         util = ComponentsPrometheusMetricsUtil()
         metrics_text = util.get_prometheus_metrics_text()
         return HttpResponse(metrics_text, content_type='text/plain; version=0.0.4; charset=utf-8')
-
