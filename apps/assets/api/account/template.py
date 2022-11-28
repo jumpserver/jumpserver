4f718f9b1fa16c5221168a7237bf0c2254155989
fit2bot <68588906+fit2bot@users.noreply.github.com>
2022-11-28 15:54:16 +0800

    perf: account template secret api (#9127)
    Co-authored-by: feng <1304903146@qq.com>

--- apps/assets/api/account/template.py
+++ apps/assets/api/account/template.py
@@ -1,6 +1,10 @@
-from orgs.mixins.api import OrgBulkModelViewSet
-from assets.models import AccountTemplate
 from assets import serializers
+from assets.models import AccountTemplate
+from rbac.permissions import RBACPermission
+from authentication.const import ConfirmType
+from common.mixins import RecordViewLogMixin
+from common.permissions import UserConfirmation
+from orgs.mixins.api import OrgBulkModelViewSet
 
 
 class AccountTemplateViewSet(OrgBulkModelViewSet):
@@ -9,4 +13,17 @@ class AccountTemplateViewSet(OrgBulkModelViewSet):
     search_fields = ('username', 'name')
     serializer_classes = {
         'default': serializers.AccountTemplateSerializer
+    }
+
+
+class AccountTemplateSecretsViewSet(RecordViewLogMixin, AccountTemplateViewSet):
+    serializer_classes = {
+        'default': serializers.AccountTemplateSecretSerializer,
+    }
+    http_method_names = ['get', 'options']
+    # Todo: 记得打开
+    # permission_classes = [RBACPermission, UserConfirmation.require(ConfirmType.MFA)]
+    rbac_perms = {
+        'list': 'assets.view_accounttemplatesecret',
+        'retrieve': 'assets.view_accounttemplatesecret',
     }
