19c3f98e8fbc4d333180551051c25d171f628d2b
ibuler <ibuler@qq.com>
2022-12-02 11:14:29 +0800

    pref: 修改 migrations 冲突

--- apps/audits/migrations/0016_alter_userloginlog_type.py
+++ apps/audits/migrations/0016_alter_userloginlog_type.py
@@ -4,15 +4,17 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('audits', '0015_auto_20221011_1745'),
+        ('audits', '0015_auto_20221111_1919'),
     ]
 
     operations = [
         migrations.AlterField(
             model_name='userloginlog',
             name='type',
-            field=models.CharField(choices=[('web_cli', 'Web Client'), ('web_gui', 'Web GUI'), ('db_cli', 'DB Client'), ('db_gui', 'DB GUI'), ('rdp_cli', 'RDP Client'), ('rdp_file', 'RDP File'), ('ssh_cli', 'SSH Client'), ('web_sftp', 'Web SFTP')], max_length=128, verbose_name='Login type'),
+            field=models.CharField(choices=[('web_cli', 'Web Client'), ('web_gui', 'Web GUI'), ('db_cli', 'DB Client'),
+                                            ('db_gui', 'DB GUI'), ('rdp_cli', 'RDP Client'), ('rdp_file', 'RDP File'),
+                                            ('ssh_cli', 'SSH Client'), ('web_sftp', 'Web SFTP')], max_length=128,
+                                   verbose_name='Login type'),
         ),
     ]
