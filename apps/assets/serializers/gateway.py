dea586873b9bc29d4db5e2b86f122e220a1e2a45
Bai <baijiangjie@gmail.com>
2022-12-05 19:08:05 +0800

    perf: 修改 GatewaySerializer 目录

--- apps/assets/serializers/gateway.py
+++ apps/assets/serializers/gateway.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-from rest_framework import serializers
-
 from ..serializers import HostSerializer
 from ..models import Gateway
 
@@ -9,19 +7,5 @@ __all__ = ['GatewaySerializer']
 
 
 class GatewaySerializer(HostSerializer):
-    effective_accounts = serializers.SerializerMethodField()
-
     class Meta(HostSerializer.Meta):
         model = Gateway
-        fields = HostSerializer.Meta.fields + ['effective_accounts']
-
-    @staticmethod
-    def get_effective_accounts(obj):
-        accounts = obj.accounts.all()
-        return [
-            {
-                'id': account.id,
-                'username': account.username,
-                'secret_type': account.secret_type,
-            } for account in accounts
-        ]
