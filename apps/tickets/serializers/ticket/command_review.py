c9bf99468cd8567df3b5c0ccf417e4abd5741ac7
Bai <baijiangjie@gmail.com>
2022-12-06 18:26:07 +0800

    feat: 添加 command-review API

--- apps/tickets/serializers/ticket/command_review.py
+++ apps/tickets/serializers/ticket/command_review.py
@@ -2,15 +2,15 @@ from tickets.models import ApplyCommandTicket
 from .ticket import TicketApplySerializer
 
 __all__ = [
-    'ApplyCommandConfirmSerializer',
+    'ApplyCommandReviewSerializer',
 ]
 
 
-class ApplyCommandConfirmSerializer(TicketApplySerializer):
+class ApplyCommandReviewSerializer(TicketApplySerializer):
     class Meta:
         model = ApplyCommandTicket
         writeable_fields = [
             'apply_run_user', 'apply_run_asset', 'apply_run_account', 'apply_run_command',
-            'apply_from_session', 'apply_from_cmd_filter', 'apply_from_cmd_filter_rule'
+            'apply_from_session', 'apply_from_cmd_filter_acl'
         ]
         fields = TicketApplySerializer.Meta.fields + writeable_fields
