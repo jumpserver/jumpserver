f2e183e4ed0e193299b97655e1c6c65e77b11866
Bai <baijiangjie@gmail.com>
2022-12-21 21:07:25 +0800

    perf: 修改方法名 check_db_port_mapper

--- apps/terminal/signal_handlers.py
+++ apps/terminal/signal_handlers.py
@@ -42,7 +42,7 @@ def on_applet_create(sender, instance, created=False, **kwargs):
 
 
 @receiver(django_ready)
-def init_db_port_mapper(sender, **kwargs):
+def check_db_port_mapper(sender, **kwargs):
     logger.info('Init db port mapper')
     try:
         db_port_manager.check()
