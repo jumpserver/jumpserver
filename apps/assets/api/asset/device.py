ed82249fd14a95c79fc14d0d0eaf7b87711c420a
fit2bot <68588906+fit2bot@users.noreply.github.com>
2022-12-23 19:01:29 +0800

    perf: asset perm (#9240)
    Co-authored-by: feng <1304903146@qq.com>

--- apps/assets/api/asset/device.py
+++ apps/assets/api/asset/device.py
@@ -1,6 +1,5 @@
-
 from assets.serializers import DeviceSerializer
-from assets.models import Device
+from assets.models import Device, Asset
 from .asset import AssetViewSet
 
 __all__ = ['DeviceViewSet']
@@ -8,6 +7,7 @@ __all__ = ['DeviceViewSet']
 
 class DeviceViewSet(AssetViewSet):
     model = Device
+    perm_model = Asset
 
     def get_serializer_classes(self):
         serializer_classes = super().get_serializer_classes()
