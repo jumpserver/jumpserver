0a68db8d65481d9006d20d662a309c52b41bc6f3
jiangweidong <weidong.jiang@fit2cloud.com>
2023-01-04 15:07:57 +0800

    fix: 错误的修改改回去

--- apps/terminal/models/applet/applet.py
+++ apps/terminal/models/applet/applet.py
@@ -125,7 +125,7 @@ class Applet(JMSBaseModel):
 class AppletPublication(JMSBaseModel):
     applet = models.ForeignKey('Applet', on_delete=models.CASCADE, related_name='publications',
                                verbose_name=_('Applet'))
-    host = models.ForeignKey('AppletHost', on_delete=models.PROTECT, related_name='publications',
+    host = models.ForeignKey('AppletHost', on_delete=models.CASCADE, related_name='publications',
                              verbose_name=_('Hosting'))
     status = models.CharField(max_length=16, default='ready', verbose_name=_('Status'))
     comment = models.TextField(default='', blank=True, verbose_name=_('Comment'))
