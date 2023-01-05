0062705040b326b67221d585a14ae498b60a2651
ibuler <ibuler@qq.com>
2023-01-05 14:58:55 +0800

    fix: applet list 404

--- apps/common/views/http.py
+++ apps/common/views/http.py
@@ -3,8 +3,6 @@
 from django.http import HttpResponse
 from django.utils.encoding import iri_to_uri
 
-from rest_framework.serializers import BooleanField
-
 
 class HttpResponseTemporaryRedirect(HttpResponse):
     status_code = 307
@@ -13,10 +11,3 @@ class HttpResponseTemporaryRedirect(HttpResponse):
         HttpResponse.__init__(self)
         self['Location'] = iri_to_uri(redirect_to)
 
-
-def get_remote_addr(request):
-    return request.META.get("HTTP_X_FORWARDED_HOST") or request.META.get("REMOTE_ADDR")
-
-
-def is_true(value):
-    return value in BooleanField.TRUE_VALUES
