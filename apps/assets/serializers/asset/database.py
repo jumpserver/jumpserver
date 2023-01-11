7c74db07a8e054fae7abc1fa478ae5a95b0cebe2
jiangweidong <weidong.jiang@fit2cloud.com>
2023-01-11 14:36:24 +0800

    feat: Redis/MongoDB 支持SSL

--- apps/assets/serializers/asset/database.py
+++ apps/assets/serializers/asset/database.py
@@ -8,7 +8,11 @@ __all__ = ['DatabaseSerializer', 'DatabaseWithGatewaySerializer']
 class DatabaseSerializer(AssetSerializer):
     class Meta(AssetSerializer.Meta):
         model = Database
-        fields = AssetSerializer.Meta.fields + ['db_name']
+        extra_fields = [
+            'db_name', 'use_ssl', 'ca_cert', 'client_cert',
+            'client_key', 'allow_invalid_cert'
+        ]
+        fields = AssetSerializer.Meta.fields + extra_fields
 
 
 class DatabaseWithGatewaySerializer(DatabaseSerializer):
