308077155951ce9d0fcf67a985fd91bd08ea9920
jiangweidong <80373698+Hi-JWD@users.noreply.github.com>
2023-02-02 10:32:48 +0800

    fix: 解决没获取到证书的mongodb导致的无法执行自动化任务 (#9399)

--- apps/ops/ansible/inventory.py
+++ apps/ops/ansible/inventory.py
@@ -105,7 +105,7 @@ class JMSInventory:
                 'id': str(asset.id), 'name': asset.name, 'address': asset.address,
                 'type': asset.type, 'category': asset.category,
                 'protocol': asset.protocol, 'port': asset.port,
-                'spec_info': asset.spec_info,
+                'spec_info': asset.spec_info, 'secret_info': asset.secret_info,
                 'protocols': [{'name': p.name, 'port': p.port} for p in protocols],
             },
             'jms_account': {
