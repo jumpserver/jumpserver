c3c2e529ede00564d78e204f2b76857bc6979cc1
Bai <baijiangjie@gmail.com>
2023-02-09 19:38:28 +0800

    fix: 修复第三方用户登录复核时，可以跳过的问题

--- apps/acls/serializers/login_acl.py
+++ apps/acls/serializers/login_acl.py
@@ -22,7 +22,7 @@ class LoginACLSerializer(BulkModelSerializer):
     reviewers = ObjectRelatedField(
         queryset=User.objects, label=_("Reviewers"), many=True, required=False
     )
-    action = LabeledChoiceField(choices=LoginACL.ActionChoices.choices)
+    action = LabeledChoiceField(choices=LoginACL.ActionChoices.choices, label=_('Action'))
     reviewers_amount = serializers.IntegerField(
         read_only=True, source="reviewers.count", label=_("Reviewers amount")
     )
