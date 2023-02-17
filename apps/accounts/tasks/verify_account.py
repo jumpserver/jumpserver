fb32c976b2b71f658391fe1533e1b0b71bdabe65
Bai <baijiangjie@gmail.com>
2023-02-17 19:23:42 +0800

    fix: 翻译所有 celerytask verbose

--- apps/accounts/tasks/verify_account.py
+++ apps/accounts/tasks/verify_account.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 from django.utils.translation import gettext_noop
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 
 from accounts.const import AutomationTypes
 from accounts.tasks.common import automation_execute_start
--- apps/locale/zh/LC_MESSAGES/django.mo
+++ apps/locale/zh/LC_MESSAGES/django.mo
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8109162896c38e80e2a22895cd42e48b174e7b44897d7ea34c4c19f8eb865e81
-size 112043
+oid sha256:efc2a851536c7c7ee6fc9de33630d716e58fdaaa4dd1bce63a13e8a32482b4ae
+size 111687
