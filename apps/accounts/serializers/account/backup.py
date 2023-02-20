9644d8b1bc90d8cfbc86791a5c8343f8623d65d6
ibuler <ibuler@qq.com>
2023-02-20 16:20:03 +0800

    fix: 修复 ansible 网关问题

--- apps/accounts/serializers/account/backup.py
+++ apps/accounts/serializers/account/backup.py
@@ -3,13 +3,12 @@
 from django.utils.translation import ugettext as _
 from rest_framework import serializers
 
-from orgs.mixins.serializers import BulkOrgResourceModelSerializer
-from ops.mixin import PeriodTaskSerializerMixin
-from common.utils import get_logger
+from accounts.models import AccountBackupAutomation, AccountBackupExecution
 from common.const.choices import Trigger
 from common.serializers.fields import LabeledChoiceField
-
-from accounts.models import AccountBackupAutomation, AccountBackupExecution
+from common.utils import get_logger
+from ops.mixin import PeriodTaskSerializerMixin
+from orgs.mixins.serializers import BulkOrgResourceModelSerializer
 
 logger = get_logger(__file__)
 
@@ -20,18 +19,21 @@ class AccountBackupSerializer(PeriodTaskSerializerMixin, BulkOrgResourceModelSer
     class Meta:
         model = AccountBackupAutomation
         read_only_fields = [
-            'date_created', 'date_updated', 'created_by', 'periodic_display', 'executed_amount'
+            'date_created', 'date_updated', 'created_by',
+            'periodic_display', 'executed_amount'
         ]
         fields = read_only_fields + [
-            'id', 'name', 'is_periodic', 'interval', 'crontab', 'comment', 'recipients', 'types'
+            'id', 'name', 'is_periodic', 'interval', 'crontab',
+            'comment', 'recipients', 'types'
         ]
         extra_kwargs = {
             'name': {'required': True},
             'periodic_display': {'label': _('Periodic perform')},
             'executed_amount': {'label': _('Executed amount')},
-            'recipients': {'label': _('Recipient'), 'help_text': _(
-                'Currently only mail sending is supported'
-            )},
+            'recipients': {
+                'label': _('Recipient'),
+                'help_text': _('Currently only mail sending is supported')
+            },
             'types': {'label': _('Asset type')}
         }
 
@@ -42,7 +44,7 @@ class AccountBackupPlanExecutionSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountBackupExecution
         read_only_fields = [
-            'id', 'date_start', 'timedelta', 'plan_snapshot', 'trigger', 'reason',
-            'is_success', 'org_id', 'recipients'
+            'id', 'date_start', 'timedelta', 'plan_snapshot',
+            'trigger', 'reason', 'is_success', 'org_id', 'recipients'
         ]
         fields = read_only_fields + ['plan']
