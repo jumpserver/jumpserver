8ddf442ba897842959366ac4ed5fd526e46da0ba
Bai <baijiangjie@gmail.com>
2023-03-01 15:16:11 +0800

    fix: 修改翻译 Core API & HelpText

--- apps/terminal/serializers/applet_host.py
+++ apps/terminal/serializers/applet_host.py
@@ -29,7 +29,16 @@ class DeployOptionsSerializer(serializers.Serializer):
         (0, _("Enabled")),
     )
 
-    CORE_HOST = serializers.CharField(default=settings.SITE_URL, label=_('API Server'), max_length=1024)
+    CORE_HOST = serializers.CharField(
+        default=settings.SITE_URL, label=_('Core API'), max_length=1024,
+        help_text=_(""" 
+        Tips: The application release machine communicates with the Core service. 
+        If the release machine and the Core service are on the same network segment, 
+        it is recommended to fill in the intranet address, otherwise fill in the current site URL 
+        <br> 
+        eg: https://172.16.10.110 or https://dev.jumpserver.com
+        """)
+    )
     RDS_Licensing = serializers.BooleanField(default=False, label=_("Existing RDS license"))
     RDS_LicenseServer = serializers.CharField(default='127.0.0.1', label=_('RDS License Server'), max_length=1024)
     RDS_LicensingMode = serializers.ChoiceField(choices=LICENSE_MODE_CHOICES, default=2, label=_('RDS Licensing Mode'))
