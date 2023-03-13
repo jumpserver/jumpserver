16a7ccc95e89158d74dd94bd31a814431587aeef
ibuler <ibuler@qq.com>
2023-03-13 18:06:54 +0800

    perf: 优化文案

--- apps/common/drf/parsers/excel.py
+++ apps/common/drf/parsers/excel.py
@@ -1,4 +1,5 @@
 import pyexcel
+from django.utils.translation import gettext as _
 
 from .base import BaseFileParser
 
@@ -10,7 +11,7 @@ class ExcelFileParser(BaseFileParser):
         try:
             workbook = pyexcel.get_book(file_type='xlsx', file_content=stream_data)
         except Exception:
-            raise Exception('Invalid excel file')
+            raise Exception(_('Invalid excel file'))
         # 默认获取第一个工作表sheet
         sheet = workbook.sheet_by_index(0)
         rows = sheet.rows()
