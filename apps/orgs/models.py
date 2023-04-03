ef44d4b04488e5bf23a789dda8596e8087560dfc
Bai <baijiangjie@gmail.com>
2023-04-03 15:21:55 +0800

    fix: 修复 LDAP 导入用户时指定其他组织，还会导入到 Default 组织的问题

--- apps/orgs/models.py
+++ apps/orgs/models.py
@@ -32,6 +32,12 @@ class OrgRoleMixin:
             }
             self.members.through.objects.update_or_create(**defaults, defaults=defaults)
 
+    def remove_member(self, users):
+        from orgs.utils import tmp_to_org
+        with tmp_to_org(self):
+            for user in users:
+                user.remove()
+
     def get_origin_role_members(self, role_name):
         from rbac.models import OrgRoleBinding
         from rbac.builtin import BuiltinRole
