a75401d3108c1db5234833811e850b76024150ae
feng <1304903146@qq.com>
2023-04-20 15:47:21 +0800

    perf: asset date_updated

--- apps/assets/api/asset/asset.py
+++ apps/assets/api/asset/asset.py
@@ -96,7 +96,7 @@ class AssetViewSet(SuggestionMixin, NodeFilterMixin, OrgBulkModelViewSet):
     model = Asset
     filterset_class = AssetFilterSet
     search_fields = ("name", "address")
-    ordering_fields = ('name', 'connectivity', 'platform')
+    ordering_fields = ('name', 'connectivity', 'platform', 'date_updated')
     serializer_classes = (
         ("default", serializers.AssetSerializer),
         ("platform", serializers.PlatformSerializer),
