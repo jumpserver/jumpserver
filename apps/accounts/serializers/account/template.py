fe036dfd32bb06b6ca2402ed4dfacf37ee600350
feng <1304903146@qq.com>
2023-04-24 16:30:41 +0800

    feat: 修改模版账号密码 同步更新关联的账号

--- apps/accounts/serializers/account/template.py
+++ apps/accounts/serializers/account/template.py
@@ -1,5 +1,6 @@
-from django.db.transaction import atomic
-from django.db.utils import IntegrityError
+from django.db.models import Count
+from django.utils import timezone
+from rest_framework import serializers
 
 from accounts.models import AccountTemplate, Account
 from assets.models import Asset
@@ -8,79 +9,86 @@ from .base import BaseAccountSerializer
 
 
 class AccountTemplateSerializer(BaseAccountSerializer):
+    is_sync_account = serializers.BooleanField(default=False, write_only=True)
+    _is_sync_account = False
+
     class Meta(BaseAccountSerializer.Meta):
         model = AccountTemplate
+        fields = BaseAccountSerializer.Meta.fields + ['is_sync_account']
 
     @staticmethod
-    def account_save(data, account):
-        for field, value in data.items():
-            setattr(account, field, value)
-        try:
-            account.save(update_fields=list(data.keys()))
-        except IntegrityError:
-            pass
+    def bulk_update_accounts(accounts, data):
+        history_model = Account.history.model
+        account_ids = accounts.values_list('id', flat=True)
+        history_accounts = history_model.objects.filter(id__in=account_ids)
+        account_id_count_map = {
+            str(i['id']): i['count']
+            for i in history_accounts.values('id').order_by('id')
+            .annotate(count=Count(1)).values('id', 'count')
+        }
 
-    # TODO 数据库访问的太多了 后期优化
-    @atomic()
-    def bulk_update_accounts(self, instance, diff):
+        for account in accounts:
+            account_id = str(account.id)
+            account.version = account_id_count_map.get(account_id) + 1
+            for k, v in data.items():
+                setattr(account, k, v)
+        Account.objects.bulk_update(accounts, ['version', 'secret'])
+
+    def bulk_create_history_accounts(self, accounts):
+        history_model = Account.history.model
+        history_account_objs = []
+        for account in accounts:
+            history_account_objs.append(
+                history_model(
+                    id=account.id,
+                    version=account.version,
+                    secret=account.secret,
+                    secret_type=account.secret_type,
+                    history_user_id=self.context['request'].user.id,
+                    history_date=timezone.now()
+                )
+            )
+        history_model.objects.bulk_create(history_account_objs)
+
+    def sync_accounts(self, instance, diff):
+        if not self._is_sync_account:
+            return
+
         accounts = Account.objects.filter(source_id=instance.id)
         if not accounts:
             return
 
-        diff.pop('secret', None)
-        name = diff.pop('name', None)
-        username = diff.pop('username', None)
+        secret = diff.pop('secret', None)
         secret_type = diff.pop('secret_type', None)
-        update_accounts = []
-        for account in accounts:
-            for field, value in diff.items():
-                setattr(account, field, value)
-                update_accounts.append(account)
-
-        if update_accounts:
-            Account.objects.bulk_update(update_accounts, diff.keys())
-
-        if name:
-            for account in accounts:
-                data = {'name': name}
-                self.account_save(data, account)
-
-        if secret_type and username:
+        if secret_type:
             asset_ids_supports = self.get_asset_ids_supports(accounts, secret_type)
-            for account in accounts:
-                asset_id = account.asset_id
-                if asset_id not in asset_ids_supports:
-                    data = {'username': username}
-                    self.account_save(data, account)
-                    continue
-                data = {'username': username, 'secret_type': secret_type, 'secret': instance.secret}
-                self.account_save(data, account)
-        elif secret_type:
-            asset_ids_supports = self.get_asset_ids_supports(accounts, secret_type)
-            for account in accounts:
-                asset_id = account.asset_id
-                if asset_id not in asset_ids_supports:
-                    continue
-                data = {'secret_type': secret_type, 'secret': instance.secret}
-                self.account_save(data, account)
-        elif username:
-            for account in accounts:
-                data = {'username': username}
-                self.account_save(data, account)
+            accounts = accounts.filter(asset_id__in=asset_ids_supports)
+            if not accounts:
+                return
+            accounts.update(secret_type=secret_type)
+
+        if secret:
+            self.bulk_update_accounts(accounts, {'secret': instance.secret})
+            self.bulk_create_history_accounts(accounts)
 
     @staticmethod
     def get_asset_ids_supports(accounts, secret_type):
         asset_ids = accounts.values_list('asset_id', flat=True)
         secret_type_supports = Asset.get_secret_type_assets(asset_ids, secret_type)
-        return [asset.id for asset in secret_type_supports]
+        return [str(asset.id) for asset in secret_type_supports]
+
+    def validate(self, attrs):
+        self._is_sync_account = attrs.pop('is_sync_account', None)
+        attrs = super().validate(attrs)
+        return attrs
 
     def update(self, instance, validated_data):
-        # diff = {
-        #     k: v for k, v in validated_data.items()
-        #     if getattr(instance, k) != v
-        # }
+        diff = {
+            k: v for k, v in validated_data.items()
+            if getattr(instance, k) != v
+        }
         instance = super().update(instance, validated_data)
-        # self.bulk_update_accounts(instance, diff)
+        self.sync_accounts(instance, diff)
         return instance
 
 
