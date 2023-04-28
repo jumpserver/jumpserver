bb516a71369aa0e38769effdb380fadc8dee4e18
jiangweidong <weidong.jiang@fit2cloud.com>
2023-04-28 16:24:33 +0800

    perf: 优化逻辑，抽离callback_base类

---
+++ apps/authentication/views/base.py
@@ -0,0 +1,105 @@
+from functools import lru_cache
+
+from rest_framework.request import Request
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from django.db.utils import IntegrityError
+from django.views import View
+
+from authentication import errors
+from authentication.mixins import AuthMixin
+from users.models import User
+from common.utils.django import reverse, get_object_or_none
+from common.utils import get_logger
+
+from .mixins import FlashMessageMixin
+
+
+logger = get_logger(__file__)
+
+
+class BaseLoginCallbackView(AuthMixin, FlashMessageMixin, View):
+
+    def __init__(self):
+        super().__init__()
+        self.client_type = None
+        self.client_auth_params = {}
+        self.user_type = None
+        self.auth_backend = None
+        self.create_user_if_not_exist_setting = ''
+        # 提示信息
+        self.msg_client_err = _('Error')
+        self.msg_user_not_bound_err = _('Error')
+        self.msg_user_need_bound_warning = _('Error')
+        self.msg_not_found_user_from_client_err = _('Error')
+
+    def verify_state(self):
+        raise NotImplementedError
+
+    def get_verify_state_failed_response(self, redirect_uri):
+        raise NotImplementedError
+
+    @property
+    @lru_cache(maxsize=1)
+    def client(self):
+        if self.client_type is None or not self.client_auth_params:
+            raise NotImplementedError
+        client_init = {k: getattr(settings, v) for k, v in self.client_auth_params.items()}
+        return self.client_type(**client_init)
+
+    def create_user_if_not_exist(self, user_id, **kwargs):
+        user = None
+        if not getattr(settings, self.create_user_if_not_exist_setting):
+            title = self.msg_client_err
+            msg = self.msg_user_need_bound_warning
+            return user, (title, msg)
+
+        user_attr = self.client.get_user_detail(user_id, **kwargs)
+        try:
+            user, create = User.objects.get_or_create(
+                username=user_attr['username'], defaults=user_attr
+            )
+            setattr(user, f'{self.user_type}_id', user_id)
+            if create:
+                setattr(user, 'source', self.user_type)
+            user.save()
+        except IntegrityError as err:
+            logger.error(f'{self.msg_client_err}: create user error: {err}')
+
+        if user is None:
+            title = self.msg_client_err
+            msg = _('If you have any question, please contact the administrator')
+            return user, (title, msg)
+
+        return user, None
+
+    def get(self, request: Request):
+        code = request.GET.get('code')
+        redirect_url = request.GET.get('redirect_url')
+        login_url = reverse('authentication:login')
+
+        if not self.verify_state():
+            return self.get_verify_state_failed_response(redirect_url)
+
+        user_id, other_info = self.client.get_user_id_by_code(code)
+        if not user_id:
+            # 正常流程不会出这个错误，hack 行为
+            err = self.msg_not_found_user_from_client_err
+            response = self.get_failed_response(login_url, title=err, msg=err)
+            return response
+
+        user = get_object_or_none(User, **{f'{self.user_type}_id': user_id})
+        if user is None:
+            user, err = self.create_user_if_not_exist(user_id, other_info=other_info)
+            if err is not None:
+                response = self.get_failed_response(login_url, title=err[0], msg=err[1])
+                return response
+
+        try:
+            self.check_oauth2_auth(user, getattr(settings, self.auth_backend))
+        except errors.AuthFailedError as e:
+            self.set_login_failed_mark()
+            msg = e.msg
+            response = self.get_failed_response(login_url, title=msg, msg=msg)
+            return response
+        return self.redirect_to_guard_view()
--- apps/authentication/views/mixins.py
+++ apps/authentication/views/mixins.py
@@ -1,19 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-from functools import lru_cache
-
-from rest_framework.request import Request
-from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
-from django.db.utils import IntegrityError
-
-from authentication import errors
-from users.models import User
-from common.utils.django import reverse, get_object_or_none
-from common.utils import get_logger
-
-
-logger = get_logger(__file__)
+from common.utils import FlashMessageUtil
 
 
 class METAMixin:
@@ -27,90 +14,14 @@ class METAMixin:
         return next_url
 
 
-class Client:
-    get_user_id_by_code: callable
-    get_user_detail: callable
+class FlashMessageMixin:
+    @staticmethod
+    def get_response(redirect_url, title, msg, m_type='message'):
+        message_data = {'title': title, 'interval': 5, 'redirect_url': redirect_url, m_type: msg}
+        return FlashMessageUtil.gen_and_redirect_to(message_data)
 
+    def get_success_response(self, redirect_url, title, msg):
+        self.get_response(redirect_url, title, msg)
 
-class QRLoginCallbackMixin:
-    verify_state: callable
-    get_verify_state_failed_response: callable
-    get_failed_response: callable
-    check_oauth2_auth: callable
-    set_login_failed_mark: callable
-    redirect_to_guard_view: callable
-    # 属性
-    _client: Client
-    CLIENT_INFO: tuple
-    USER_TYPE: str
-    AUTH_BACKEND: str
-    CREATE_USER_IF_NOT_EXIST: str
-    # 提示信息
-    MSG_CLIENT_ERR: str
-    MSG_USER_NOT_BOUND_ERR: str
-    MSG_USER_NEED_BOUND_WARNING: str
-    MSG_NOT_FOUND_USER_FROM_CLIENT_ERR: str
-
-    @property
-    @lru_cache(maxsize=1)
-    def client(self):
-        client_type, client_init = self.CLIENT_INFO
-        client_init = {k: getattr(settings, v) for k, v in client_init.items()}
-        return client_type(**client_init)
-
-    def create_user_if_not_exist(self, user_id, **kwargs):
-        user = None
-        if not getattr(settings, self.CREATE_USER_IF_NOT_EXIST):
-            title = self.MSG_CLIENT_ERR
-            msg = self.MSG_USER_NEED_BOUND_WARNING
-            return user, (title, msg)
-
-        user_attr = self.client.get_user_detail(user_id, **kwargs)
-        try:
-            user, create = User.objects.get_or_create(
-                username=user_attr['username'], defaults=user_attr
-            )
-            setattr(user, f'{self.USER_TYPE}_id', user_id)
-            if create:
-                setattr(user, 'source', self.USER_TYPE)
-            user.save()
-        except IntegrityError as err:
-            logger.error(f'{self.MSG_CLIENT_ERR}: create user error: {err}')
-
-        if user is None:
-            title = self.MSG_CLIENT_ERR
-            msg = _('If you have any question, please contact the administrator')
-            return user, (title, msg)
-
-        return user, None
-
-    def get(self, request: Request):
-        code = request.GET.get('code')
-        redirect_url = request.GET.get('redirect_url')
-        login_url = reverse('authentication:login')
-
-        if not self.verify_state():
-            return self.get_verify_state_failed_response(redirect_url)
-
-        user_id, other_info = self.client.get_user_id_by_code(code)
-        if not user_id:
-            # 正常流程不会出这个错误，hack 行为
-            err = self.MSG_NOT_FOUND_USER_FROM_CLIENT_ERR
-            response = self.get_failed_response(login_url, title=err, msg=err)
-            return response
-
-        user = get_object_or_none(User, **{f'{self.USER_TYPE}_id': user_id})
-        if user is None:
-            user, err = self.create_user_if_not_exist(user_id, other_info=other_info)
-            if err is not None:
-                response = self.get_failed_response(login_url, title=err[0], msg=err[1])
-                return response
-
-        try:
-            self.check_oauth2_auth(user, getattr(settings, self.AUTH_BACKEND))
-        except errors.AuthFailedError as e:
-            self.set_login_failed_mark()
-            msg = e.msg
-            response = self.get_failed_response(login_url, title=msg, msg=msg)
-            return response
-        return self.redirect_to_guard_view()
+    def get_failed_response(self, redirect_url, title, msg):
+        self.get_response(redirect_url, title, msg, 'error')
