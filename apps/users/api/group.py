fd7e821f111243dc326f9180d667b011170678b8
feng <1304903146@qq.com>
2023-05-29 15:30:06 +0800

    feat: 用户组绑定所有用户

--- apps/users/api/group.py
+++ apps/users/api/group.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 #
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from orgs.mixins.api import OrgBulkModelViewSet
-from ..models import UserGroup
+from ..models import UserGroup, User
 from ..serializers import UserGroupSerializer
 
 __all__ = ['UserGroupViewSet']
@@ -14,3 +17,13 @@ class UserGroupViewSet(OrgBulkModelViewSet):
     search_fields = filterset_fields
     serializer_class = UserGroupSerializer
     ordering = ('name',)
+    rbac_perms = (
+        ("add_all_users", "users.change_usergroup"),
+    )
+
+    @action(methods=['post'], detail=True, url_path='add-all-users')
+    def add_all_users(self, request, *args, **kwargs):
+        instance = self.get_object()
+        users = User.get_org_users().exclude(groups__id=instance.id)
+        instance.users.add(*users)
+        return Response(status=status.HTTP_200_OK)
