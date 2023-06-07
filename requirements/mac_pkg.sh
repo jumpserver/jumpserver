bfd77aa1b04ec4a70651c01d9929381a582d1a40
fit2bot <68588906+fit2bot@users.noreply.github.com>
2023-06-07 17:28:35 +0800

    feat: automation windows pyrdp ping (#10602)
    * feat: automation windows pyrdp ping
    
    * perf: add pyfreerdp deps
    
    ---------
    
    Co-authored-by: feng <1304903146@qq.com>
    Co-authored-by: Eric <xplzv@126.com>

--- requirements/mac_pkg.sh
+++ requirements/mac_pkg.sh
@@ -5,7 +5,7 @@ PROJECT_DIR=$(dirname "$BASE_DIR")
 echo "1. 安装依赖"
 brew install libtiff libjpeg webp little-cms2 openssl gettext git \
    git-lfs mysql libxml2 libxmlsec1 pkg-config postgresql freetds openssl \
-   libffi
+   libffi freerdp
 
 echo "2. 下载 IP 数据库"
 ip_db_path="${PROJECT_DIR}/apps/common/utils/geoip/GeoLite2-City.mmdb"
