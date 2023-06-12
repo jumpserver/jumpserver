937befcfcaa0e9c8f51dcd0dab08504e82438673
jiangweidong <weidong.jiang@fit2cloud.com>
2023-06-12 15:35:39 +0800

    fix: 解决录像无法在线观看的问题

--- apps/terminal/api/session/session.py
+++ apps/terminal/api/session/session.py
@@ -212,7 +212,7 @@ class SessionReplayViewSet(AsyncApiMixin, viewsets.ViewSet):
 
         storage = ReplayStorageHandler(session)
         local_path, url_or_err = storage.get_file_path_url()
-        if url_or_err:
+        if local_path is None:
             return Response({"error": url_or_err}, status=404)
         data = self.get_replay_data(session, url_or_err)
         return Response(data)
