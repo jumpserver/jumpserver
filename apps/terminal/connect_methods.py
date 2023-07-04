58beecc0be824e9e6f75c7bcceda7dd772299c9c
Eric_Lee <xplzv@126.com>
2023-07-04 17:58:04 +0800

    Revert "perf: rdp7 可使用 web gui方式连接"

--- apps/terminal/connect_methods.py
+++ apps/terminal/connect_methods.py
@@ -195,7 +195,7 @@ class ConnectMethodUtil:
             TerminalType.lion: {
                 'web_methods': [WebMethod.web_gui],
                 'listen': [Protocol.http],
-                'support': [Protocol.rdp, Protocol.rdp7, Protocol.vnc],
+                'support': [Protocol.rdp, Protocol.vnc],
                 'match': 'm2m'
             },
             TerminalType.magnus: {
