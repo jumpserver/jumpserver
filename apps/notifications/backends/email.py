ff8e48f67b77c53e7c1427c57b5f2600755ceef2
halo <wuyihuangw@gmail.com>
2023-07-13 21:50:17 +0800

    fix: 邮件主题前缀设置不生效的问题

--- apps/notifications/backends/email.py
+++ apps/notifications/backends/email.py
@@ -11,6 +11,7 @@ class Email(BackendBase):
     def send_msg(self, users, message, subject):
         from_email = settings.EMAIL_FROM or settings.EMAIL_HOST_USER
         accounts, __, __ = self.get_accounts(users)
+        subject = (settings.EMAIL_SUBJECT_PREFIX or '') + subject
         send_mail(subject, message, from_email, accounts, html_message=message)
 
 
