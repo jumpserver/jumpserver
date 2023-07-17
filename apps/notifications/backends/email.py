55210f984059dc909cf08df31a03c593d897d381
halo <wuyihuangw@gmail.com>
2023-07-17 22:13:58 +0800

    perf: 优化邮件参数

--- apps/notifications/backends/email.py
+++ apps/notifications/backends/email.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from common.tasks import send_mail_async
 from .base import BackendBase
 
@@ -8,10 +7,8 @@ class Email(BackendBase):
     is_enable_field_in_settings = 'EMAIL_HOST_USER'
 
     def send_msg(self, users, message, subject):
-        from_email = settings.EMAIL_FROM or settings.EMAIL_HOST_USER
         accounts, __, __ = self.get_accounts(users)
-        subject = (settings.EMAIL_SUBJECT_PREFIX or '') + subject
-        send_mail_async(subject, message, from_email, accounts, html_message=message)
+        send_mail_async(subject, message, accounts, html_message=message)
 
 
 backend = Email
