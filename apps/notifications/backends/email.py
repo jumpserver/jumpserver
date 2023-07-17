36986879f84311d68f963aea6bab2b6f1836b44a
halo <wuyihuangw@gmail.com>
2023-07-17 14:54:30 +0800

    perf: 异步发送

--- apps/notifications/backends/email.py
+++ apps/notifications/backends/email.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-from django.core.mail import send_mail
-
+from common.tasks import send_mail_async
 from .base import BackendBase
 
 
@@ -12,7 +11,7 @@ class Email(BackendBase):
         from_email = settings.EMAIL_FROM or settings.EMAIL_HOST_USER
         accounts, __, __ = self.get_accounts(users)
         subject = (settings.EMAIL_SUBJECT_PREFIX or '') + subject
-        send_mail(subject, message, from_email, accounts, html_message=message)
+        send_mail_async.delay(subject, message, from_email, accounts, html_message=message)
 
 
 backend = Email
