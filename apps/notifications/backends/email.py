b9c61a54942509f34669e5c65c1db2b027cf6321
Bai <baijiangjie@gmail.com>
2023-07-17 17:08:06 +0800

    perf: 邮件同步发送

--- apps/notifications/backends/email.py
+++ apps/notifications/backends/email.py
@@ -11,7 +11,7 @@ class Email(BackendBase):
         from_email = settings.EMAIL_FROM or settings.EMAIL_HOST_USER
         accounts, __, __ = self.get_accounts(users)
         subject = (settings.EMAIL_SUBJECT_PREFIX or '') + subject
-        send_mail_async.delay(subject, message, from_email, accounts, html_message=message)
+        send_mail_async(subject, message, from_email, accounts, html_message=message)
 
 
 backend = Email
