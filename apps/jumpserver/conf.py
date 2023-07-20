2291cfeaae45f99cb3bb392af0bc4a89a0c09c84
Bai <baijiangjie@gmail.com>
2023-07-20 10:41:11 +0800

    fix: 修复 ConnectionToken 默认值类型没有转化的问题

--- apps/jumpserver/conf.py
+++ apps/jumpserver/conf.py
@@ -232,8 +232,12 @@ class Config(dict):
         'SESSION_COOKIE_AGE': 3600 * 24,
         'SESSION_EXPIRE_AT_BROWSER_CLOSE': False,
         'LOGIN_URL': reverse_lazy('authentication:login'),
-        'CONNECTION_TOKEN_ONETIME_EXPIRATION': 5 * 60,  # 默认
-        'CONNECTION_TOKEN_REUSABLE_EXPIRATION': 60 * 60 * 24 * 30,  # 最大
+
+        'CONNECTION_TOKEN_ONETIME_EXPIRATION': 5 * 60,  # 默认(new)
+        'CONNECTION_TOKEN_EXPIRATION':  5 * 60,  # 默认(old)
+
+        'CONNECTION_TOKEN_REUSABLE_EXPIRATION': 60 * 60 * 24 * 30,  # 最大(new)
+        'CONNECTION_TOKEN_EXPIRATION_MAX': 60 * 60 * 24 * 30,  # 最大(old)
         'CONNECTION_TOKEN_REUSABLE': False,
 
         # Custom Config
