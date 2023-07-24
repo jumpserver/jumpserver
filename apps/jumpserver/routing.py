5fedb5440ce6c20fe76756c927f7c46e2ea3401a
ibuler <ibuler@qq.com>
2023-07-24 23:23:04 +0800

    perf: 设置 application 到 __all__

--- apps/jumpserver/routing.py
+++ apps/jumpserver/routing.py
@@ -13,7 +13,7 @@ from ops.urls.ws_urls import urlpatterns as ops_urlpatterns
 from settings.urls.ws_urls import urlpatterns as setting_urlpatterns
 from terminal.urls.ws_urls import urlpatterns as terminal_urlpatterns
 
-__all__ = ['urlpatterns']
+__all__ = ['urlpatterns', 'application']
 
 urlpatterns = ops_urlpatterns + \
               notifications_urlpatterns + \
