b0f1c0c054973808be3858d8bcf6ce6a2249372c
feng <1304903146@qq.com>
2023-08-17 11:41:58 +0800

    perf: 优化 vault 配置

--- apps/settings/api/vault.py
+++ apps/settings/api/vault.py
@@ -29,7 +29,7 @@ class VaultTestingAPI(GenericAPIView):
 
     def post(self, request):
         config = self.get_config(request)
-        config['VAULT_TYPE'] = settings.VAULT_TYPE
+        config['VAULT_ENABLED'] = settings.VAULT_ENABLED
         try:
             client = get_vault_client(raise_exception=True, **config)
             ok, error = client.is_active()
