714ace473965e8b5786bacae1e7d2e5d942af48f
ibuler <ibuler@qq.com>
2023-09-06 13:35:13 +0800

    perf: 优化 ftp log 索引

--- apps/audits/models.py
+++ apps/audits/models.py
@@ -57,7 +57,7 @@ class FTPLog(OrgModelMixin):
     )
     filename = models.CharField(max_length=1024, verbose_name=_("Filename"))
     is_success = models.BooleanField(default=True, verbose_name=_("Success"))
-    date_start = models.DateTimeField(auto_now_add=True, verbose_name=_("Date start"))
+    date_start = models.DateTimeField(auto_now_add=True, verbose_name=_("Date start"), db_index=True)
     has_file = models.BooleanField(default=False, verbose_name=_("File"))
     session = models.CharField(max_length=36, verbose_name=_("Session"), default=uuid.uuid4)
 
@@ -198,7 +198,7 @@ class UserLoginLog(models.Model):
         choices=LoginStatusChoices.choices,
         verbose_name=_("Status"),
     )
-    datetime = models.DateTimeField(default=timezone.now, verbose_name=_("Date login"))
+    datetime = models.DateTimeField(default=timezone.now, verbose_name=_("Date login"), db_index=True)
     backend = models.CharField(
         max_length=32, default="", verbose_name=_("Authentication backend")
     )
