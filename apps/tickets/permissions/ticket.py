8989944e3383e2842f5b69ee2ee908828691caee
ibuler <ibuler@qq.com>
2023-09-11 11:13:34 +0800

    fix: 修复 private storage permission

--- apps/tickets/permissions/ticket.py
+++ apps/tickets/permissions/ticket.py
@@ -1,12 +1,12 @@
 from rest_framework import permissions
 
 
-class IsAssignee(permissions.BasePermission):
+class IsAssignee(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         return obj.has_current_assignee(request.user)
 
 
-class IsApplicant(permissions.BasePermission):
+class IsApplicant(permissions.IsAuthenticated):
 
     def has_object_permission(self, request, view, obj):
         return obj.applicant == request.user
