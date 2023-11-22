6b7119ea7420ba94fcb893887395cc41350a9248
wangruidong <940853815@qq.com>
2023-11-22 16:46:05 +0800

    perf: ldap接口请求换成websocket连接

--- apps/locale/zh/LC_MESSAGES/django.mo
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8324031d40c7cbcd9e332221ee8f607396836b87907847b7f94e879269cc97c8
-size 135739
--- apps/settings/urls/api_urls.py
+++ apps/settings/urls/api_urls.py
@@ -8,11 +8,8 @@ app_name = 'common'
 
 urlpatterns = [
     path('mail/testing/', api.MailTestingAPI.as_view(), name='mail-testing'),
-    path('ldap/testing/config/', api.LDAPTestingConfigAPI.as_view(), name='ldap-testing-config'),
-    path('ldap/testing/login/', api.LDAPTestingLoginAPI.as_view(), name='ldap-testing-login'),
     path('ldap/users/', api.LDAPUserListApi.as_view(), name='ldap-user-list'),
     path('ldap/users/import/', api.LDAPUserImportAPI.as_view(), name='ldap-user-import'),
-    path('ldap/cache/refresh/', api.LDAPCacheRefreshAPI.as_view(), name='ldap-cache-refresh'),
     path('wecom/testing/', api.WeComTestingAPI.as_view(), name='wecom-testing'),
     path('dingtalk/testing/', api.DingTalkTestingAPI.as_view(), name='dingtalk-testing'),
     path('feishu/testing/', api.FeiShuTestingAPI.as_view(), name='feishu-testing'),
