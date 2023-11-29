eee41008ccb5045708002db99be5595ec95771f1
吴小白 <296015668@qq.com>
2023-11-29 11:14:09 +0800

    perf: 优化 celery health 判断

--- utils/check_celery.sh
+++ utils/check_celery.sh
@@ -4,5 +4,5 @@ set -e
 
 test -e /tmp/worker_ready_ansible
 test -e /tmp/worker_ready_celery
-test -e /tmp/worker_heartbeat_ansible && test $(($(date +%s) - $(stat -c %Y /tmp/worker_heartbeat_ansible))) -lt 10
-test -e /tmp/worker_heartbeat_celery && test $(($(date +%s) - $(stat -c %Y /tmp/worker_heartbeat_celery))) -lt 10
+test -e /tmp/worker_heartbeat_ansible && test $(($(date +%s) - $(stat -c %Y /tmp/worker_heartbeat_ansible))) -lt 20
+test -e /tmp/worker_heartbeat_celery && test $(($(date +%s) - $(stat -c %Y /tmp/worker_heartbeat_celery))) -lt 20
