0b7552a6eedbf82b77fef70770638fd48f80c824
ibuler <ibuler@qq.com>
2023-12-19 18:42:44 +0800

    perf: 修改 labels 绑定引起的问题

--- apps/labels/mixins.py
+++ apps/labels/mixins.py
@@ -7,7 +7,15 @@ __all__ = ['LabeledMixin']
 
 
 class LabeledMixin(models.Model):
-    labels = GenericRelation(LabeledResource, object_id_field='res_id', content_type_field='res_type')
+    _labels = GenericRelation(LabeledResource, object_id_field='res_id', content_type_field='res_type')
 
     class Meta:
         abstract = True
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @labels.setter
+    def labels(self, value):
+        self._labels.set(value, bulk=False)
