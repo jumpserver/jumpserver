39e1d60b60204525983e218aa4f611805039a99f
ibuler <ibuler@qq.com>
2023-12-21 11:56:10 +0800

    perf: 优化导入账号报错

--- apps/common/serializers/fields.py
+++ apps/common/serializers/fields.py
@@ -66,6 +66,10 @@ class LabeledChoiceField(ChoiceField):
     def to_internal_value(self, data):
         if isinstance(data, dict):
             data = data.get("value")
+        if "(" in data and data.endswith(")"):
+            d = data.strip(")").split('(')[-1]
+            if d in self.choices:
+                data = d
         return super(LabeledChoiceField, self).to_internal_value(data)
 
 
