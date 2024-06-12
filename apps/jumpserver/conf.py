4f2e2aedbb362f11546a7ca2c8bd9d2cd4961188
wangruidong <940853815@qq.com>
2024-06-12 19:30:12 +0800

    perf: Modify some translations

--- apps/jumpserver/conf.py
+++ apps/jumpserver/conf.py
@@ -489,7 +489,7 @@ class Config(dict):
         # 安全配置
         'SECURITY_MFA_AUTH': 0,  # 0 不开启 1 全局开启 2 管理员开启
         'SECURITY_MFA_AUTH_ENABLED_FOR_THIRD_PARTY': True,
-        'SECURITY_COMMAND_EXECUTION': True,
+        'SECURITY_COMMAND_EXECUTION': False,
         'SECURITY_COMMAND_BLACKLIST': [
             'reboot', 'shutdown', 'poweroff', 'halt', 'dd', 'half', 'top'
         ],
