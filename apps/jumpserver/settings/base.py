6c59888d777ee0098fe79aafc3978a785eae9ecb
feng <1304903146@qq.com>
2024-06-14 11:26:54 +0800

    perf: Refactor OperateLogStore separator logic for database compatibility

--- apps/jumpserver/settings/base.py
+++ apps/jumpserver/settings/base.py
@@ -243,9 +243,10 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DB_OPTIONS = {}
+DB_ENGINE = CONFIG.DB_ENGINE.lower()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.{}'.format(CONFIG.DB_ENGINE.lower()),
+        'ENGINE': 'django.db.backends.{}'.format(DB_ENGINE),
         'NAME': CONFIG.DB_NAME,
         'HOST': CONFIG.DB_HOST,
         'PORT': CONFIG.DB_PORT,
@@ -257,7 +258,7 @@ DATABASES = {
 }
 
 DB_USE_SSL = CONFIG.DB_USE_SSL
-if CONFIG.DB_ENGINE.lower() == 'mysql':
+if DB_ENGINE == 'mysql':
     DB_OPTIONS['init_command'] = "SET sql_mode='STRICT_TRANS_TABLES'"
     if DB_USE_SSL:
         DB_CA_PATH = exist_or_default(os.path.join(CERTS_DIR, 'db_ca.pem'), None)
