03a66fd5634e23791c443e81ce6eb1cfe78fc2de
fit2bot <68588906+fit2bot@users.noreply.github.com>
2024-07-24 10:59:54 +0800

    perf: Modify error message for desktop client login (#13763)
    * perf: Modify error message for desktop client login
    
    * perf: Update Dockerfile with new base image tag
    
    ---------
    
    Co-authored-by: wangruidong <940853815@qq.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- apps/authentication/views/slack.py
+++ apps/authentication/views/slack.py
@@ -37,15 +37,7 @@ class SlackMixin(UserConfirmRequiredExceptionMixin, PermissionsMixin, FlashMessa
             )
 
     def verify_state(self):
-        state = self.request.GET.get('state')
-        session_state = self.request.session.get(SLACK_STATE_SESSION_KEY)
-        if state != session_state:
-            return False
-        return True
-
-    def get_verify_state_failed_response(self, redirect_uri):
-        msg = _("The system configuration is incorrect. Please contact your administrator")
-        return self.get_failed_response(redirect_uri, msg, msg)
+        return self.verify_state_with_session_key(SLACK_STATE_SESSION_KEY)
 
     def get_qr_url(self, redirect_uri):
         state = random_string(16)
