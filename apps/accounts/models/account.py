77144eec052303a5638892f5351d1cb3d7aed487
feng <1304903146@qq.com>
2024-08-14 14:53:07 +0800

    fix: Use only_sudo failed

--- apps/accounts/models/account.py
+++ apps/accounts/models/account.py
@@ -120,6 +120,7 @@ class Account(AbsConnectivity, LabeledMixin, BaseAccount):
 
         auth.update(self.make_account_ansible_vars(su_from))
         become_method = platform.su_method if platform.su_method else 'sudo'
+        become_method = become_method.lstrip('only_')
         password = su_from.secret if become_method == 'sudo' else self.secret
         auth['ansible_become'] = True
         auth['ansible_become_method'] = become_method
