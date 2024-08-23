7c521d2d1b4a2d235d416c6b555a3b1b89455338
wangruidong <940853815@qq.com>
2024-08-23 18:55:25 +0800

    perf: Optimize asset connection speed with es command storage

--- apps/terminal/signal_handlers/session.py
+++ apps/terminal/signal_handlers/session.py
@@ -4,16 +4,9 @@ from django.dispatch import receiver
 from terminal.models import Session
 
 
-@receiver(pre_save, sender=Session)
-def on_session_pre_save(sender, instance, **kwargs):
-    if instance.need_update_cmd_amount:
-        instance.cmd_amount = instance.compute_command_amount()
-
-
 @receiver(post_save, sender=Session)
 def on_session_finished(sender, instance: Session, created, **kwargs):
     if not instance.is_finished:
         return
     # 清理一次可能因 task 未执行的缓存数据
     Session.unlock_session(instance.id)
-
