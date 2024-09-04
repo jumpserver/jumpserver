7973239424b809b68588beb4888e0f3998b5a0c9
ibuler <ibuler@qq.com>
2024-09-04 18:24:41 +0800

    perf: support change gateway platform

--- apps/assets/serializers/gateway.py
+++ apps/assets/serializers/gateway.py
@@ -14,6 +14,11 @@ class GatewaySerializer(HostSerializer):
     class Meta(HostSerializer.Meta):
         model = Gateway
 
+    def validate_platform(self, p):
+        if not p.name.startswith('Gateway'):
+            raise serializers.ValidationError(_('The platform must start with Gateway'))
+        return p
+
     def validate_name(self, value):
         queryset = Asset.objects.filter(name=value)
         if self.instance:
