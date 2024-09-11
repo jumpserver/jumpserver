8e01c571a5f0b60581809301674f6943795f5aae
wangruidong <940853815@qq.com>
2024-09-11 18:01:01 +0800

    perf: modify only_myself to only_mine

--- apps/ops/api/adhoc.py
+++ apps/ops/api/adhoc.py
@@ -30,7 +30,7 @@ class AdHocViewSet(JMSBulkModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         user = self.request.user
-        if is_true(self.request.query_params.get('only_myself')):
+        if is_true(self.request.query_params.get('only_mine')):
             queryset = queryset.filter(creator=user)
         else:
             queryset = queryset.filter(Q(creator=user) | Q(scope=Scope.public))
