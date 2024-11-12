31b0e3ecf0513211ffae71f62486a7676f1e8d9e
feng <1304903146@qq.com>
2024-11-12 10:53:16 +0800

    perf: Modify account migration files

--- apps/accounts/migrations/0008_automation_check_conn_after_change.py
+++ apps/accounts/migrations/0008_automation_check_conn_after_change.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0006_accountcheckautomation_accountrisk'),
+        ('accounts', '0007_alter_accountrisk_risk'),
     ]
 
     operations = [
