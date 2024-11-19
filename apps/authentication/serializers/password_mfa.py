f92c557235d5ddb2ad6861877f1ac0deaca78db5
Aaron3S <chenyang@fit2cloud.com>
2024-11-19 19:30:31 +0800

    feat: 增加人脸识别超时控制

--- apps/authentication/serializers/password_mfa.py
+++ apps/authentication/serializers/password_mfa.py
@@ -58,7 +58,7 @@ class MFAFaceCallbackSerializer(serializers.Serializer):
     token = serializers.CharField(required=True, allow_blank=False)
     success = serializers.BooleanField(required=True, allow_null=False)
     error_message = serializers.CharField(required=False, allow_null=True, allow_blank=True)
-    face_code = serializers.CharField(required=True)
+    face_code = serializers.CharField(required=False, allow_null=True, allow_blank=True)
 
     def update(self, instance, validated_data):
         pass
