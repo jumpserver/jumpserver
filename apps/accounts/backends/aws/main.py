94634e835d73f42edb53b0a850e6ddce1e13f720
jiangweidong <1053570670@qq.com>
2024-11-22 10:41:24 +0800

    perf: optimizing the code

--- apps/accounts/backends/aws/main.py
+++ apps/accounts/backends/aws/main.py
@@ -1,15 +1,10 @@
-from common.db.utils import get_logger
 from .service import AmazonSecretsManagerClient
 from ..base.vault import BaseVault
+from ..utils.mixins import GeneralVaultMixin
 from ...const import VaultTypeChoices
 
 
-logger = get_logger(__name__)
-
-__all__ = ['Vault']
-
-
-class Vault(BaseVault):
+class Vault(GeneralVaultMixin, BaseVault):
     type = VaultTypeChoices.aws
 
     def __init__(self, *args, **kwargs):
@@ -19,27 +14,3 @@ class Vault(BaseVault):
             access_key_id=kwargs.get('VAULT_AWS_ACCESS_KEY_ID'),
             secret_key=kwargs.get('VAULT_AWS_ACCESS_SECRET_KEY'),
         )
-
-    def is_active(self):
-        return self.client.is_active()
-
-    def _get(self, entry):
-        secret = self.client.get(name=entry.full_path)
-        return entry.get_decrypt_secret(secret)
-
-    def _create(self, entry):
-        secret = entry.get_encrypt_secret()
-        self.client.create(name=entry.full_path, secret=secret)
-
-    def _update(self, entry):
-        secret = entry.get_encrypt_secret()
-        self.client.update(name=entry.full_path, secret=secret)
-
-    def _delete(self, entry):
-        self.client.delete(name=entry.full_path)
-
-    def _save_metadata(self, entry, metadata):
-        try:
-            self.client.update_metadata(name=entry.full_path, metadata=metadata)
-        except Exception as e:
-            logger.error(f'save metadata error: {e}')
--- apps/accounts/backends/azure/main.py
+++ apps/accounts/backends/azure/main.py
@@ -1,15 +1,10 @@
-from common.db.utils import get_logger
 from .service import AZUREVaultClient
 from ..base.vault import BaseVault
+from ..utils.mixins import GeneralVaultMixin
 from ...const import VaultTypeChoices
 
 
-logger = get_logger(__name__)
-
-__all__ = ['Vault']
-
-
-class Vault(BaseVault):
+class Vault(GeneralVaultMixin, BaseVault):
     type = VaultTypeChoices.azure
 
     def __init__(self, *args, **kwargs):
@@ -20,27 +15,3 @@ class Vault(BaseVault):
             client_id=kwargs.get('VAULT_AZURE_CLIENT_ID'),
             client_secret=kwargs.get('VAULT_AZURE_CLIENT_SECRET')
         )
-
-    def is_active(self):
-        return self.client.is_active()
-
-    def _get(self, entry):
-        secret = self.client.get(name=entry.full_path)
-        return entry.get_decrypt_secret(secret)
-
-    def _create(self, entry):
-        secret = entry.get_encrypt_secret()
-        self.client.create(name=entry.full_path, secret=secret)
-
-    def _update(self, entry):
-        secret = entry.get_encrypt_secret()
-        self.client.update(name=entry.full_path, secret=secret)
-
-    def _delete(self, entry):
-        self.client.delete(name=entry.full_path)
-
-    def _save_metadata(self, entry, metadata):
-        try:
-            self.client.update_metadata(name=entry.full_path, metadata=metadata)
-        except Exception as e:
-            logger.error(f'save metadata error: {e}')
