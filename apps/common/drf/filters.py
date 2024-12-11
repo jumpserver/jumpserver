8aeffba64dc1bd5142496d73934b3ff11c0b9c62
ibuler <ibuler@qq.com>
2024-12-11 09:59:59 +0800

    perf: update status

--- apps/common/drf/filters.py
+++ apps/common/drf/filters.py
@@ -4,6 +4,7 @@ import base64
 import json
 import logging
 from collections import defaultdict
+from django.utils import timezone
 
 from django.core.cache import cache
 from django.core.exceptions import ImproperlyConfigured
@@ -18,18 +19,26 @@ from rest_framework.filters import OrderingFilter
 from common import const
 from common.db.fields import RelatedManager
 
-logger = logging.getLogger('jumpserver.common')
+logger = logging.getLogger("jumpserver.common")
 
 __all__ = [
-    "DatetimeRangeFilterBackend", "IDSpmFilterBackend",
-    'IDInFilterBackend', "CustomFilterBackend",
-    "BaseFilterSet", 'IDNotFilterBackend',
-    'NotOrRelFilterBackend', 'LabelFilterBackend',
-    'RewriteOrderingFilter', 'AttrRulesFilterBackend'
+    "DatetimeRangeFilterBackend",
+    "IDSpmFilterBackend",
+    "IDInFilterBackend",
+    "CustomFilterBackend",
+    "BaseFilterSet",
+    "IDNotFilterBackend",
+    "NotOrRelFilterBackend",
+    "LabelFilterBackend",
+    "RewriteOrderingFilter",
+    "AttrRulesFilterBackend",
 ]
 
 
 class BaseFilterSet(drf_filters.FilterSet):
+    days = drf_filters.NumberFilter(method="filter_days")
+    days__lt = drf_filters.NumberFilter(method="filter_days")
+
     def do_nothing(self, queryset, name, value):
         return queryset
 
@@ -37,6 +46,22 @@ class BaseFilterSet(drf_filters.FilterSet):
         if k in self.form.data:
             return self.form.cleaned_data[k]
         return default
+
+    @staticmethod
+    def filter_days(queryset, name, value):
+        try:
+            value = int(value)
+        except ValueError:
+            return queryset.none()
+
+        if name == 'days':
+            arg = 'date_created__gte'
+        else:
+            arg = 'date_created__lt'
+
+        date = timezone.now() - timezone.timedelta(days=value)
+        kwargs = {arg: date}
+        return queryset.filter(**kwargs)
 
 
 class DatetimeRangeFilterBackend(filters.BaseFilterBackend):
@@ -50,18 +75,20 @@ class DatetimeRangeFilterBackend(filters.BaseFilterBackend):
             for v in date_range_keyword:
                 ret.append(
                     coreapi.Field(
-                        name=v, location='query', required=False, type='string',
+                        name=v,
+                        location="query",
+                        required=False,
+                        type="string",
                         schema=coreschema.String(
-                            title=v,
-                            description='%s %s' % (attr, v)
-                        )
+                            title=v, description="%s %s" % (attr, v)
+                        ),
                     )
                 )
 
         return ret
 
     def _get_date_range_filter_fields(self, view):
-        if not hasattr(view, 'date_range_filter_fields'):
+        if not hasattr(view, "date_range_filter_fields"):
             return {}
         try:
             return dict(view.date_range_filter_fields)
@@ -75,7 +102,9 @@ class DatetimeRangeFilterBackend(filters.BaseFilterBackend):
                             ('db column', ('query param date from', 'query param date to'))
                         ]
                 ```
-            """.format(view.name)
+            """.format(
+                view.name
+            )
             logger.error(msg)
             raise ImproperlyConfigured(msg)
 
@@ -110,14 +139,17 @@ class IDSpmFilterBackend(filters.BaseFilterBackend):
     def get_schema_fields(self, view):
         return [
             coreapi.Field(
-                name='spm', location='query', required=False,
-                type='string', example='',
-                description='Pre post objects id get spm id, then using filter'
+                name="spm",
+                location="query",
+                required=False,
+                type="string",
+                example="",
+                description="Pre post objects id get spm id, then using filter",
             )
         ]
 
     def filter_queryset(self, request, queryset, view):
-        spm = request.query_params.get('spm')
+        spm = request.query_params.get("spm")
         if not spm:
             return queryset
         cache_key = const.KEY_CACHE_RESOURCE_IDS.format(spm)
@@ -127,7 +159,7 @@ class IDSpmFilterBackend(filters.BaseFilterBackend):
             return queryset.none()
         if isinstance(resource_ids, str):
             resource_ids = [resource_ids]
-        if hasattr(view, 'filter_spm_queryset'):
+        if hasattr(view, "filter_spm_queryset"):
             queryset = view.filter_spm_queryset(resource_ids, queryset)
         else:
             queryset = queryset.filter(id__in=resource_ids)
@@ -138,17 +170,20 @@ class IDInFilterBackend(filters.BaseFilterBackend):
     def get_schema_fields(self, view):
         return [
             coreapi.Field(
-                name='ids', location='query', required=False,
-                type='string', example='/api/v1/users/users?ids=1,2,3',
-                description='Filter by id set'
+                name="ids",
+                location="query",
+                required=False,
+                type="string",
+                example="/api/v1/users/users?ids=1,2,3",
+                description="Filter by id set",
             )
         ]
 
     def filter_queryset(self, request, queryset, view):
-        ids = request.query_params.get('ids')
+        ids = request.query_params.get("ids")
         if not ids:
             return queryset
-        id_list = [i.strip() for i in ids.split(',')]
+        id_list = [i.strip() for i in ids.split(",")]
         queryset = queryset.filter(id__in=id_list)
         return queryset
 
@@ -157,17 +192,20 @@ class IDNotFilterBackend(filters.BaseFilterBackend):
     def get_schema_fields(self, view):
         return [
             coreapi.Field(
-                name='id!', location='query', required=False,
-                type='string', example='/api/v1/users/users?id!=1,2,3',
-                description='Exclude by id set'
+                name="id!",
+                location="query",
+                required=False,
+                type="string",
+                example="/api/v1/users/users?id!=1,2,3",
+                description="Exclude by id set",
             )
         ]
 
     def filter_queryset(self, request, queryset, view):
-        ids = request.query_params.get('id!')
+        ids = request.query_params.get("id!")
         if not ids:
             return queryset
-        id_list = [i.strip() for i in ids.split(',')]
+        id_list = [i.strip() for i in ids.split(",")]
         queryset = queryset.exclude(id__in=id_list)
         return queryset
 
@@ -176,26 +214,30 @@ class LabelFilterBackend(filters.BaseFilterBackend):
     def get_schema_fields(self, view):
         return [
             coreapi.Field(
-                name='label', location='query', required=False,
-                type='string', example='/api/v1/users/users?label=abc',
-                description='Filter by label'
+                name="label",
+                location="query",
+                required=False,
+                type="string",
+                example="/api/v1/users/users?label=abc",
+                description="Filter by label",
             )
         ]
 
     @staticmethod
     def parse_labels(labels_id):
         from labels.models import Label
-        label_ids = [i.strip() for i in labels_id.split(',')]
+
+        label_ids = [i.strip() for i in labels_id.split(",")]
         cleaned = []
 
         args = []
         for label_id in label_ids:
             kwargs = {}
-            if ':' in label_id:
-                k, v = label_id.split(':', 1)
-                kwargs['name'] = k.strip()
-                if v != '*':
-                    kwargs['value'] = v.strip()
+            if ":" in label_id:
+                k, v = label_id.split(":", 1)
+                kwargs["name"] = k.strip()
+                if v != "*":
+                    kwargs["value"] = v.strip()
                 args.append(kwargs)
             else:
                 cleaned.append(label_id)
@@ -209,14 +251,14 @@ class LabelFilterBackend(filters.BaseFilterBackend):
         return cleaned
 
     def filter_queryset(self, request, queryset, view):
-        labels_id = request.query_params.get('labels')
+        labels_id = request.query_params.get("labels")
         if not labels_id:
             return queryset
 
-        if not hasattr(queryset, 'model'):
+        if not hasattr(queryset, "model"):
             return queryset
 
-        if not hasattr(queryset.model, 'label_model'):
+        if not hasattr(queryset.model, "label_model"):
             return queryset
 
         model = queryset.model.label_model()
@@ -225,7 +267,8 @@ class LabelFilterBackend(filters.BaseFilterBackend):
         model_name = model._meta.model_name
 
         full_resources = labeled_resource_cls.objects.filter(
-            res_type__app_label=app_label, res_type__model=model_name,
+            res_type__app_label=app_label,
+            res_type__model=model_name,
         )
         labels = self.parse_labels(labels_id)
         grouped = defaultdict(set)
@@ -234,8 +277,10 @@ class LabelFilterBackend(filters.BaseFilterBackend):
 
         matched_ids = set()
         for name, label_ids in grouped.items():
-            resources = model.filter_resources_by_labels(full_resources, label_ids, rel='any')
-            res_ids = resources.values_list('res_id', flat=True)
+            resources = model.filter_resources_by_labels(
+                full_resources, label_ids, rel="any"
+            )
+            res_ids = resources.values_list("res_id", flat=True)
             if not matched_ids:
                 matched_ids = set(res_ids)
             else:
@@ -249,16 +294,14 @@ class CustomFilterBackend(filters.BaseFilterBackend):
     def get_schema_fields(self, view):
         fields = []
         defaults = dict(
-            location='query', required=False,
-            type='string', example='',
-            description=''
+            location="query", required=False, type="string", example="", description=""
         )
-        if not hasattr(view, 'custom_filter_fields'):
+        if not hasattr(view, "custom_filter_fields"):
             return []
 
         for field in view.custom_filter_fields:
             if isinstance(field, str):
-                defaults['name'] = field
+                defaults["name"] = field
             elif isinstance(field, dict):
                 defaults.update(field)
             else:
@@ -270,7 +313,7 @@ class CustomFilterBackend(filters.BaseFilterBackend):
         return queryset
 
 
-def current_user_filter(user_field='user'):
+def current_user_filter(user_field="user"):
     class CurrentUserFilter(filters.BaseFilterBackend):
         def filter_queryset(self, request, queryset, view):
             return queryset.filter(**{user_field: request.user})
@@ -290,27 +333,30 @@ class AttrRulesFilterBackend(filters.BaseFilterBackend):
     def get_schema_fields(self, view):
         return [
             coreapi.Field(
-                name='attr_rules', location='query', required=False,
-                type='string', example='/api/v1/users/users?attr_rules=jsonbase64',
-                description='Filter by json like {"type": "attrs", "attrs": []} to base64'
+                name="attr_rules",
+                location="query",
+                required=False,
+                type="string",
+                example="/api/v1/users/users?attr_rules=jsonbase64",
+                description='Filter by json like {"type": "attrs", "attrs": []} to base64',
             )
         ]
 
     def filter_queryset(self, request, queryset, view):
-        attr_rules = request.query_params.get('attr_rules')
+        attr_rules = request.query_params.get("attr_rules")
         if not attr_rules:
             return queryset
 
         try:
-            attr_rules = base64.b64decode(attr_rules.encode('utf-8'))
+            attr_rules = base64.b64decode(attr_rules.encode("utf-8"))
         except Exception:
-            raise ValidationError({'attr_rules': 'attr_rules should be base64'})
+            raise ValidationError({"attr_rules": "attr_rules should be base64"})
         try:
             attr_rules = json.loads(attr_rules)
         except Exception:
-            raise ValidationError({'attr_rules': 'attr_rules should be json'})
+            raise ValidationError({"attr_rules": "attr_rules should be json"})
 
-        logger.debug('attr_rules: %s', attr_rules)
+        logger.debug("attr_rules: %s", attr_rules)
         qs = RelatedManager.get_to_filter_qs(attr_rules, queryset.model)
         for q in qs:
             queryset = queryset.filter(q)
@@ -321,33 +367,38 @@ class NotOrRelFilterBackend(filters.BaseFilterBackend):
     def get_schema_fields(self, view):
         return [
             coreapi.Field(
-                name='_rel', location='query', required=False,
-                type='string', example='/api/v1/users/users?name=abc&username=def&_rel=union',
-                description='Filter by rel, or not, default is and'
+                name="_rel",
+                location="query",
+                required=False,
+                type="string",
+                example="/api/v1/users/users?name=abc&username=def&_rel=union",
+                description="Filter by rel, or not, default is and",
             )
         ]
 
     def filter_queryset(self, request, queryset, view):
-        _rel = request.query_params.get('_rel')
-        if not _rel or _rel not in ('or', 'not'):
+        _rel = request.query_params.get("_rel")
+        if not _rel or _rel not in ("or", "not"):
             return queryset
-        if _rel == 'not':
+        if _rel == "not":
             queryset.query.where.negated = True
-        elif _rel == 'or':
-            queryset.query.where.connector = 'OR'
+        elif _rel == "or":
+            queryset.query.where.connector = "OR"
         queryset._result_cache = None
         return queryset
 
 
 class RewriteOrderingFilter(OrderingFilter):
-    default_ordering_if_has = ('name', )
+    default_ordering_if_has = ("name",)
 
     def get_default_ordering(self, view):
         ordering = super().get_default_ordering(view)
         # 如果 view.ordering = [] 表示不排序, 这样可以节约性能 (比如: 用户授权的资产)
         if ordering is not None:
             return ordering
-        ordering_fields = getattr(view, 'ordering_fields', self.ordering_fields)
+        ordering_fields = getattr(view, "ordering_fields", self.ordering_fields)
         if ordering_fields:
-            ordering = tuple([f for f in ordering_fields if f in self.default_ordering_if_has])
+            ordering = tuple(
+                [f for f in ordering_fields if f in self.default_ordering_if_has]
+            )
         return ordering
