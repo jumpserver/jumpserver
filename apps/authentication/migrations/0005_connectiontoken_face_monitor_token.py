0dd015bcba702daba69bb1fc262a183d35957c62
Aaron3S <chenyang@fit2cloud.com>
2024-12-13 12:40:06 +0800

    feat: update migrations

--- apps/authentication/migrations/0005_connectiontoken_face_monitor_token.py
+++ apps/authentication/migrations/0005_connectiontoken_face_monitor_token.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('authentication', '0003_sshkey'),
+        ('authentication', '0004_alter_passkey_token'),
     ]
 
     operations = [
