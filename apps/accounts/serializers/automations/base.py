04bf099dbe0dc765d8d36f5c5a2d527e590510f4
ibuler <ibuler@qq.com>
2025-02-20 11:16:21 +0800

    perf: update account serializer remote unused code

--- apps/accounts/serializers/automations/base.py
+++ apps/accounts/serializers/automations/base.py
@@ -2,40 +2,19 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 from assets.const import AutomationTypes
-from assets.models import Asset, Node, BaseAutomation
+from assets.models import BaseAutomation
 from assets.serializers.automations import AutomationExecutionSerializer as AssetAutomationExecutionSerializer
-from common.serializers.fields import ObjectRelatedField
+from assets.serializers.automations import BaseAutomationSerializer as AssetBaseAutomationSerializer
 from common.utils import get_logger
-from ops.mixin import PeriodTaskSerializerMixin
-from orgs.mixins.serializers import BulkOrgResourceModelSerializer
 
 logger = get_logger(__file__)
 
 __all__ = [
     'BaseAutomationSerializer', 'AutomationExecutionSerializer',
-    'UpdateAssetSerializer', 'UpdateNodeSerializer', 'AutomationAssetsSerializer',
 ]
 
 
-class BaseAutomationSerializer(PeriodTaskSerializerMixin, BulkOrgResourceModelSerializer):
-    assets = ObjectRelatedField(many=True, required=False, queryset=Asset.objects, label=_('Assets'))
-    nodes = ObjectRelatedField(many=True, required=False, queryset=Node.objects, label=_('Nodes'))
-    is_periodic = serializers.BooleanField(default=False, required=False, label=_("Periodic perform"))
-
-    class Meta:
-        read_only_fields = [
-            'date_created', 'date_updated', 'created_by',
-            'periodic_display', 'executed_amount', 'type'
-        ]
-        fields = read_only_fields + [
-            'id', 'name', 'is_periodic', 'interval', 'crontab', 'comment',
-            'accounts', 'nodes', 'assets', 'is_active',
-        ]
-        extra_kwargs = {
-            'name': {'required': True},
-            'executed_amount': {'label': _('Executions')},
-        }
-
+class BaseAutomationSerializer(AssetBaseAutomationSerializer):
     def validate_name(self, name):
         if self.instance and self.instance.name == name:
             return name
@@ -66,22 +45,3 @@ class AutomationExecutionSerializer(AssetAutomationExecutionSerializer):
             'type_display': type_display,
         }
         return snapshot
-
-
-class UpdateAssetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BaseAutomation
-        fields = ['id', 'assets']
-
-
-class UpdateNodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BaseAutomation
-        fields = ['id', 'nodes']
-
-
-class AutomationAssetsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Asset
-        only_fields = ['id', 'name', 'address']
-        fields = tuple(only_fields)
