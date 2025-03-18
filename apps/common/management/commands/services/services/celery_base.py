49378d1f13c69cdb4a265d60b84588ac4c64bb19
ibuler <ibuler@qq.com>
2025-03-18 16:32:59 +0800

    perf: update ansible env set

--- apps/common/management/commands/services/services/celery_base.py
+++ apps/common/management/commands/services/services/celery_base.py
@@ -12,6 +12,10 @@ class CeleryBaseService(BaseService):
     @property
     def cmd(self):
         print('\n- Start Celery as Distributed Task Queue: {}'.format(self.queue.capitalize()))
+        os.environ.setdefault('PYTHONPATH', settings.APPS_DIR)
+        os.environ.setdefault('LC_ALL', 'en_US.UTF-8')
+        os.environ.setdefault('LANG', 'en_US.UTF-8')
+        os.environ.setdefault('PYTHONOPTIMIZE', '1')
 
         if os.getuid() == 0:
             os.environ.setdefault('C_FORCE_ROOT', '1')
