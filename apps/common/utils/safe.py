bf92c756d41ea9691bfe0e956276614a2a928536
wangruidong <940853815@qq.com>
2025-08-27 14:24:26 +0800

    fix: Ensure command arguments are safely quoted in safe_run_cmd

--- apps/common/utils/safe.py
+++ apps/common/utils/safe.py
@@ -1,9 +1,8 @@
-import re
-import subprocess
 import shlex
+import subprocess
 
 
 def safe_run_cmd(cmd_str, cmd_args=(), shell=True):
-    cmd_args = [shlex.quote(arg) for arg in cmd_args]
+    cmd_args = [shlex.quote(str(arg)) for arg in cmd_args]
     cmd = cmd_str % tuple(cmd_args)
     return subprocess.run(cmd, shell=shell)
