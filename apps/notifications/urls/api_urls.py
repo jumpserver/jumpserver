fa472e42051dd58f22a1332bb0c4e0c3f600fee1
wangruidong <940853815@qq.com>
2025-09-08 18:03:32 +0800

    perf: content serialize valid

--- apps/notifications/urls/api_urls.py
+++ apps/notifications/urls/api_urls.py
@@ -10,12 +10,9 @@ router = BulkRouter()
 router.register('system-msg-subscription', api.SystemMsgSubscriptionViewSet, 'system-msg-subscription')
 router.register('user-msg-subscription', api.UserMsgSubscriptionViewSet, 'user-msg-subscription')
 router.register('site-messages', api.SiteMessageViewSet, 'site-message')
-
+router.register('templates', api.TemplateViewSet, 'template')
 urlpatterns = [
     path('backends/', api.BackendListView.as_view(), name='backends'),
-    # 模板管理接口：列出（渲染或读取 data/template）和编辑保存
-    path('templates/', api.get_templates_list, name='templates-list'),
-    path('templates/edit/', api.edit_template, name='templates-edit'),
 ]
 urlpatterns += router.urls
 
