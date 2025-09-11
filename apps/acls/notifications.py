a732cc614ec6ed1fa3a16e754054f53ae5deaab4
feng <1304903146@qq.com>
2025-09-11 14:14:13 +0800

    perf: Asset user login notify

--- apps/acls/notifications.py
+++ apps/acls/notifications.py
@@ -1,4 +1,4 @@
-from django.template.loader import render_to_string
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from accounts.models import Account
@@ -26,15 +26,20 @@ class UserLoginReminderMsg(UserMessage):
     def __init__(self, user, user_log: UserLoginLog, acl: LoginACL):
         self.user_log = user_log
         self.acl_name = str(acl)
+        self.login_from = user_log.get_type_display()
+        now = timezone.localtime(user_log.datetime)
+        self.time = now.strftime('%Y-%m-%d %H:%M:%S')
         super().__init__(user)
 
     def get_html_msg(self) -> dict:
         user_log = self.user_log
         context = {
             'ip': user_log.ip,
+            'time': self.time,
             'city': user_log.city,
-            'username': user_log.username,
             'acl_name': self.acl_name,
+            'login_from': self.login_from,
+            'username': user_log.username,
             'recipient_name': self.user.name,
             'recipient_username': self.user.username,
             'user_agent': user_log.user_agent,
@@ -71,22 +76,27 @@ class AssetLoginReminderMsg(UserMessage):
     def __init__(
             self, user, asset: Asset, login_user: User,
             account: Account, acl: LoginAssetACL,
-            ip, input_username
+            ip, input_username, login_from
     ):
         self.ip = ip
         self.asset = asset
         self.login_user = login_user
         self.account = account
         self.acl_name = str(acl)
+        self.login_from = login_from
         self.login_user = login_user
         self.input_username = input_username
+
+        now = timezone.localtime(timezone.now())
+        self.time = now.strftime('%Y-%m-%d %H:%M:%S')
         super().__init__(user)
 
     def get_html_msg(self) -> dict:
         context = {
             'ip': self.ip,
-            'recipient_name': self.user.name,
-            'recipient_username': self.user.username,
+            'time': self.time,
+            'login_from': self.login_from,
+            'recipient': self.user,
             'username': self.login_user.username,
             'name': self.login_user.name,
             'asset': str(self.asset),
