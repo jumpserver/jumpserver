aafff768c14584c128d12afee2d30bd9b199be71
Bai <baijiangjie@gmail.com>
2025-09-25 14:29:56 +0800

    perf: passkey auth hidden

--- apps/authentication/backends/passkey/urls.py
+++ apps/authentication/backends/passkey/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import api
 
 router = DefaultRouter()
-router.register('passkeys', api.PasskeyViewSet, 'passkey')
+# router.register('passkeys', api.PasskeyViewSet, 'passkey')
 
 urlpatterns = []
 urlpatterns += router.urls
