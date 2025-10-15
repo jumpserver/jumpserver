8f0a41b1a89dac25bb171fb832fff805f91d47aa
Aaron3S <chenyang@fit2cloud.com>
2025-10-15 15:44:51 +0800

    fix: fix data masking org problem

--- apps/acls/api/data_masking.py
+++ apps/acls/api/data_masking.py
@@ -1,6 +1,5 @@
-from common.api import JMSBulkModelViewSet
+from orgs.mixins.api import OrgBulkModelViewSet
 
-from orgs.utils import tmp_to_root_org
 from .common import ACLUserFilterMixin
 from ..models import DataMaskingRule
 from .. import serializers
@@ -15,12 +14,8 @@ class DataMaskingRuleFilter(ACLUserFilterMixin):
         fields = ('name', 'action')
 
 
-class DataMaskingRuleViewSet(JMSBulkModelViewSet):
-    queryset = DataMaskingRule.objects.all()
+class DataMaskingRuleViewSet(OrgBulkModelViewSet):
+    model = DataMaskingRule
     filterset_class = DataMaskingRuleFilter
     search_fields = ('name',)
     serializer_class = serializers.DataMaskingRuleSerializer
-
-    def filter_queryset(self, queryset):
-        with tmp_to_root_org():
-            return super().filter_queryset(queryset)
