350a8b570ed63cc8cb4f968dd2db2e604058df3c
Ewall555 <a03216@foxmail.com>
2025-11-13 16:46:22 +0800

    feat: add namespace field to Cloud model and serializer for k8s and translate

--- apps/assets/models/asset/cloud.py
+++ apps/assets/models/asset/cloud.py
@@ -1,9 +1,12 @@
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from .common import Asset
 
 
 class Cloud(Asset):
+    namespace = models.CharField(max_length=1024, verbose_name=_("Namespace"), blank=True)
+
     def __str__(self):
         return self.name
 
