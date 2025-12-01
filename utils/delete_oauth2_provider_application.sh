1ee68134f2ae1506ac3c2bf20cf9a6be26f1f850
Bai <baijiangjie@gmail.com>
2025-12-01 10:38:22 +0800

    fix: rename utils methond

--- utils/delete_oauth2_provider_application.sh
+++ utils/delete_oauth2_provider_application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-function disable_auth_ldap() {
+function delete_oauth2_provider_applications() {
 python3 ../apps/manage.py shell << EOF
 from oauth2_provider.models import *
 apps = Application.objects.all()
@@ -10,4 +10,4 @@ print("OAuth2 Provider Applications deleted successfully!")
 EOF
 }
 
-disable_auth_ldap
+delete_oauth2_provider_applications
