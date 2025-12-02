b8ecb703cff805d332491f5efcdfda5e65a51059
Bai <baijiangjie@gmail.com>
2025-12-02 18:04:31 +0800

    perf: url revoke_token/ to revoke/

--- apps/authentication/backends/oauth2_provider/urls.py
+++ apps/authentication/backends/oauth2_provider/urls.py
@@ -9,6 +9,6 @@ from . import views
 urlpatterns = [
     path("authorize/", op_views.AuthorizationView.as_view(), name="authorize"),
     path("token/", op_views.TokenView.as_view(), name="token"),
-    path("revoke_token/", op_views.RevokeTokenView.as_view(), name="revoke-token"),
+    path("revoke/", op_views.RevokeTokenView.as_view(), name="revoke-token"),
     path(".well-known/oauth-authorization-server", views.OAuthAuthorizationServerView.as_view(), name="oauth-authorization-server"),
 ]
