9833aab4dc7176e098e3ddad553e15949817c79c
ibuler <ibuler@qq.com>
2026-01-16 09:43:59 +0800

    perf: add disable user script

--- utils/disable_user.py
+++ utils/disable_user.py
@@ -13,33 +13,22 @@ os.environ.setdefault("DJANGO_DEBUG_SHELL", "1")
 django.setup()
 
 from users.models import User
-from django.utils import timezone
 
 
-def activate_user(username):
+def disable_user(username):
     user = User.objects.filter(username=username).first()
     if not user:
         print("Not found user: ", username)
         return
 
-    print("Activate user: ", username)
-    user.is_active = True
-
-    fields = []
-    if user.is_expired:
-        user.date_expired = timezone.now() + timezone.timedelta(days=365)
-        fields.append('date_expired')
-
-    if user.password_has_expired:
-        user.date_password_last_updated = timezone.now()
-        fields.append('date_password_last_updated')
-
-    user.save(update_fields=['is_active'] + fields)
+    print("Disable user: ", username)
+    user.is_active = False
+    user.save(update_fields=['is_active'])
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python activate_user.py <username>")
+        print("Usage: python disable_user.py <username>")
         sys.exit(1)
     username = sys.argv[1]
-    activate_user(username)
+    disable_user(username)
