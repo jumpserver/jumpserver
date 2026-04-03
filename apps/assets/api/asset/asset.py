jumpserver/jumpserver

commit 697b3fb860042f5f0eaf9a43e32cbc6e1ef9014e

perf: 自动化按钮 (#9008)
Co-authored-by: feng <1304903146@qq.com>

apps/assets/api/asset/asset.py
@@ -12,6 +12,8 @@ from orgs.mixins import generics
 from assets import serializers
 from assets.models import Asset, Gateway
 from assets.tasks import (
+    push_accounts_to_assets,
+    verify_accounts_connectivity,
     test_assets_connectivity_manual,
     update_assets_hardware_info_manual,
 )
@@ -110,9 +112,9 @@ class AssetTaskCreateApi(AssetsTaskMixin, generics.CreateAPIView):
         action = request.data.get('action')
         action_perm_require = {
             'refresh': 'assets.refresh_assethardwareinfo',
-            'push_system_user': 'assets.push_assetsystemuser',
+            'push_account': 'assets.push_assetsystemuser',
             'test': 'assets.test_assetconnectivity',
-            'test_system_user': 'assets.test_assetconnectivity'
+            'test_account': 'assets.test_assetconnectivity'
         }
         perm_required = action_perm_require.get(action)
         has = self.request.user.has_perm(perm_required)
@@ -120,20 +122,23 @@ class AssetTaskCreateApi(AssetsTaskMixin, generics.CreateAPIView):
         if not has:
             self.permission_denied(request)
 
-    def perform_asset_task(self, serializer):
+    @staticmethod
+    def perform_asset_task(serializer):
         data = serializer.validated_data
-        action = data['action']
-        if action not in ['push_system_user', 'test_system_user']:
+        if data['action'] not in ['push_system_user', 'test_system_user']:
             return
 
         asset = data['asset']
-        system_users = data.get('system_users')
-        if not system_users:
-            system_users = asset.get_all_system_users()
-        if action == 'push_system_user':
-            task = push_system_users_a_asset.delay(system_users, asset=asset)
-        elif action == 'test_system_user':
-            task = test_system_users_connectivity_a_asset.delay(system_users, asset=asset)
+        accounts = data.get('accounts')
+        if not accounts:
+            accounts = asset.accounts.all()
+
+        asset_ids = [asset.id]
+        account_ids = accounts.values_list('id', flat=True)
+        if action == 'push_account':
+            task = push_accounts_to_assets.delay(account_ids, asset_ids)
+        elif action == 'test_account':
+            task = verify_accounts_connectivity.delay(account_ids, asset_ids)
         else:
             task = None
         return task

apps/assets/serializers/asset/common.py
@@ -206,3 +206,6 @@ class AssetTaskSerializer(AssetsTaskSerializer):
     asset = serializers.PrimaryKeyRelatedField(
         queryset=Asset.objects, required=False, allow_empty=True, many=False
     )
+    accounts = serializers.PrimaryKeyRelatedField(
+        queryset=Account.objects, required=False, allow_empty=True, many=True
+    )

apps/assets/models/asset/common.py
@@ -229,7 +229,7 @@ class Asset(NodesRelationMixin, AbsConnectivity, JMSOrgBaseModel):
         permissions = [
             ('refresh_assethardwareinfo', _('Can refresh asset hardware info')),
             ('test_assetconnectivity', _('Can test asset connectivity')),
-            ('push_assetsystemuser', _('Can push system user to asset')),
+            ('push_assetaccount', _('Can push account to asset')),
             ('match_asset', _('Can match asset')),
             ('add_assettonode', _('Add asset to node')),
             ('move_assettonode', _('Move asset to node')),

apps/assets/api/account/account.py
@@ -10,7 +10,7 @@ from common.permissions import UserConfirmation
 from authentication.const import ConfirmType
 from assets.models import Account
 from assets.filters import AccountFilterSet
-from assets.tasks.account_connectivity import test_accounts_connectivity_manual
+from assets.tasks import verify_accounts_connectivity
 from assets import serializers
 
 __all__ = ['AccountViewSet', 'AccountSecretsViewSet', 'AccountTaskCreateAPI', 'AccountHistoriesSecretAPI']
@@ -32,7 +32,9 @@ class AccountViewSet(OrgBulkModelViewSet):
     @action(methods=['post'], detail=True, url_path='verify')
     def verify_account(self, request, *args, **kwargs):
         account = super().get_object()
-        task = test_accounts_connectivity_manual.delay([account.id])
+        account_ids = [account.id]
+        asset_ids = [account.asset_id]
+        task = verify_accounts_connectivity.delay(account_ids, asset_ids)
         return Response(data={'task': task.id})
 
 
@@ -80,8 +82,10 @@ class AccountTaskCreateAPI(CreateAPIView):
         return queryset
 
     def perform_create(self, serializer):
-        account_ids = self.get_accounts().values_list('id', flat=True)
-        task = test_accounts_connectivity_manual.delay(account_ids)
+        accounts = self.get_accounts()
+        account_ids = accounts.values_list('id', flat=True)
+        asset_ids = [account.asset_id for account in accounts]
+        task = verify_accounts_connectivity.delay(account_ids, asset_ids)
         data = getattr(serializer, '_data', {})
         data["task"] = task.id
         setattr(serializer, '_data', data)

apps/assets/migrations/0108_auto_20221027_1053.py
@@ -38,4 +38,13 @@ class Migration(migrations.Migration):
             },
             bases=('assets.baseautomation',),
         ),
+        migrations.AlterModelOptions(
+            name='asset',
+            options={'ordering': ['name'],
+                     'permissions': [('refresh_assethardwareinfo', 'Can refresh asset hardware info'),
+                                     ('test_assetconnectivity', 'Can test asset connectivity'),
+                                     ('push_assetaccount', 'Can push account to asset'),
+                                     ('match_asset', 'Can match asset'), ('add_assettonode', 'Add asset to node'),
+                                     ('move_assettonode', 'Move asset to node')], 'verbose_name': 'Asset'},
+        ),
     ]

apps/assets/tasks/__init__.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-
+from .ping import *
 from .utils import *
 from .common import *
 from .backup import *
 from .automation import *
+from .gather_facts import *
 from .nodes_amount import *
-from .gather_asset_users import *
-from .asset_connectivity import *
-from .account_connectivity import *
-from .gather_asset_hardware_info import *
+from .push_account import *
+from .verify_account import *

apps/assets/tasks/account_connectivity.py
@@ -1,110 +1,0 @@
-# ~*~ coding: utf-8 ~*~
-
-from celery import shared_task
-from django.utils.translation import ugettext as _, gettext_noop
-
-from common.utils import get_logger
-from orgs.utils import org_aware_func
-from ..models import Connectivity, Account
-from . import const
-from .utils import check_asset_can_run_ansible
-
-
-logger = get_logger(__file__)
-
-
-__all__ = [
-    'test_account_connectivity_util', 'test_accounts_connectivity_manual',
-    'get_test_account_connectivity_tasks', 'test_user_connectivity',
-    'run_adhoc',
-]
-
-
-def get_test_account_connectivity_tasks(asset):
-    if asset.is_unixlike():
-        tasks = const.PING_UNIXLIKE_TASKS
-    elif asset.is_windows():
-        tasks = const.PING_WINDOWS_TASKS
-    else:
-        msg = _(
-            "The asset {} system platform {} does not "
-            "support run Ansible tasks".format(asset.name, asset.platform)
-        )
-        logger.info(msg)
-        tasks = []
-    return tasks
-
-
-def run_adhoc(task_name, tasks, inventory):
-    """
-    :param task_name
-    :param tasks
-    :param inventory
-    """
-    from ops.ansible.runner import AdHocRunner
-    runner = AdHocRunner(inventory, options=const.TASK_OPTIONS)
-    result = runner.run(tasks, 'all', task_name)
-    return result.results_raw, result.results_summary
-
-
-def test_user_connectivity(task_name, asset, username, password=None, private_key=None):
-    """
-    :param task_name
-    :param asset
-    :param username
-    :param password
-    :param private_key
-    """
-    from ops.inventory import JMSCustomInventory
-
-    tasks = get_test_account_connectivity_tasks(asset)
-    if not tasks:
-        logger.debug("No tasks ")
-        return {}, {}
-    inventory = JMSCustomInventory(
-        assets=[asset], username=username, password=password,
-        private_key=private_key
-    )
-    raw, summary = run_adhoc(
-        task_name=task_name, tasks=tasks, inventory=inventory
-    )
-    return raw, summary
-
-
-@org_aware_func("account")
-def test_account_connectivity_util(account, task_name):
-    """
-    :param account: <Account>对象
-    :param task_name:
-    :return:
-    """
-    if not check_asset_can_run_ansible(account.asset):
-        return
-
-    account.load_auth()
-    try:
-        raw, summary = test_user_connectivity(
-            task_name=task_name, asset=account.asset,
-            username=account.username, password=account.password,
-            private_key=account.private_key_file
-        )
-    except Exception as e:
-        logger.warn("Failed run adhoc {}, {}".format(task_name, e))
-        return
-
-    if summary.get('success'):
-        account.set_connectivity(Connectivity.ok)
-    else:
-        account.set_connectivity(Connectivity.failed)
-
-
-@shared_task(queue="ansible")
-def test_accounts_connectivity_manual(account_ids):
-    """
-    :param accounts: <Account>对象
-    """
-    accounts = Account.objects.filter(id__in=account_ids)
-    for account in accounts:
-        task_name = gettext_noop("Test account connectivity: ") + str(account)
-        test_account_connectivity_util(account, task_name)
-        print(".\n")

apps/assets/tasks/automation.py
@@ -6,7 +6,7 @@ from common.utils import get_logger, get_object_or_none
 logger = get_logger(__file__)
 
 
-@shared_task
+@shared_task(queue='ansible')
 def execute_automation(pid, trigger, mode):
     with tmp_to_root_org():
         instance = get_object_or_none(mode, pk=pid)

commit 50bdf768d2376f6b54d326dfb7e6c3ef2d7c2112

fix: NameError in AssetTaskCreateApi.perform_asset_task for push_account/test_account actions
Three bugs introduced in commit 697b3fb8 (2022-11-02) during system_user to account migration:

1. AssetTaskSerializer.ACTION_CHOICES still used old 'push_system_user'/'test_system_user'
   values, causing serializer to reject 'push_account'/'test_account' with HTTP 400.

2. Local variable 'action' was removed but still referenced on lines 286-289,
   causing NameError: name 'action' is not defined.

3. The 'not in' guard check still used old values ['push_system_user', 'test_system_user'],
   so even if the serializer were fixed, the function would always return None.

Fix:
- Update ACTION_CHOICES to ('push_account', 'test_account')
- Restore 'action = data["action"]' local variable
- Guard check now uses the action variable consistently

apps/assets/api/asset/asset.py
@@ -273,7 +273,8 @@ class AssetTaskCreateApi(AssetsTaskMixin, generics.CreateAPIView):
     @staticmethod
     def perform_asset_task(serializer):
         data = serializer.validated_data
-        if data["action"] not in ["push_account", "test_account"]:
+        action = data["action"]
+        if action not in ["push_account", "test_account"]:
             return
 
         asset = data["asset"]

apps/assets/serializers/asset/common.py
@@ -507,8 +507,8 @@ class AssetsTaskSerializer(serializers.Serializer):
 
 class AssetTaskSerializer(AssetsTaskSerializer):
     ACTION_CHOICES = tuple(list(AssetsTaskSerializer.ACTION_CHOICES) + [
-        ('push_system_user', 'push_system_user'),
-        ('test_system_user', 'test_system_user')
+        ('push_account', 'push_account'),
+        ('test_account', 'test_account')
     ])
     action = serializers.ChoiceField(choices=ACTION_CHOICES, write_only=True)
     asset = serializers.PrimaryKeyRelatedField(