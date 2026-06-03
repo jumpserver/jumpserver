8d4ef1ec57889015a20ac1638aaf427aff610558
ibuler <ibuler@qq.com>
2026-06-03 11:09:08 +0800

    perf: update input ssh key

--- apps/authentication/serializers/connection_token.py
+++ apps/authentication/serializers/connection_token.py
@@ -26,7 +26,7 @@ class ConnectionTokenSerializer(OrgResourceModelSerializerMixin):
         model = ConnectionToken
         fields_mini = ['id', 'value']
         fields_small = fields_mini + [
-            'user', 'asset', 'account', 'input_username', 'input_secret',
+            'user', 'asset', 'account', 'input_username', 'input_secret', 'input_secret_type',
             'connect_method', 'connect_options', 'protocol', 'actions',
             'is_active', 'is_reusable', 'from_ticket', 'from_ticket_info',
             'date_expired', 'date_created', 'date_updated', 'created_by',
