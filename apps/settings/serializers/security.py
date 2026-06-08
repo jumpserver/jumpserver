bb81068c70aa235e4c94e3a32451df2a1cd4d5a4
Gerry.tan <qian.tan@fit2cloud.com>
2026-06-08 13:52:25 +0800

    perf: Optional parameter

--- apps/settings/serializers/security.py
+++ apps/settings/serializers/security.py
@@ -182,18 +182,6 @@ class SecurityAuthSerializer(serializers.Serializer):
         help_text=_("Detect infrequent users daily and disable them if they exceed the predetermined time limit")
     )
 
-    RDP_SIGN_ENABLED = serializers.BooleanField(
-        required=False, default=False, label=_("RDP sign enabled"),
-        help_text=_("Enable RDP sign to protect RDP connection")
-    )
-    RDP_SIGN_CERT = serializers.CharField(
-        required=False, label=_("RDP sign cert"),
-        help_text=_("RDP sign cert, please do not modify")
-    )
-    RDP_SIGN_CERT_KEY = serializers.CharField(
-        required=False, label=_("RDP sign cert key"),
-        help_text=_("RDP sign cert key, please do not modify")
-    )
 
 class SecurityLoginSerializer(serializers.Serializer):
 
