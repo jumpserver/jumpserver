269c9bf145e4c7fa4a1f9b273f48fbe2f86344b8
YS-B <3322602367@qq.com>
2026-07-14 16:07:08 +0800

    fix(authentication): validate post-auth redirect URLs

--- apps/authentication/tests.py
+++ apps/authentication/tests.py
@@ -1,4 +1,9 @@
 from common.utils import gen_key_pair, rsa_decrypt, rsa_encrypt
+from django.test import RequestFactory, SimpleTestCase, override_settings
+
+from authentication.api.sso import SSOViewSet
+from common.utils import reverse
+from users.views.profile.password import UserVerifyPasswordView
 
 
 def test_rsa_encrypt_decrypt(message='test-password-$%^&*'):
@@ -13,3 +18,41 @@ def test_rsa_encrypt_decrypt(message='test-password-$%^&*'):
     print('Decrypted message: {}'.format(message_decrypted))
 
 
+class SSOLoginRedirectTests(SimpleTestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    @override_settings(ALLOWED_HOSTS=['testserver'])
+    def test_scheme_relative_next_url_is_rejected(self):
+        request = self.factory.get('/api/v1/authentication/sso/login/', {'next': '//attacker.example.com'})
+
+        self.assertEqual(SSOViewSet.get_safe_next_url(request), reverse('index'))
+
+    @override_settings(ALLOWED_HOSTS=['testserver'])
+    def test_local_next_url_is_preserved(self):
+        request = self.factory.get('/api/v1/authentication/sso/login/', {'next': '/ui/#/workbench'})
+
+        self.assertEqual(SSOViewSet.get_safe_next_url(request), '/ui/#/workbench')
+
+
+class UserVerifyPasswordRedirectTests(SimpleTestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    @override_settings(ALLOWED_HOSTS=['testserver'])
+    def test_scheme_relative_next_url_is_rejected(self):
+        request = self.factory.get('/core/auth/password/verify/', {'next': '//attacker.example.com'})
+        view = UserVerifyPasswordView()
+        view.request = request
+
+        self.assertEqual(view.get_success_url(), '/')
+
+    @override_settings(ALLOWED_HOSTS=['testserver'])
+    def test_local_next_url_is_preserved(self):
+        request = self.factory.get('/core/auth/password/verify/', {'next': '/ui/#/profile/index'})
+        view = UserVerifyPasswordView()
+        view.request = request
+
+        self.assertEqual(view.get_success_url(), '/ui/#/profile/index')
