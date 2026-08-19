e8b9825286e207802a6e4d1be5aeb7dd0a3d922c
52hongyu <1345411028@qq.com>
2026-08-19 14:27:17 +0800

    feat：add syslog docx download

--- apps/settings/api/syslog.py
+++ apps/settings/api/syslog.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 import logging
+import os
 
 from django.conf import settings
-from django.utils.translation import gettext_lazy as _
+from django.http import FileResponse
+from django.utils.encoding import escape_uri_path
+from django.utils.translation import gettext_lazy as _, gettext
 from rest_framework.views import Response, APIView
 
 from common.utils import get_logger
 
 logger = get_logger(__file__)
 
-__all__ = ['SyslogTestingAPI']
+__all__ = ['SyslogTestingAPI', 'SyslogDocDownloadAPI']
 
 
 class SyslogTestingAPI(APIView):
@@ -45,3 +48,32 @@ class SyslogTestingAPI(APIView):
             return Response({"error": str(e)}, status=400)
 
         return Response({"msg": str(self.success_message)})
+
+
+class SyslogDocDownloadAPI(APIView):
+    rbac_perms = {
+        'GET': 'settings.change_other'
+    }
+
+    def get(self, request):
+        doc_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'docs')
+        doc_path = os.path.join(doc_dir, 'syslog_format_sample.docx')
+        if not os.path.exists(doc_path):
+            return Response(
+                {"error": str(_("Syslog format document not found"))},
+                status=404
+            )
+
+        filename = '{}.docx'.format(gettext('Syslog format document'))
+        try:
+            response = FileResponse(
+                open(doc_path, 'rb'),
+                content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            )
+            response['Content-Disposition'] = "attachment; filename*=UTF-8''{}".format(
+                escape_uri_path(filename)
+            )
+            return response
+        except Exception as e:
+            logger.error("Failed to download syslog format document: %s", e)
+            return Response({"error": str(e)}, status=400)
