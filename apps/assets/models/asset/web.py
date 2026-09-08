9d9c7759c9f4db040913e8d07b9be550445db394
ibuler <ibuler@qq.com>
2026-09-08 21:48:20 +0800

    perf: add web black list

--- apps/assets/models/asset/web.py
+++ apps/assets/models/asset/web.py
@@ -6,6 +6,7 @@ from .common import Asset
 
 
 class Web(Asset):
+    allowed_urls = models.JSONField(blank=True, default=list, verbose_name=_("Allowed sites"))
     autofill = models.CharField(max_length=16, choices=FillType.choices, default='basic', verbose_name=_("Autofill"))
     username_selector = models.CharField(max_length=128, blank=True, default='', verbose_name=_("Username selector"))
     password_selector = models.CharField(max_length=128, blank=True, default='', verbose_name=_("Password selector"))
--- apps/assets/tests/test_web_interactive_selector.py
+++ apps/assets/tests/test_web_interactive_selector.py
@@ -9,6 +9,19 @@ from common.serializers.dynamic import create_serializer_class
 
 
 class WebInteractiveSelectorTests(SimpleTestCase):
+    def test_navigation_allowlist_is_optional_and_validates_origins(self):
+        field = WebSerializer().fields['allowed_urls']
+        self.assertFalse(field.required)
+        self.assertEqual(Web().allowed_urls, [])
+        self.assertEqual(WebSpecSerializer(Web(allowed_urls=['https://sso.example.com'])).data['allowed_urls'], ['https://sso.example.com'])
+        for value in ([], ['https://sso.example.com', 'http://localhost:8080', 'https://[::1]:8443']):
+            self.assertEqual(field.run_validation(value), value)
+        for value in (None, '*', ['*'], ['file:///tmp'], ['https://user:secret@example.com'],
+                      ['https://example.com/path'], ['https://example.com?next=evil'],
+                      ['https://example.com:0'], ['https://example.com'] * 101):
+            with self.subTest(value=value), self.assertRaises(ValidationError):
+                field.run_validation(value)
+
     def test_optional_field_is_exposed_in_asset_and_spec(self):
         web = Web()
         for name in ('interactive_selector', 'success_selector'):
