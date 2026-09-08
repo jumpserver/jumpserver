f0848c9b115e9d136bfdf948d0acec1b80a8e9a3
feng <1304903146@qq.com>
2026-09-08 18:37:27 +0800

    fix: make request body memory limit configurable

--- apps/jumpserver/settings/base.py
+++ apps/jumpserver/settings/base.py
@@ -120,6 +120,7 @@ CSRF_FAILURE_VIEW = 'jumpserver.views.other.csrf_failure'
 # print('  - ' + origin)
 # Max post update field num
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
+DATA_UPLOAD_MAX_MEMORY_SIZE = CONFIG.DATA_UPLOAD_MAX_MEMORY_SIZE
 
 # Application definition
 
