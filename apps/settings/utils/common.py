c97e7c8734c3c7729768fab0996b131675770ed1
Eric <xplzv@126.com>
2026-09-23 11:08:30 +0800

    perf: bound nmap scan resource usage

--- apps/settings/utils/common.py
+++ apps/settings/utils/common.py
@@ -18,7 +18,7 @@ def get_login_title():
     return get_interface_setting_or_default()['login_title']
 
 
-def generate_ips(address_string):
+def generate_ips(address_string, max_count=None):
     def transform(_ip):
         real_ip, err_msg = lookup_domain(_ip)
         return _ip if err_msg or real_ip == '0.0.0.0' else real_ip
@@ -26,14 +26,23 @@ def generate_ips(address_string):
     # 支持的格式
     # 192.168.1.1,192.168.1.2
     # 192.168.1.1-12 | 192.168.1.1-192.168.1.12 | 192.168.1.0/30 | 192.168.1.1
+    if max_count is not None and len(address_string) > max_count * 256:
+        raise ValueError('Target specification is too long')
     ips = []
     ip_list = address_string.split(',')
+    if max_count is not None and len(ip_list) > max_count:
+        raise ValueError(f'At most {max_count} target addresses may be scanned')
     if len(ip_list) >= 1:
+        count = 0
         for ip in ip_list:
             try:
-                ips.append(str(IP(transform(ip))))
+                address = IP(transform(ip))
             except ValueError:
-                pass
+                continue
+            count += address.len()
+            if max_count is not None and count > max_count:
+                raise ValueError(f'At most {max_count} target addresses may be scanned')
+            ips.append(str(address))
         if ips:
             return ips
 
@@ -43,12 +52,18 @@ def generate_ips(address_string):
             start_ip, end_ip = ip_list
             if ip_list[1].find('.') == -1:
                 end_ip = start_ip[:start_ip.rindex('.') + 1] + end_ip
-            for ip in range(IP(start_ip).int(), IP(end_ip).int() + 1):
-                ips.extend((str(ip) for ip in IP(ip)))
+            start, end = IP(start_ip).int(), IP(end_ip).int()
+            count = end - start + 1
+            addresses = (str(addr) for value in range(start, end + 1) for addr in IP(value))
         else:
-            ips.extend((str(ip) for ip in IP(ip_list[0])))
+            network = IP(ip_list[0])
+            count = network.len()
+            addresses = (str(ip) for ip in network)
     except ValueError:
-        ips = []
+        return []
+    if max_count is not None and count > max_count:
+        raise ValueError(f'At most {max_count} target addresses may be scanned')
+    ips.extend(addresses)
     return ips
 
 
