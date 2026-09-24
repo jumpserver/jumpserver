5e88b9595ca4d0c5f4c2432760e97462c2ee0655
wangruidong <940853815@qq.com>
2026-09-24 17:34:31 +0800

    fix(migrate): sequence application credentials after template following

--- apps/accounts/migrations/0013_application_credentials.py
+++ apps/accounts/migrations/0013_application_credentials.py
@@ -11,7 +11,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0011_personalassetcredential'),
+        ('accounts', '0012_account_follow_template'),
     ]
 
     operations = [
